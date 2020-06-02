c17045ed0750ff4ba8ea6f148992e4de611c5537
Blaž Hrastnik <blaz@mxxn.io>
2020-06-02 10:44:44 +0900

    Fixup Surface

--- helix-term/src/component.rs
+++ helix-term/src/component.rs
@@ -1,15 +1,17 @@
-
 // IDEA: render to a cache buffer, then if not changed, copy the buf into the parent
+type Surface = ();
 pub trait Component {
     /// Process input events, return true if handled.
-    fn process_event(&mut self, event: crossterm::event::Event, args) -> bool;
+    fn process_event(&mut self, event: crossterm::event::Event, args: ()) -> bool;
     /// Should redraw? Useful for saving redraw cycles if we know component didn't change.
-    fn should_update(&self) -> bool { true }
+    fn should_update(&self) -> bool {
+        true
+    }
 
     fn render(&mut self, surface: &mut Surface, args: ());
 }
 
-// HStack / VStack 
+// HStack / VStack
 // focus by component id: each View/Editor gets it's own incremental id at create
 // Component: View(Arc<State>) -> multiple views can point to same state
 // id 0 = prompt?
