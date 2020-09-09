7eac12a4bb02aaa96f63de612077a12df1d5fedc
Blaž Hrastnik <blaz@mxxn.io>
2020-09-09 14:41:12 +0900

    Add helix-syntax as a wrapper around tree-sitter parsers.

---
+++ helix-syntax/build.rs
@@ -0,0 +1,126 @@
+use cc::Build;
+
+use std::io::{BufWriter, Read, Write};
+use std::path::{Path, PathBuf};
+use std::{env, fs};
+
+fn get_opt_level() -> u32 {
+    env::var("OPT_LEVEL").unwrap().parse::<u32>().unwrap()
+}
+
+fn get_debug() -> bool {
+    env::var("DEBUG").unwrap() == "true"
+}
+
+fn collect_tree_sitter_dirs(ignore: Vec<String>) -> Vec<String> {
+    let mut dirs = Vec::new();
+    for entry in fs::read_dir("languages").unwrap() {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            let dir = path.file_name().unwrap().to_str().unwrap().to_string();
+            if !ignore.contains(&dir) {
+                dirs.push(dir);
+            }
+        }
+    }
+    dirs
+}
+
+fn collect_src_files(dir: &str) -> (Vec<String>, Vec<String>) {
+    eprintln!("Collect files for {}", dir);
+
+    let mut c_files = Vec::new();
+    let mut cpp_files = Vec::new();
+    let path = PathBuf::from("languages").join(&dir).join("src");
+    for entry in fs::read_dir(path).unwrap() {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            if path
+                .file_stem()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .starts_with("binding")
+            {
+                continue;
+            }
+            if let Some(ext) = path.extension() {
+                if ext == "c" {
+                    c_files.push(path.to_str().unwrap().to_string());
+                } else if ext == "cc" || ext == "cpp" || ext == "cxx" {
+                    cpp_files.push(path.to_str().unwrap().to_string());
+                }
+            }
+        }
+    }
+    (c_files, cpp_files)
+}
+
+fn build_c(files: Vec<String>, language: &str) {
+    let mut build = cc::Build::new();
+    for file in files {
+        build
+            .file(&file)
+            .include(PathBuf::from(file).parent().unwrap())
+            .pic(true)
+            .opt_level(get_opt_level())
+            .debug(get_debug())
+            .warnings(false)
+            .flag_if_supported("-std=c99");
+    }
+    build.compile(&format!("tree-sitter-{}-c", language));
+}
+
+fn build_cpp(files: Vec<String>, language: &str) {
+    let mut build = cc::Build::new();
+    for file in files {
+        build
+            .file(&file)
+            .include(PathBuf::from(file).parent().unwrap())
+            .pic(true)
+            .opt_level(get_opt_level())
+            .debug(get_debug())
+            .warnings(false)
+            .cpp(true);
+    }
+    build.compile(&format!("tree-sitter-{}-cpp", language));
+}
+
+fn build_dir(dir: &str, language: &str) {
+    println!("Build language {}", language);
+    if PathBuf::from("languages")
+        .join(dir)
+        .read_dir()
+        .unwrap()
+        .next()
+        .is_none()
+    {
+        eprintln!(
+            "The directory {} is empty, did you use 'git clone --recursive'?",
+            dir
+        );
+        eprintln!("You can fix in using 'git submodule init && git submodule update --recursive'.");
+        std::process::exit(1);
+    }
+    let (c, cpp) = collect_src_files(&dir);
+    if !c.is_empty() {
+        build_c(c, &language);
+    }
+    if !cpp.is_empty() {
+        build_cpp(cpp, &language);
+    }
+}
+
+fn main() {
+    let ignore = vec![
+        "tree-sitter-typescript".to_string(),
+        "tree-sitter-cpp".to_string(),
+    ];
+    let dirs = collect_tree_sitter_dirs(ignore);
+    for dir in dirs {
+        let language = &dir[12..]; // skip tree-sitter- prefix
+        build_dir(&dir, &language);
+    }
+    build_dir("tree-sitter-typescript/tsx", "tsx");
+    build_dir("tree-sitter-typescript/typescript", "typescript");
+}
---
+++ helix-syntax/src/lib.rs
@@ -0,0 +1,87 @@
+use enum_iterator::IntoEnumIterator;
+use tree_sitter::Language;
+
+#[macro_export]
+macro_rules! mk_extern {
+    ( $( $name:ident ),* ) => {
+        $(
+            extern "C" { pub fn $name() -> Language; }
+        )*
+    };
+}
+
+#[macro_export]
+macro_rules! mk_enum {
+    ( $( $camel:ident ),* ) => {
+        #[derive(Clone, Debug, IntoEnumIterator, PartialEq)]
+        pub enum LANG {
+            $(
+                $camel,
+            )*
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! mk_get_language {
+    ( $( ($camel:ident, $name:ident) ),* ) => {
+        pub fn get_language(lang: &LANG) -> Language {
+            unsafe {
+                match lang {
+                    $(
+                        LANG::$camel => $name(),
+                    )*
+                }
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! mk_get_language_name {
+    ( $( $camel:ident ),* ) => {
+        pub fn get_language_name(lang: &LANG) -> &'static str {
+            match lang {
+                $(
+                    LANG::$camel => stringify!($camel),
+                )*
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! mk_langs {
+    ( $( ($camel:ident, $name:ident) ),* ) => {
+        mk_extern!($( $name ),*);
+        mk_enum!($( $camel ),*);
+        mk_get_language!($( ($camel, $name) ),*);
+        mk_get_language_name!($( $camel ),*);
+    };
+}
+
+mk_langs!(
+    // 1) Name for enum
+    // 2) tree-sitter function to call to get a Language
+    (Agda, tree_sitter_agda),
+    (Bash, tree_sitter_bash),
+    (C, tree_sitter_c),
+    (CSharp, tree_sitter_c_sharp),
+    // (Cpp, tree_sitter_cpp),
+    (Css, tree_sitter_css),
+    (Go, tree_sitter_go),
+    (Haskell, tree_sitter_haskell),
+    (Html, tree_sitter_html),
+    (Java, tree_sitter_java),
+    (Javascript, tree_sitter_javascript),
+    (Json, tree_sitter_json),
+    (Julia, tree_sitter_julia),
+    (Php, tree_sitter_php),
+    (Python, tree_sitter_python),
+    (Ruby, tree_sitter_ruby),
+    (Rust, tree_sitter_rust),
+    (Scala, tree_sitter_scala),
+    (Swift, tree_sitter_swift),
+    (Tsx, tree_sitter_tsx),
+    (Typescript, tree_sitter_typescript)
+);
