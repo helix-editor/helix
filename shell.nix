240f3a6cc4af8b51a866770fbbb6771032d10f78
Blaž Hrastnik <blaz@mxxn.io>
2021-01-19 16:52:19 +0900

    nix: Fix execution errors regarding runtime C lib.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,15 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ stdenv, pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     (rust-bin.nightly.latest.rust.override { extensions = ["rust-src"]; })
     lld_10
-    # pkgconfig
+    stdenv.cc.cc.lib
+    # pkg-config
   ];
   RUSTFLAGS = "-C link-arg=-fuse-ld=lld -C target-cpu=native";
   RUST_BACKTRACE = "1";
+  # https://github.com/rust-lang/rust/issues/55979
+  LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
 }
 
