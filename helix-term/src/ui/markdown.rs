80eca5c32f2a9f3345a2449c8620f86423061d33
Blaž Hrastnik <blaz@mxxn.io>
2021-03-15 17:55:13 +0900

    ui: Use parsed markdown contents to determine sizing.

--- helix-term/src/ui/markdown.rs
+++ helix-term/src/ui/markdown.rs
@@ -21,98 +21,101 @@ impl Markdown {
         Self { contents }
     }
 }
+fn parse(contents: &str) -> tui::text::Text {
+    use pulldown_cmark::{CodeBlockKind, CowStr, Event, Options, Parser, Tag};
+    use tui::text::{Span, Spans, Text};
+
+    // also 2021-03-04T16:33:58.553 helix_lsp::transport [INFO] <- {"contents":{"kind":"markdown","value":"\n```rust\ncore::num\n```\n\n```rust\npub const fn saturating_sub(self, rhs:Self) ->Self\n```\n\n---\n\n```rust\n```"},"range":{"end":{"character":61,"line":101},"start":{"character":47,"line":101}}}
+    let text = "\n```rust\ncore::iter::traits::iterator::Iterator\n```\n\n```rust\nfn collect<B: FromIterator<Self::Item>>(self) -> B\nwhere\n        Self: Sized,\n```\n\n---\n\nTransforms an iterator into a collection.\n\n`collect()` can take anything iterable, and turn it into a relevant\ncollection. This is one of the more powerful methods in the standard\nlibrary, used in a variety of contexts.\n\nThe most basic pattern in which `collect()` is used is to turn one\ncollection into another. You take a collection, call [`iter`](https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html) on it,\ndo a bunch of transformations, and then `collect()` at the end.\n\n`collect()` can also create instances of types that are not typical\ncollections. For example, a [`String`](https://doc.rust-lang.org/nightly/core/iter/std/string/struct.String.html) can be built from [`char`](type@char)s,\nand an iterator of [`Result<T, E>`](https://doc.rust-lang.org/nightly/core/result/enum.Result.html) items can be collected\ninto `Result<Collection<T>, E>`. See the examples below for more.\n\nBecause `collect()` is so general, it can cause problems with type\ninference. As such, `collect()` is one of the few times you'll see\nthe syntax affectionately known as the 'turbofish': `::<>`. This\nhelps the inference algorithm understand specifically which collection\nyou're trying to collect into.\n\n# Examples\n\nBasic usage:\n\n```rust\nlet a = [1, 2, 3];\n\nlet doubled: Vec<i32> = a.iter()\n                         .map(|&x| x * 2)\n                         .collect();\n\nassert_eq!(vec![2, 4, 6], doubled);\n```\n\nNote that we needed the `: Vec<i32>` on the left-hand side. This is because\nwe could collect into, for example, a [`VecDeque<T>`](https://doc.rust-lang.org/nightly/core/iter/std/collections/struct.VecDeque.html) instead:\n\n```rust\nuse std::collections::VecDeque;\n\nlet a = [1, 2, 3];\n\nlet doubled: VecDeque<i32> = a.iter().map(|&x| x * 2).collect();\n\nassert_eq!(2, doubled[0]);\nassert_eq!(4, doubled[1]);\nassert_eq!(6, doubled[2]);\n```\n\nUsing the 'turbofish' instead of annotating `doubled`:\n\n```rust\nlet a = [1, 2, 3];\n\nlet doubled = a.iter().map(|x| x * 2).collect::<Vec<i32>>();\n\nassert_eq!(vec![2, 4, 6], doubled);\n```\n\nBecause `collect()` only cares about what you're collecting into, you can\nstill use a partial type hint, `_`, with the turbofish:\n\n```rust\nlet a = [1, 2, 3];\n\nlet doubled = a.iter().map(|x| x * 2).collect::<Vec<_>>();\n\nassert_eq!(vec![2, 4, 6], doubled);\n```\n\nUsing `collect()` to make a [`String`](https://doc.rust-lang.org/nightly/core/iter/std/string/struct.String.html):\n\n```rust\nlet chars = ['g', 'd', 'k', 'k', 'n'];\n\nlet hello: String = chars.iter()\n    .map(|&x| x as u8)\n    .map(|x| (x + 1) as char)\n    .collect();\n\nassert_eq!(\"hello\", hello);\n```\n\nIf you have a list of [`Result<T, E>`](https://doc.rust-lang.org/nightly/core/result/enum.Result.html)s, you can use `collect()` to\nsee if any of them failed:\n\n```rust\nlet results = [Ok(1), Err(\"nope\"), Ok(3), Err(\"bad\")];\n\nlet result: Result<Vec<_>, &str> = results.iter().cloned().collect();\n\n// gives us the first error\nassert_eq!(Err(\"nope\"), result);\n\nlet results = [Ok(1), Ok(3)];\n\nlet result: Result<Vec<_>, &str> = results.iter().cloned().collect();\n\n// gives us the list of answers\nassert_eq!(Ok(vec![1, 3]), result);\n```";
+
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(contents, options);
+
+    // TODO: if possible, render links as terminal hyperlinks: https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda
+    let mut tags = Vec::new();
+    let mut spans = Vec::new();
+    let mut lines = Vec::new();
+
+    fn to_span(text: pulldown_cmark::CowStr) -> Span {
+        use std::ops::Deref;
+        Span::raw::<std::borrow::Cow<_>>(match text {
+            CowStr::Borrowed(s) => s.to_string().into(), // could retain borrow
+            CowStr::Boxed(s) => s.to_string().into(),
+            CowStr::Inlined(s) => s.deref().to_owned().into(),
+        })
+    }
+
+    let text_style = Style::default().fg(Color::Rgb(164, 160, 232)); // lavender
+    let code_style = Style::default().fg(Color::Rgb(255, 255, 255)); // white
+    let heading_style = Style::default().fg(Color::Rgb(219, 191, 239)); // lilac
+
+    for event in parser {
+        match event {
+            Event::Start(tag) => tags.push(tag),
+            Event::End(tag) => {
+                tags.pop();
+                match tag {
+                    Tag::Heading(_) | Tag::Paragraph | Tag::CodeBlock(CodeBlockKind::Fenced(_)) => {
+                        // whenever code block or paragraph closes, new line
+                        let spans = std::mem::replace(&mut spans, Vec::new());
+                        if !spans.is_empty() {
+                            lines.push(Spans::from(spans));
+                        }
+                        lines.push(Spans::default());
+                    }
+                    _ => (),
+                }
+            }
+            Event::Text(text) => {
+                if let Some(Tag::CodeBlock(CodeBlockKind::Fenced(_))) = tags.last() {
+                    for line in text.lines() {
+                        let mut span = Span::styled(line.to_string(), code_style);
+                        lines.push(Spans::from(span));
+                    }
+                } else if let Some(Tag::Heading(_)) = tags.last() {
+                    let mut span = to_span(text);
+                    span.style = heading_style;
+                    spans.push(span);
+                } else {
+                    let mut span = to_span(text);
+                    span.style = text_style;
+                    spans.push(span);
+                }
+            }
+            Event::Code(text) | Event::Html(text) => {
+                let mut span = to_span(text);
+                span.style = code_style;
+                spans.push(span);
+            }
+            Event::SoftBreak | Event::HardBreak => {
+                // let spans = std::mem::replace(&mut spans, Vec::new());
+                // lines.push(Spans::from(spans));
+                spans.push(Span::raw(" "));
+            }
+            Event::Rule => {
+                lines.push(Spans::from("---"));
+                lines.push(Spans::default());
+            }
+            // TaskListMarker(bool) true if checked
+            _ => (),
+        }
+        // build up a vec of Paragraph tui widgets
+    }
+
+    if !spans.is_empty() {
+        lines.push(Spans::from(spans));
+    }
+
+    Text::from(lines)
+}
 impl Component for Markdown {
     fn render(&self, area: Rect, surface: &mut Surface, cx: &mut Context) {
         use tui::widgets::{Paragraph, Widget, Wrap};
 
-        use pulldown_cmark::{CodeBlockKind, CowStr, Event, Options, Parser, Tag};
-        use tui::text::{Span, Spans, Text};
+        let text = parse(&self.contents);
 
-        // also 2021-03-04T16:33:58.553 helix_lsp::transport [INFO] <- {"contents":{"kind":"markdown","value":"\n```rust\ncore::num\n```\n\n```rust\npub const fn saturating_sub(self, rhs:Self) ->Self\n```\n\n---\n\n```rust\n```"},"range":{"end":{"character":61,"line":101},"start":{"character":47,"line":101}}}
-        let text = "\n```rust\ncore::iter::traits::iterator::Iterator\n```\n\n```rust\nfn collect<B: FromIterator<Self::Item>>(self) -> B\nwhere\n        Self: Sized,\n```\n\n---\n\nTransforms an iterator into a collection.\n\n`collect()` can take anything iterable, and turn it into a relevant\ncollection. This is one of the more powerful methods in the standard\nlibrary, used in a variety of contexts.\n\nThe most basic pattern in which `collect()` is used is to turn one\ncollection into another. You take a collection, call [`iter`](https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html) on it,\ndo a bunch of transformations, and then `collect()` at the end.\n\n`collect()` can also create instances of types that are not typical\ncollections. For example, a [`String`](https://doc.rust-lang.org/nightly/core/iter/std/string/struct.String.html) can be built from [`char`](type@char)s,\nand an iterator of [`Result<T, E>`](https://doc.rust-lang.org/nightly/core/result/enum.Result.html) items can be collected\ninto `Result<Collection<T>, E>`. See the examples below for more.\n\nBecause `collect()` is so general, it can cause problems with type\ninference. As such, `collect()` is one of the few times you'll see\nthe syntax affectionately known as the 'turbofish': `::<>`. This\nhelps the inference algorithm understand specifically which collection\nyou're trying to collect into.\n\n# Examples\n\nBasic usage:\n\n```rust\nlet a = [1, 2, 3];\n\nlet doubled: Vec<i32> = a.iter()\n                         .map(|&x| x * 2)\n                         .collect();\n\nassert_eq!(vec![2, 4, 6], doubled);\n```\n\nNote that we needed the `: Vec<i32>` on the left-hand side. This is because\nwe could collect into, for example, a [`VecDeque<T>`](https://doc.rust-lang.org/nightly/core/iter/std/collections/struct.VecDeque.html) instead:\n\n```rust\nuse std::collections::VecDeque;\n\nlet a = [1, 2, 3];\n\nlet doubled: VecDeque<i32> = a.iter().map(|&x| x * 2).collect();\n\nassert_eq!(2, doubled[0]);\nassert_eq!(4, doubled[1]);\nassert_eq!(6, doubled[2]);\n```\n\nUsing the 'turbofish' instead of annotating `doubled`:\n\n```rust\nlet a = [1, 2, 3];\n\nlet doubled = a.iter().map(|x| x * 2).collect::<Vec<i32>>();\n\nassert_eq!(vec![2, 4, 6], doubled);\n```\n\nBecause `collect()` only cares about what you're collecting into, you can\nstill use a partial type hint, `_`, with the turbofish:\n\n```rust\nlet a = [1, 2, 3];\n\nlet doubled = a.iter().map(|x| x * 2).collect::<Vec<_>>();\n\nassert_eq!(vec![2, 4, 6], doubled);\n```\n\nUsing `collect()` to make a [`String`](https://doc.rust-lang.org/nightly/core/iter/std/string/struct.String.html):\n\n```rust\nlet chars = ['g', 'd', 'k', 'k', 'n'];\n\nlet hello: String = chars.iter()\n    .map(|&x| x as u8)\n    .map(|x| (x + 1) as char)\n    .collect();\n\nassert_eq!(\"hello\", hello);\n```\n\nIf you have a list of [`Result<T, E>`](https://doc.rust-lang.org/nightly/core/result/enum.Result.html)s, you can use `collect()` to\nsee if any of them failed:\n\n```rust\nlet results = [Ok(1), Err(\"nope\"), Ok(3), Err(\"bad\")];\n\nlet result: Result<Vec<_>, &str> = results.iter().cloned().collect();\n\n// gives us the first error\nassert_eq!(Err(\"nope\"), result);\n\nlet results = [Ok(1), Ok(3)];\n\nlet result: Result<Vec<_>, &str> = results.iter().cloned().collect();\n\n// gives us the list of answers\nassert_eq!(Ok(vec![1, 3]), result);\n```";
-
-        let mut options = Options::empty();
-        options.insert(Options::ENABLE_STRIKETHROUGH);
-        let parser = Parser::new_ext(&self.contents, options);
-
-        // TODO: if possible, render links as terminal hyperlinks: https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda
-        let mut tags = Vec::new();
-        let mut spans = Vec::new();
-        let mut lines = Vec::new();
-
-        fn to_span(text: pulldown_cmark::CowStr) -> Span {
-            use std::ops::Deref;
-            Span::raw::<std::borrow::Cow<_>>(match text {
-                CowStr::Borrowed(s) => s.into(),
-                CowStr::Boxed(s) => s.to_string().into(),
-                CowStr::Inlined(s) => s.deref().to_owned().into(),
-            })
-        }
-
-        let text_style = Style::default().fg(Color::Rgb(164, 160, 232)); // lavender
-        let code_style = Style::default().fg(Color::Rgb(255, 255, 255)); // white
-        let heading_style = Style::default().fg(Color::Rgb(219, 191, 239)); // lilac
-
-        for event in parser {
-            match event {
-                Event::Start(tag) => tags.push(tag),
-                Event::End(tag) => {
-                    tags.pop();
-                    match tag {
-                        Tag::Heading(_)
-                        | Tag::Paragraph
-                        | Tag::CodeBlock(CodeBlockKind::Fenced(_)) => {
-                            // whenever code block or paragraph closes, new line
-                            let spans = std::mem::replace(&mut spans, Vec::new());
-                            lines.push(Spans::from(spans));
-                            lines.push(Spans::default());
-                        }
-                        _ => (),
-                    }
-                }
-                Event::Text(text) => {
-                    if let Some(Tag::CodeBlock(CodeBlockKind::Fenced(_))) = tags.last() {
-                        for line in text.lines() {
-                            let mut span = Span::styled(line.to_string(), code_style);
-                            lines.push(Spans::from(span));
-                        }
-                    } else if let Some(Tag::Heading(_)) = tags.last() {
-                        let mut span = to_span(text);
-                        span.style = heading_style;
-                        spans.push(span);
-                    } else {
-                        let mut span = to_span(text);
-                        span.style = text_style;
-                        spans.push(span);
-                    }
-                }
-                Event::Code(text) | Event::Html(text) => {
-                    let mut span = to_span(text);
-                    span.style = code_style;
-                    spans.push(span);
-                }
-                Event::SoftBreak | Event::HardBreak => {
-                    // let spans = std::mem::replace(&mut spans, Vec::new());
-                    // lines.push(Spans::from(spans));
-                    spans.push(Span::raw(" "));
-                }
-                Event::Rule => {
-                    lines.push(Spans::from("---"));
-                    lines.push(Spans::default());
-                }
-                // TaskListMarker(bool) true if checked
-                _ => (),
-            }
-            // build up a vec of Paragraph tui widgets
-        }
-
-        if !spans.is_empty() {
-            lines.push(Spans::from(spans));
-        }
-
-        let contents = Text::from(lines);
-
-        let par = Paragraph::new(contents)
+        let par = Paragraph::new(text)
             .wrap(Wrap { trim: false })
             .scroll((cx.scroll.unwrap_or_default() as u16, 0));
 
@@ -121,7 +124,7 @@ impl Component for Markdown {
     }
 
     fn required_size(&mut self, viewport: (u16, u16)) -> Option<(u16, u16)> {
-        let contents = tui::text::Text::from(self.contents.clone());
+        let contents = parse(&self.contents);
         let padding = 2;
         let width = std::cmp::min(contents.width() as u16 + padding, viewport.0);
         let height = std::cmp::min(contents.height() as u16 + padding, viewport.1);
