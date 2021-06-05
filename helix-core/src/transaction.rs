566e2b0e7c3d35f1fc59352309e8353a59a3bd8b
Shafkath Shuhan <shafkathshuhannyc@gmail.com>
2021-06-04 20:40:23 -0400

    fmt

--- helix-core/src/transaction.rs
+++ helix-core/src/transaction.rs
@@ -765,18 +765,12 @@ mod test {
 
         let mut b = ChangeSet::new(&empty);
         b.insert(TEST_CASE.into());
-        
+
         let changes = a.compose(b);
 
         use Operation::*;
-        assert_eq!(
-            changes.changes,
-            &[Insert(TEST_CASE.into())]
-        );
+        assert_eq!(changes.changes, &[Insert(TEST_CASE.into())]);
 
-        assert_eq!(
-            changes.len_after,
-            TEST_CASE.chars().count()
-        );
+        assert_eq!(changes.len_after, TEST_CASE.chars().count());
     }
 }
