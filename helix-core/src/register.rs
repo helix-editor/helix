56d13bf4d0bd7faf4349331c24666684006158c3
Benoît CORTIER <benoit.cortier@fried-world.eu>
2021-06-04 22:21:08 -0400

    Support yanking to system clipboard
    Selection is copied into system clipboard when yanking to register `+`.
    internal register `+` is still updated because we want to keep multicursor
    capabilities when pasting later.
    
    Closes https://github.com/helix-editor/helix/issues/76

--- helix-core/src/register.rs
+++ helix-core/src/register.rs
@@ -6,16 +6,35 @@ use std::{collections::HashMap, sync::RwLock};
 static REGISTRY: Lazy<RwLock<HashMap<char, Vec<String>>>> =
     Lazy::new(|| RwLock::new(HashMap::new()));
 
+const SYSTEM_CLIPBOARD_REGISTER: char = '+';
+
+// TODO: we need a way to hook to system clipboard events to replace "+ register value
+// when appropriate
+pub fn system_clipboard_updated(content: String) {
+    REGISTRY.write().unwrap().insert(SYSTEM_CLIPBOARD_REGISTER, vec![content]);
+}
+
+/// Read register values.
 pub fn get(register: char) -> Option<Vec<String>> {
     let registry = REGISTRY.read().unwrap();
-
     // TODO: no cloning
     registry.get(&register).cloned()
 }
 
+/// Store values into the register.
+/// `+` is a special register interfaced with system clipboard.
 // restoring: bool
 pub fn set(register: char, values: Vec<String>) {
-    let mut registry = REGISTRY.write().unwrap();
+    if register == SYSTEM_CLIPBOARD_REGISTER {
+        let _ = set_clipboard_content(values.join("\n"));
+    }
 
+    let mut registry = REGISTRY.write().unwrap();
     registry.insert(register, values);
 }
+
+fn set_clipboard_content(value: String) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    use copypasta::{ClipboardProvider, ClipboardContext};
+    ClipboardContext::new()?.set_contents(value)
+}
+
