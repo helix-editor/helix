748f97f399a3553f501c7a4fba66dd115e1149f0
Shafkath Shuhan <shafkathshuhannyc@gmail.com>
2021-06-24 17:57:35 -0400

    make conditional
    wip
    
    better conditional
    
    wip
    
    wip
    
    wip
    
    wip
    
    make conditional

--- helix-view/src/clipboard.rs
+++ helix-view/src/clipboard.rs
@@ -76,10 +76,12 @@ pub fn get_clipboard_provider() -> Box<dyn ClipboardProvider> {
             paste => "tmux", "save-buffer", "-";
             copy => "tmux", "load-buffer", "-";
         }
-    } else if cfg!(windows) {
-        Box::new(provider::WindowsProvider)
     } else {
-        Box::new(provider::NopProvider)
+        #[cfg(target_os = "windows")]
+        return Box::new(provider::WindowsProvider);
+
+        #[cfg(not(target_os = "windows"))]
+        return Box::new(provider::NopProvider);
     }
 }
 
@@ -103,7 +105,6 @@ fn is_exit_success(program: &str, args: &[&str]) -> bool {
 mod provider {
     use super::ClipboardProvider;
     use anyhow::{bail, Context as _, Result};
-    use clipboard_win::{formats, get_clipboard, set_clipboard};
     use std::borrow::Cow;
 
     #[derive(Debug)]
@@ -123,21 +124,23 @@ mod provider {
         }
     }
 
+    #[cfg(target_os = "windows")]
     #[derive(Debug)]
     pub struct WindowsProvider;
 
+    #[cfg(target_os = "windows")]
     impl ClipboardProvider for WindowsProvider {
         fn name(&self) -> Cow<str> {
             Cow::Borrowed("Windows Clipboard")
         }
 
         fn get_contents(&self) -> Result<String> {
-            let contents = get_clipboard(formats::Unicode)?;
+            let contents = clipboard_win::get_clipboard(clipboard_win::formats::Unicode)?;
             Ok(contents)
         }
 
         fn set_contents(&self, contents: String) -> Result<()> {
-            set_clipboard(formats::Unicode, contents)?;
+            clipboard_win::set_clipboard(clipboard_win::formats::Unicode, contents)?;
             Ok(())
         }
     }
