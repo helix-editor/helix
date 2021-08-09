6f416de69e5ca1d039f36e8c910cc9a4ff0d1829
Shafkath Shuhan <shafkathshuhannyc@gmail.com>
2021-08-08 20:15:02 -0400

    fix small terminal size panic with info popup

--- helix-term/src/ui/info.rs
+++ helix-term/src/ui/info.rs
@@ -7,24 +7,33 @@ use tui::widgets::{Block, Borders, Widget};
 impl Component for Info {
     fn render(&self, viewport: Rect, surface: &mut Surface, cx: &mut Context) {
         let style = cx.editor.theme.get("ui.popup");
+
+        // Calculate the area of the terminal to modify. Because we want to
+        // render at the bottom right, we use the viewport's width and height
+        // which evaluate to the most bottom right coordinate.
+        let (width, height) = (self.width + 2, self.height + 2);
+        let area = viewport.intersection(Rect::new(
+            viewport.width.saturating_sub(width),
+            viewport.height.saturating_sub(height + 2),
+            width,
+            height,
+        ));
+        surface.clear_with(area, style);
+
         let block = Block::default()
             .title(self.title.as_str())
             .borders(Borders::ALL)
             .border_style(style);
-        let Info { width, height, .. } = self;
-        let (w, h) = (*width + 2, *height + 2);
-        // -2 to subtract command line + statusline. a bit of a hack, because of splits.
-        let area = viewport.intersection(Rect::new(
-            viewport.width.saturating_sub(w),
-            viewport.height.saturating_sub(h + 2),
-            w,
-            h,
-        ));
-        surface.clear_with(area, style);
-        let Rect { x, y, .. } = block.inner(area);
-        for (y, line) in (y..).zip(self.text.lines()) {
-            surface.set_string(x, y, line, style);
-        }
+        let inner = block.inner(area);
         block.render(area, surface);
+
+        // Only write as many lines as there are rows available.
+        for (_, (y, line)) in (inner.y..)
+            .zip(self.text.lines())
+            .take(inner.height as usize)
+            .enumerate()
+        {
+            surface.set_string(inner.x, y, line, style);
+        }
     }
 }
