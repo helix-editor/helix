d07c8b20c78026ffe608b8542a3fd178ab763c41
orhun <orhun@archlinux.org>
2021-08-17 00:11:14 +0300

    Compile the grammar libraries with full RELRO

--- helix-syntax/build.rs
+++ helix-syntax/build.rs
@@ -105,6 +105,7 @@ fn build_library(src_path: &Path, language: &str) -> Result<()> {
             }
         }
         command.arg("-xc").arg(parser_path);
+        command.arg("-Wl,-z,relro,-z,now");
     }
 
     let output = command
