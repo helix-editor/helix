b590504143412f185f02ee21a54ad4e6d966b257
gbaranski <root@gbaranski.com>
2021-08-30 20:28:40 +0200

    fix: use head instead of anchor for relative line

--- helix-term/src/ui/editor.rs
+++ helix-term/src/ui/editor.rs
@@ -427,7 +427,7 @@ impl EditorView {
 
         let current_line = doc
             .text()
-            .char_to_line(doc.selection(view.id).primary().anchor);
+            .char_to_line(doc.selection(view.id).primary().head);
 
         // it's used inside an iterator so the collect isn't needless:
         // https://github.com/rust-lang/rust-clippy/issues/6164
