4f756c3e48cdb3126e1d5bc536e1efc46a596f8a
Brian Shu <littlebubu.shu@gmail.com>
2021-09-01 12:07:34 -0400

    remove

--- helix-view/src/macros.rs
+++ helix-view/src/macros.rs
@@ -6,32 +6,6 @@
 //! this circumvents the problem because it is just like indexing fields by hand and then
 //! putting a &mut in front of it. This way rust can see that we are only borrowing a
 //! part of the struct and not the entire thing.
-//!
-//! In this example the fields of the struct are not nested and easy to index by hand so the helper
-//! functions are not useful, they are just to show the ownership issues.
-//! ```ignore
-//! struct Test {
-//!     field1: u32,
-//!     field2: u32,
-//! }
-//!
-//! impl Test {
-//!     fn mut_field1(&mut self) -> &mut u32 {
-//!         &mut self.field1
-//!     }
-//!
-//!     fn mut_field2(&mut self) -> &mut u32 {
-//!         &mut self.field2
-//!     }
-//! }
-//!
-//! 
-//! let test = Test { field1: 1, field2: 2 }
-//! let field1 = test.mut_field1();
-//!
-//! // uh-oh
-//! let field2 = test.mut_field2();
-//! ```
 
 #[macro_export]
 macro_rules! current {
