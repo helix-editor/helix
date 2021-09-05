99a753a5797d38885560e9026b86952615032556
oberblastmeister <61095988+oberblastmeister@users.noreply.github.com>
2021-09-04 23:42:33 -0400

    Document macros (#693)
    * add docs
    
    * clean up
    
    * remove
    
    * more
    
    * Update helix-view/src/macros.rs
    
    Co-authored-by: Ivan Tham <pickfire@riseup.net>
    
    Co-authored-by: Ivan Tham <pickfire@riseup.net>

--- helix-view/src/macros.rs
+++ helix-view/src/macros.rs
@@ -1,3 +1,14 @@
+//! These are macros to make getting very nested fields in the `Editor` struct easier
+//! These are macros instead of functions because functions will have to take `&mut self`
+//! However, rust doesn't know that you only want a partial borrow instead of borrowing the
+//! entire struct which `&mut self` says.  This makes it impossible to do other mutable
+//! stuff to the struct because it is already borrowed. Because macros are expanded,
+//! this circumvents the problem because it is just like indexing fields by hand and then
+//! putting a `&mut` in front of it. This way rust can see that we are only borrowing a
+//! part of the struct and not the entire thing.
+
+/// Get the current view and document mutably as a tuple.
+/// Returns `(&mut View, &mut Document)`
 #[macro_export]
 macro_rules! current {
     ( $( $editor:ident ).+ ) => {{
@@ -7,6 +18,8 @@ macro_rules! current {
     }};
 }
 
+/// Get the current document mutably.
+/// Returns `&mut Document`
 #[macro_export]
 macro_rules! doc_mut {
     ( $( $editor:ident ).+ ) => {{
@@ -14,6 +27,8 @@ macro_rules! doc_mut {
     }};
 }
 
+/// Get the current view mutably.
+/// Returns `&mut View`
 #[macro_export]
 macro_rules! view_mut {
     ( $( $editor:ident ).+ ) => {{
@@ -21,6 +36,8 @@ macro_rules! view_mut {
     }};
 }
 
+/// Get the current view immutably
+/// Returns `&View`
 #[macro_export]
 macro_rules! view {
     ( $( $editor:ident ).+ ) => {{
