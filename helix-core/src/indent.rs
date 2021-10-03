59ce668afd247b9f0a121d596ed2f9d7403897c8
cossonleo <cossonleo@foxmail.com>
2021-10-03 17:30:53 +0800

    fix test

--- helix-core/src/indent.rs
+++ helix-core/src/indent.rs
@@ -458,6 +458,7 @@ where
                 roots: vec![],
                 comment_token: None,
                 auto_format: false,
+                completion_punctuation: vec![".".into()],
                 language_server: None,
                 indent: Some(IndentationConfiguration {
                     tab_width: 4,
