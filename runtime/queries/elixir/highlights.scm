b2655a7f5ca0edfffcaabb8ab84b53571afba19a
Michael Davis <mcarsondavis@gmail.com>
2021-10-11 21:59:07 -0500

    add LICENSE snippet at elixir hightlights top

--- runtime/queries/elixir/highlights.scm
+++ runtime/queries/elixir/highlights.scm
@@ -1,3 +1,20 @@
+; The following code originates mostly from
+; https://github.com/elixir-lang/tree-sitter-elixir, with minor edits to
+; align the captures with helix. The following should be considered
+; Copyright 2021 The Elixir Team
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;    https://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
 ; Reserved keywords
 
 ["when" "and" "or" "not" "in" "fn" "do" "end" "catch" "rescue" "after" "else"] @keyword
