7f84bd6a770ccfffb617a04f2214f958aea0f734
Michael Davis <mcarsondavis@gmail.com>
2021-10-18 07:38:23 -0500

    clarify comment on combined

--- runtime/queries/heex/injections.scm
+++ runtime/queries/heex/injections.scm
@@ -1,9 +1,8 @@
 ((expression_value) @injection.content
  (#set! injection.language "elixir"))
 
-; combined injection does not appear to work as expected,
-; see: https://github.com/nvim-treesitter/nvim-treesitter/blob/58dd95f4a4db38a011c8f28564786c9d98b010c8/queries/heex/injections.scm#L1
-; once combined injections works with this grammar, the following rules should be used instead:
+; combined injection does not appear to work as expected yet
+; once combined injections work, these rules should be used instead of the above
 
 ; directives are standalone tags like <%= %>
 ; ((directive (expression_value) @injection.content)
