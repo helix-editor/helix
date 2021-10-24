883d45f4511578b583413a90406a749297100125
Shafkath Shuhan <shafkathshuhannyc@gmail.com>
2021-10-24 12:47:42 -0400

    doc

--- helix-core/src/diagnostic.rs
+++ helix-core/src/diagnostic.rs
@@ -1,3 +1,6 @@
+//! LSP diagnostic utility types.
+
+/// Describes the severity level of a [`Diagnostic`].
 #[derive(Debug, Eq, PartialEq)]
 pub enum Severity {
     Error,
@@ -6,12 +9,14 @@ pub enum Severity {
     Hint,
 }
 
+/// A range of `char`s within the text.
 #[derive(Debug)]
 pub struct Range {
     pub start: usize,
     pub end: usize,
 }
 
+/// Corresponds to [`lsp_types::Diagnostic`](https://docs.rs/lsp-types/0.91.0/lsp_types/struct.Diagnostic.html)
 #[derive(Debug)]
 pub struct Diagnostic {
     pub range: Range,
