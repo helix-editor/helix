c7a59e24e6e1609dd65e5ee66fef52f76aabe9b7
Ivan Tham <pickfire@riseup.net>
2021-12-12 21:09:01 +0800

    Switch macro Q and q

--- helix-term/src/keymap.rs
+++ helix-term/src/keymap.rs
@@ -593,8 +593,8 @@ impl Default for Keymaps {
             // paste_all
             "P" => paste_before,
 
-            "q" => record_macro,
-            "Q" => play_macro,
+            "Q" => record_macro,
+            "q" => play_macro,
 
             ">" => indent,
             "<" => unindent,
