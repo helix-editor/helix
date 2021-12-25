4b0b1a5657b78693efe609647360de30264fcc92
Matouš Dzivjak <matousdzivjak@gmail.com>
2021-12-25 16:10:46 +0100

    feat(ui): file encoding in statusline (#1355)
    * feat(ui): file encoding in statusline
    
    Display file encoding in statusline if the encoding
    isn't UTF-8.
    
    * Re-export encoding_rs from core
    
    From there it can be imported by other mods
    that rely on it.

--- helix-term/src/ui/editor.rs
+++ helix-term/src/ui/editor.rs
@@ -7,7 +7,7 @@ use crate::{
 };
 
 use helix_core::{
-    coords_at_pos,
+    coords_at_pos, encoding,
     graphemes::{ensure_grapheme_boundary_next, next_grapheme_boundary, prev_grapheme_boundary},
     movement::Direction,
     syntax::{self, HighlightEvent},
@@ -620,6 +620,13 @@ impl EditorView {
             format!(" {}:{} ", pos.row + 1, pos.col + 1), // Convert to 1-indexing.
             base_style,
         ));
+
+        let enc = doc.encoding();
+        if enc != encoding::UTF_8 {
+            right_side_text
+                .0
+                .push(Span::styled(format!(" {} ", enc.name()), base_style));
+        }
 
         // Render to the statusline.
         surface.set_spans(
