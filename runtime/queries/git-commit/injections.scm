73c514f1722ee570abcc23171d3da3b70e24aaca
Michael Davis <mcarsondavis@gmail.com>
2021-12-24 18:57:48 -0600

    inject git-diff into git-commit

--- runtime/queries/git-commit/injections.scm
+++ runtime/queries/git-commit/injections.scm
@@ -1,12 +1,6 @@
-; once a diff grammar is available, we can inject diff highlighting into the
-; trailer after scissors (git commit --verbose)
-; see https://github.com/helix-editor/helix/pull/1338#issuecomment-1000013539
-;
-; ((comment (scissors))
-;  (message) @injection.content
-;  (#set! injection.language "diff"))
-
-; ---
+((comment (scissors))
+ (message) @injection.content
+ (#set! injection.language "diff"))
 
 ; once a rebase grammar is available, we can inject rebase highlighting into
 ; interactive rebase summary sections like so:
