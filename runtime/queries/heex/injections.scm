f56ab4cdf554696dd771d236844aa22d2cf606dc
Michael Davis <mcarsondavis@gmail.com>
2022-01-02 08:42:20 -0600

    use combined injections queries for directive nodes

--- runtime/queries/heex/injections.scm
+++ runtime/queries/heex/injections.scm
@@ -1,15 +1,9 @@
-((expression_value) @injection.content
- (#set! injection.language "elixir"))
-
-; combined injection does not appear to work as expected yet
-; once combined injections work, these rules should be used instead of the above
-
-; directives are standalone tags like <%= %>
-; ((directive (expression_value) @injection.content)
-;  (#set! injection.language "elixir")
-;  (#set! injection.combined true))
+; directives are standalone tags like <%= .. %>
+((directive (expression_value) @injection.content)
+ (#set! injection.language "elixir")
+ (#set! injection.combined))
 
 ; expressions live within HTML tags
 ;     <link href={ Routes.static_path(..) } />
-; ((expression (expression_value) @injection.content)
-;  (#set! injection.language "elixir"))
+((expression (expression_value) @injection.content)
+ (#set! injection.language "elixir"))
