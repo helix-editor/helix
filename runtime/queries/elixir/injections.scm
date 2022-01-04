aa2d9005b44ea153fe0daef1f677832ff0e9aaab
Michael Davis <mcarsondavis@gmail.com>
2022-01-03 21:13:03 -0600

    set elixir regex injections as combined

--- runtime/queries/elixir/injections.scm
+++ runtime/queries/elixir/injections.scm
@@ -5,4 +5,5 @@
   (sigil_name) @_sigil_name
   (quoted_content) @injection.content)
  (#match? @_sigil_name "^(r|R)$")
- (#set! injection.language "regex"))
+ (#set! injection.language "regex")
+ (#set! injection.combined))
