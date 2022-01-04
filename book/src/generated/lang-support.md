641255ccc83648d164bf6e8e8e4e93460591830b
Sebastian Neubauer <Flakebi@users.noreply.github.com>
2022-01-04 02:52:34 +0100

    Add llvm-mir highlighting (#1398)
    * Add injection regex for more languages
    
    To support embedding them in other languages like markdown.
    
    * Add llvm-mir highlighting
    
    LLVM Machine IR is dumped as yaml files that can embed LLVM IR and
    Machine IR.
    
    To support this, add a llvm-mir-yaml language that uses the yaml
    parser, but uses different injections to highlight IR and MIR.
    
    * Update submodule with fixed multiline comments
    
    Co-authored-by: Blaž Hrastnik <blaz@mxxn.io>

--- book/src/generated/lang-support.md
+++ book/src/generated/lang-support.md
@@ -24,6 +24,8 @@
 | latex | ✓ |  |  |  |
 | ledger | ✓ |  |  |  |
 | llvm | ✓ | ✓ | ✓ |  |
+| llvm-mir | ✓ | ✓ | ✓ |  |
+| llvm-mir-yaml | ✓ |  | ✓ |  |
 | lua | ✓ |  | ✓ |  |
 | markdown | ✓ |  |  |  |
 | mint |  |  |  | `mint` |
--- helix-core/src/indent.rs
+++ helix-core/src/indent.rs
@@ -436,6 +436,7 @@ where
                 comment_token: None,
                 auto_format: false,
                 diagnostic_severity: Severity::Warning,
+                tree_sitter_library: None,
                 language_server: None,
                 indent: Some(IndentationConfiguration {
                     tab_width: 4,
