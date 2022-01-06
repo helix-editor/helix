a8fd33ac012a79069ef1409503a2edcf3a585153
Michael Davis <mcarsondavis@gmail.com>
2022-01-06 09:00:00 -0600

    add tree-sitter-regex (#1362)
    * add tree-sitter-regex
    
    * adapt regex highlights from upstream
    
    * inject regex into elixir sigil_r/2 and sigil_R/2
    
    * generate lang-support docs
    
    * capture interesting nodes in character-ranges
    
    * make $.character_class captures more consistent
    
    * fix fallthrough behavior for character classes
    
    * capture pattern characters as 'string'
    
    * use latest tree-sitter-regex
    
    * set elixir regex injections as combined
    
    * add link to upstream queries
    
    * inject regex in rust into 'Regex::new' raw string literals

--- runtime/queries/rust/injections.scm
+++ runtime/queries/rust/injections.scm
@@ -10,3 +10,17 @@
   (token_tree) @injection.content)
  (#set! injection.language "rust")
  (#set! injection.include-children))
+
+(call_expression
+  function: (scoped_identifier
+    path: (identifier) @_regex (#eq? @_regex "Regex")
+    name: (identifier) @_new (#eq? @_new "new"))
+  arguments: (arguments (raw_string_literal) @injection.content)
+  (#set! injection.language "regex"))
+
+(call_expression
+  function: (scoped_identifier
+    path: (scoped_identifier (identifier) @_regex (#eq? @_regex "Regex") .)
+    name: (identifier) @_new (#eq? @_new "new"))
+  arguments: (arguments (raw_string_literal) @injection.content)
+  (#set! injection.language "regex"))
