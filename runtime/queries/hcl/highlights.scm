c1643977b09802a123c4e510d4deb76aaf4413e1
Matouš Dzivjak <matousdzivjak@gmail.com>
2022-01-13 18:37:09 +0100

    Update highlight

--- runtime/queries/hcl/highlights.scm
+++ runtime/queries/hcl/highlights.scm
@@ -1,10 +1,29 @@
-; highlights.scm
+; { key: val }
+;
+; highlight identifier keys as though they were block attributes
+; (object_elem (identifier) () (expression (variable_expr (identifier) @field)))
 
-(ERROR) @error
+; (object_elem val: (expression
+;   (variable_expr
+;     (identifier) @type.builtin (#any-of? @type.builtin "bool" "string" "number" "object" "tuple" "list" "map" "set" "any"))))
 
-(object_elem val: (expression
-  (variable_expr
-    (identifier) @type.builtin (#any-of? @type.builtin "bool" "string" "number" "object" "tuple" "list" "map" "set" "any"))))
+(get_attr (identifier) @variable.builtin (#any-of? @variable.builtin  "root" "cwd" "module"))
+((identifier) @keyword (#any-of? @keyword "module" "root" "cwd" "resource" "variable" "data" "locals" "terraform" "provider" "output"))
+((identifier) @type.builtin (#any-of? @type.builtin "bool" "string" "number" "object" "tuple" "list" "map" "set" "any"))
+(variable_expr (identifier) @variable.builtin (#any-of? @variable.builtin "var" "local" "path"))
+
+(attribute (identifier) @field)
+(function_call (identifier) @function)
+(block (identifier) @type)
+
+[
+  (true)
+  (false)
+]  @boolean
+
+(null) @constant
+(comment) @comment
+(identifier) @variable
 
 [
   "!"
@@ -34,14 +53,11 @@
 
 [
   "."
-  ".*"
   ","
-  "[*]"
 ] @punctuation.delimiter
 
 [
-  (ellipsis)
-  "\?"
+  "?"
   "=>"
 ] @punctuation.special
 
@@ -52,52 +68,17 @@
 
 [
   "for"
-  "endfor"
   "in"
 ] @repeat
 
 [ 
+  (conditional)
   "if"
-  "else"
-  "endif"
 ] @conditional
 
 [
-  (quoted_template_start) ; "
-  (quoted_template_end); "
-  (template_literal) ; non-interpolation/directive content
+  (string_literal)
+  (quoted_template)
 ] @string
 
-[
-  (heredoc_identifier) ; <<END
-  (heredoc_start) ; END
-] @punctuation.delimiter
-
-[
-  (template_interpolation_start) ; ${
-  (template_interpolation_end) ; }
-  (template_directive_start) ; %{
-  (template_directive_end) ; }
-  (strip_marker) ; ~
-] @punctuation.special
-
-(numeric_lit) @number
-(bool_lit) @boolean
-(null_lit) @constant
-(comment) @comment
-(identifier) @variable
-
-(block (identifier) @type)
-(function_call (identifier) @function)
-(attribute (identifier) @field)
-
-; { key: val }
-;
-; highlight identifier keys as though they were block attributes
-(object_elem key: (expression (variable_expr (identifier) @field)))
-
-((identifier) @keyword (#any-of? @keyword "module" "root" "cwd" "resource" "variable" "data" "locals" "terraform" "provider" "output"))
-((identifier) @type.builtin (#any-of? @type.builtin "bool" "string" "number" "object" "tuple" "list" "map" "set" "any"))
-(variable_expr (identifier) @variable.builtin (#any-of? @variable.builtin "var" "local" "path"))
-(get_attr (identifier) @variable.builtin (#any-of? @variable.builtin  "root" "cwd" "module"))
-
+(escape_sequence) @punctuation.special
