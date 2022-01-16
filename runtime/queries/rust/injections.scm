facf9ba99639c9193a15e8fed92df77ef3cda506
Ivan Tham <pickfire@riseup.net>
2022-01-16 22:55:25 +0800

    Add rust injections for c, c++, html, js, py

--- runtime/queries/rust/injections.scm
+++ runtime/queries/rust/injections.scm
@@ -1,14 +1,52 @@
 ([(line_comment) (block_comment)] @injection.content
  (#set! injection.language "comment"))
 
-((macro_invocation
+((macro_rule
   (token_tree) @injection.content)
  (#set! injection.language "rust")
  (#set! injection.include-children))
 
-((macro_rule
+; inline_c::assert_c
+((macro_invocation
+  macro: (identifier) @_c (#eq? @_c "assert_c")
   (token_tree) @injection.content)
- (#set! injection.language "rust")
+ (#set! injection.language "c")
+ (#set! injection.include-children))
+
+; inline_c::assert_cxx
+((macro_invocation
+  macro: (identifier) @_cxx (#eq? @_cxx "assert_cxx")
+  (token_tree) @injection.content)
+ (#set! injection.language "cpp")
+ (#set! injection.include-children))
+
+; typed_html::html
+((macro_invocation
+  macro: (identifier) @_html (#eq? @_html "html")
+  (token_tree) @injection.content)
+ (#set! injection.language "html")
+ (#set! injection.include-children))
+
+; inline_python::python
+; TODO: fix bug where first import is not highlighted
+((macro_invocation
+  macro: (scoped_identifier
+    name: (identifier) @_python (#eq? @_python "python"))
+  (token_tree) @injection.content)
+ (#set! injection.language "python")
+ (#set! injection.include-children))
+
+((macro_invocation
+  macro: (identifier) @_python (#eq? @_python "python")
+  (token_tree) @injection.content)
+ (#set! injection.language "python")
+ (#set! injection.include-children))
+
+; embed_js::inline_js
+((macro_invocation
+  macro: (identifier) @_js (#eq? @_js "include_js")
+  (token_tree) @injection.content)
+ (#set! injection.language "javascript")
  (#set! injection.include-children))
 
 (call_expression
