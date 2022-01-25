f8467b05ffd4ead27adb9f632c92ee926f8d6f3f
Michael Davis <mcarsondavis@gmail.com>
2022-01-25 09:29:48 -0600

    draft batch script for grammars

---
+++ scripts/grammars.cmd
@@ -0,0 +1,54 @@
+@echo off
+
+set GRAMMARS_DIR=%~dp0..\helix-syntax\languages
+set REVISIONS_FILE=%~dp0\revisions.txt
+set REMOTE_NAME=helix-origin
+
+call :do_%1% 2>nul
+if errorlevel 1 call :print_usage_and_exit
+exit /B
+
+:print_usage_and_exit
+  echo Usage: %~f0 ^<command^>
+  echo.
+  echo Commands:
+  echo   status  Checks that each grammar is checked out at the revision in revisions.txt
+  echo   sync    Ensures all grammars are cloned at the revisions in revisions.txt
+  echo   clean   Removes all grammars from the grammars directory
+  echo.
+exit /B 1
+
+:ensure_grammar_fetched
+  setlocal
+  set "grammar_dir=%GRAMMARS_DIR%\%~nx1"
+  set remote_url=%~1
+  set revision=%~2
+
+  if not exist %grammar_dir% (
+    mkdir %grammar_dir%
+  )
+
+  pushd %grammar_dir%
+  if not exist .git (
+    git init
+  )
+  git remote set-url %REMOTE_NAME% %remote_url% 2>NUL
+  if errorlevel 1 git remote add %REMOTE_NAME% %remote_url%
+  git fetch %REMOTE_NAME% %revision% --depth=1
+  git checkout %revision%
+  popd
+exit /B 0
+
+:do_clean
+  pushd %GRAMMARS_DIR%
+  for /D %%d in (tree-sitter-*) do (
+    rmdir /s /q %%d
+  )
+  popd
+exit /B 0
+
+:do_sync
+  for /F "tokens=1,2" %%i in (%REVISIONS_FILE%) do (
+    call :ensure_grammar_fetched %%i %%j
+  )
+exit /B
