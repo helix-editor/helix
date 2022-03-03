274b882815857a760604892c0bbd4035609df494
Gokul Soumya <gokulps15@gmail.com>
2022-03-03 22:33:26 +0530

    Rename infobox theme scopes
    This makes it play nicely with https://github.com/helix-editor/helix/pull/1714

--- helix-term/src/ui/info.rs
+++ helix-term/src/ui/info.rs
@@ -6,12 +6,8 @@ use tui::widgets::{Block, Borders, Paragraph, Widget};
 
 impl Component for Info {
     fn render(&mut self, viewport: Rect, surface: &mut Surface, cx: &mut Context) {
-        let get_theme = |style, fallback| {
-            let theme = &cx.editor.theme;
-            theme.try_get(style).unwrap_or_else(|| theme.get(fallback))
-        };
-        let text_style = get_theme("ui.info.text", "ui.text");
-        let popup_style = text_style.patch(get_theme("ui.info", "ui.popup"));
+        let text_style = cx.editor.theme.get("ui.text.info");
+        let popup_style = cx.editor.theme.get("ui.popup.info");
 
         // Calculate the area of the terminal to modify. Because we want to
         // render at the bottom right, we use the viewport's width and height
