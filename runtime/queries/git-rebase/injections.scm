6f197216964a4cd41693fdd4b8353362fd9d77e4
Michael Davis <mcarsondavis@gmail.com>
2022-03-06 14:47:50 -0600

    update tree-sitter-git-rebase
    changes:
    
    * the grammar has been completely rewritten in a way that removes
      some of the "jumpiness" of the highlights
        * when changing a command (for example from 'pick' to 'drop'),
          highlighting would treat parts of the commit hash as a command

--- runtime/queries/git-rebase/injections.scm
+++ runtime/queries/git-rebase/injections.scm
@@ -1,4 +1,4 @@
-((operation
-   operator: ["x" "exec"]
-   (command) @injection.content)
+(((command) @__command__
+  (message) @injection.content)
+ (#match? @__command__ "^(x|exec)$")
  (#set! injection.language "bash"))
