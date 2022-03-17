0b9620108dabe6a26fbeac32ba9d236b6d69b56a
Blaž Hrastnik <blaz@mxxn.io>
2022-03-17 12:03:15 +0900

    ropey 1.4.1 fixes the issue

--- helix-core/src/line_ending.rs
+++ helix-core/src/line_ending.rs
@@ -300,9 +300,8 @@ mod line_ending_tests {
     fn line_end_char_index_rope_slice() {
         let r = Rope::from_str("Hello\rworld\nhow\r\nare you?");
         let s = &r.slice(..);
-        assert_eq!(line_end_char_index(s, 0), 5);
-        assert_eq!(line_end_char_index(s, 1), 11);
-        assert_eq!(line_end_char_index(s, 2), 15);
-        assert_eq!(line_end_char_index(s, 3), 25);
+        assert_eq!(line_end_char_index(s, 0), 11);
+        assert_eq!(line_end_char_index(s, 1), 15);
+        assert_eq!(line_end_char_index(s, 2), 25);
     }
 }
