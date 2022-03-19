5881b694c279213e01df5f4896fe604267dad9b9
zetashift <rskaraya@gmail.com>
2022-03-19 23:57:38 +0100

    Inital basic Org markup support thanks to tree-sitter-org

---
+++ runtime/queries/org/highlights.scm
@@ -0,0 +1,19 @@
+(headline (stars) @markup.heading.marker (#eq? @markup.heading.marker "*")) @markup.heading.1
+(headline (stars) @markup.heading.marker (#eq? @markup.heading.marker "**")) @markup.heading.2
+(headline (stars) @markup.heading.marker (#eq? @markup.heading.marker "***")) @markup.heading.3
+(headline (stars) @markup.heading.marker (#eq? @markup.heading.marker "****")) @markup.heading.4
+(headline (stars) @markup.heading.marker (#eq? @markup.heading.marker "*****")) @markup.heading.5
+(headline (stars) @markup.heading.marker (#eq? @markup.heading.marker "******")) @markup.heading.6
+
+ (block) @markup.raw.block
+ (list) @markup.list.unnumbered
+ (property_drawer) @label
+
+ ((expr) @markup.bold
+  (#match? @markup.bold "\\*.*\\*"))
+ 
+ ((expr) @markup.italic
+  (#match? @markup.italic "/.*/"))
+
+ ((expr) @markup.raw.inline
+  (#match? @markup.raw.inline "~.*~"))
