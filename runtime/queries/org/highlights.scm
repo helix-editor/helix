a5b00e53ada410c2eb45ddb6b7e001f5fc1c1e7a
zetashift <rskaraya@gmail.com>
2022-03-20 18:26:28 +0100

    Fix formatting

--- runtime/queries/org/highlights.scm
+++ runtime/queries/org/highlights.scm
@@ -5,20 +5,19 @@
 (headline (stars) @markup.heading.marker (#eq? @markup.heading.marker "*****")) @markup.heading.5
 (headline (stars) @markup.heading.marker (#eq? @markup.heading.marker "******")) @markup.heading.6
 
- (block) @markup.raw.block
- (list) @markup.list.unnumbered
- (directive) @markup.label
- (property_drawer) @markup.label
+(block) @markup.raw.block
+(list) @markup.list.unnumbered
+(directive) @markup.label
+(property_drawer) @markup.label
  
 
- ((expr) @markup.bold
-  (#match? @markup.bold "\\*.*\\*"))
- 
- ((expr) @markup.italic
-  (#match? @markup.italic "/.*/"))
+((expr) @markup.bold
+ (#match? @markup.bold "\\*.*\\*"))
 
- ((expr) @markup.raw.inline
-  (#match? @markup.raw.inline "~.*~"))
- 
- ((expr) @markup.quote
-  (#match? @markup.quote "=.*="))
+((expr) @markup.italic
+ (#match? @markup.italic "/.*/"))
+((expr) @markup.raw.inline
+ (#match? @markup.raw.inline "~.*~"))
+
+((expr) @markup.quote
+ (#match? @markup.quote "=.*="))
