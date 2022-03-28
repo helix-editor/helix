c572bdfb4f0a571c65dc87e601280734387a66bb
Michael Davis <mcarsondavis@gmail.com>
2022-03-28 08:18:03 -0500

    publish a source tarball with version and grammars

--- helix-term/build.rs
+++ helix-term/build.rs
@@ -1,8 +1,18 @@
 use helix_loader::grammar::{build_grammars, fetch_grammars};
 use std::borrow::Cow;
+use std::io::Read;
 use std::process::Command;
 
 fn main() {
+    let mut version = String::new();
+
+    if std::fs::File::open("../.version")
+        .and_then(|mut f| f.read_to_string(&mut version))
+        .is_err()
+    {
+        version = "dev".to_string();
+    }
+
     let git_hash = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output()
@@ -11,8 +21,8 @@ fn main() {
         .and_then(|x| String::from_utf8(x.stdout).ok());
 
     let version: Cow<_> = match git_hash {
-        Some(git_hash) => format!("{} ({})", env!("CARGO_PKG_VERSION"), &git_hash[..8]).into(),
-        None => env!("CARGO_PKG_VERSION").into(),
+        Some(git_hash) => format!("{} ({})", version, &git_hash[..8]).into(),
+        None => version.into(),
     };
 
     if std::env::var("HELIX_DISABLE_AUTO_GRAMMAR_BUILD").is_err() {
@@ -21,6 +31,7 @@ fn main() {
     }
 
     println!("cargo:rerun-if-changed=../runtime/grammars/");
+    println!("cargo:rerun-if-changed=../.version");
 
     println!("cargo:rustc-env=VERSION_AND_GIT_HASH={}", version);
 }
