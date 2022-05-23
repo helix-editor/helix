10463fe32c789f49c65d53c9d22621e415a854ea
Andrey Tkachenko <andrey@aidev.ru>
2022-05-23 16:24:30 +0400

    Add `parameter.around` text object query

--- runtime/queries/php/textobjects.scm
+++ runtime/queries/php/textobjects.scm
@@ -21,13 +21,19 @@
   
 (anonymous_function_creation_expression
   body: (_) @function.inside) @function.around
-  
+
+(anonymous_function_use_clause
+  ((_) @parameter.inside . ","? @parameter.around) @parameter.around)
+
 (formal_parameters
-  [
+  ([
     (simple_parameter)
     (variadic_parameter)
     (property_promotion_parameter)
-  ] @parameter.inside)
+  ] @parameter.inside . ","? @parameter.around) @parameter.around)
+
+(arguments
+  ((_) @parameter.inside . ","? @parameter.around) @parameter.around)
 
 (comment) @comment.inside
 
