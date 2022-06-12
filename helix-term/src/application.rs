c6e9ecd27701f93ab916f6e19ad932923387e432
Bjorn Ove Hay Andersen <bjrnove@gmail.com>
2022-06-12 08:43:03 +0200

    Moved editor.refresh_config() to cover the case of

--- helix-term/src/application.rs
+++ helix-term/src/application.rs
@@ -287,6 +287,10 @@ impl Application {
                 self.config.store(Arc::new(app_config));
             }
         }
+
+        // Update all the relevant members in the editor after updating
+        // the configuration.
+        self.editor.refresh_config();
     }
 
     fn refresh_config(&mut self) {
@@ -318,10 +322,6 @@ impl Application {
         }
 
         self.config.store(Arc::new(config));
-
-        // Update all the relevant members in the editor after replacing
-        // the configuration.
-        self.editor.refresh_config();
     }
 
     fn true_color(&self) -> bool {
