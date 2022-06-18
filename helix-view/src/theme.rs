5f85522d2712debce0accd1167726be0a92b738d
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2022-06-18 17:58:49 -0400

    Remove theme cache

--- helix-view/src/theme.rs
+++ helix-view/src/theme.rs
@@ -1,7 +1,6 @@
 use std::{
     collections::HashMap,
     path::{Path, PathBuf},
-    sync::{Arc, Mutex},
 };
 
 use anyhow::Context;
@@ -34,7 +33,6 @@ pub static BASE16_DEFAULT_THEME: Lazy<Theme> = Lazy::new(|| {
 pub struct Loader {
     user_dir: PathBuf,
     default_dir: PathBuf,
-    theme_cache: Arc<Mutex<HashMap<String, Theme>>>,
 }
 impl Loader {
     /// Creates a new loader that can load themes from two directories.
@@ -42,7 +40,6 @@ impl Loader {
         Self {
             user_dir: user_dir.as_ref().join("themes"),
             default_dir: default_dir.as_ref().join("themes"),
-            theme_cache: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
@@ -63,26 +60,10 @@ impl Loader {
             self.default_dir.join(filename)
         };
 
-        let path_str = path.to_str();
-
-        // We can return the theme from the cache if it already exists
-        if let Some(path_str) = path_str {
-            let tc = self.theme_cache.lock().unwrap();
-            if tc.contains_key(path_str) {
-                let mut theme = tc.get(path_str).unwrap().clone();
-                theme.name = name.to_string();
-                return Result::Ok(theme);
-            }
-        }
-
         let data = std::fs::read(&path)?;
         let theme = toml::from_slice::<Theme>(data.as_slice());
         match theme {
             Ok(mut theme) => {
-                if let Some(p) = path_str {
-                    let mut tc = self.theme_cache.lock().unwrap();
-                    tc.insert(p.to_string(), theme.clone());
-                }
                 theme.name = name.to_string();
                 Ok(theme)
             }
