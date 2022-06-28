141ce7f07a19874baadaece9dcbaf904bd4c2296
Michael Davis <mcarsondavis@gmail.com>
2022-06-28 10:09:42 -0500

    html: highlight punctuation
    * `/>` as in self-closing tags like `<hr/>`
    * `=` as in the separator between attribute name and value `<a href="bar">`

--- runtime/queries/html/highlights.scm
+++ runtime/queries/html/highlights.scm
@@ -2,11 +2,18 @@
 (erroneous_end_tag_name) @tag.error
 (doctype) @constant
 (attribute_name) @attribute
-(attribute_value) @string
 (comment) @comment
+
+[
+  "\""
+  (attribute_value)
+] @string
 
 [
   "<"
   ">"
   "</"
+  "/>"
 ] @punctuation.bracket
+
+"=" @punctuation.delimiter
