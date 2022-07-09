fefa4d8c5938c4e74b6eb01c6fb8379548c20ab4
Timothy DeHerrera <tim@nrdxp.dev>
2022-07-09 09:09:37 -0700

    nix flake: make the binary cache "just work" (#2999)

--- flake.nix
+++ flake.nix
@@ -96,4 +96,9 @@
         };
       };
     };
+
+  nixConfig = {
+    extra-substituters = ["https://helix.cachix.org"];
+    extra-trusted-public-keys = ["helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="];
+  };
 }
