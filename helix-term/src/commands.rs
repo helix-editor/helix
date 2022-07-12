bd665883dc86f193208fe57476ee3ac1b7d63284
Termina94 <revell345@gmail.com>
2022-07-12 22:33:34 +0100

    refactor editor resize functions

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -52,7 +52,7 @@ use helix_view::{
     input::KeyEvent,
     keyboard::KeyCode,
     theme::Style,
-    tree,
+    tree::{Dimension, Resize},
     view::View,
     Document, DocumentId, Editor, ViewId,
 };
@@ -912,19 +912,19 @@ fn goto_line_start(cx: &mut Context) {
 }
 
 fn grow_buffer_width(cx: &mut Context) {
-    cx.editor.grow_buffer_width();
+    cx.editor.resize_buffer(Resize::Grow, Dimension::Width);
 }
 
 fn shrink_buffer_width(cx: &mut Context) {
-    cx.editor.shrink_buffer_width();
+    cx.editor.resize_buffer(Resize::Shrink, Dimension::Width);
 }
 
 fn grow_buffer_height(cx: &mut Context) {
-    cx.editor.grow_buffer_height();
+    cx.editor.resize_buffer(Resize::Grow, Dimension::Height);
 }
 
 fn shrink_buffer_height(cx: &mut Context) {
-    cx.editor.shrink_buffer_height();
+    cx.editor.resize_buffer(Resize::Shrink, Dimension::Height);
 }
 
 fn buffer_expand_mode(cx: &mut Context) {
