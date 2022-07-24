03fa959ee82d8f92751f19600434eac513762915
A-Walrus <ofek.r.guy@gmail.com>
2022-07-24 14:27:03 +0300

    Cleanup code

--- helix-term/src/ui/mod.rs
+++ helix-term/src/ui/mod.rs
@@ -103,16 +103,14 @@ pub fn regex_prompt(
                         .build()
                     {
                         Ok(regex) => {
-                            {
-                                let (view, doc) = current!(cx.editor);
+                            let (view, doc) = current!(cx.editor);
 
-                                // revert state to what it was before the last update
-                                doc.set_selection(view.id, snapshot.clone());
+                            // revert state to what it was before the last update
+                            doc.set_selection(view.id, snapshot.clone());
 
-                                if event == PromptEvent::Validate {
-                                    // Equivalent to push_jump to store selection just before jump
-                                    view.jumps.push((doc_id, snapshot.clone()));
-                                }
+                            if event == PromptEvent::Validate {
+                                // Equivalent to push_jump to store selection just before jump
+                                view.jumps.push((doc_id, snapshot.clone()));
                             }
 
                             fun(cx.editor, regex, event);
