835d38a90b4ed4c0e7b78cd2696c3bc0854fbe06
Erasin <erasinoo@gmail.com>
2022-07-27 19:43:24 +0800

    Fix function && class

--- runtime/queries/javascript/textobjects.scm
+++ runtime/queries/javascript/textobjects.scm
@@ -13,16 +13,14 @@
 (method_definition
   body: (statement_block)) @function.around
 
+(generator_function_declaration
+  body: (_) @function.inside) @function.around
+
 (class_declaration
   body: (class_body) @class.inside) @class.around
 
-(export_statement
-  (class_declaration) @class.around) @class.around.start
-
-(export_statement
-  (class) @class.around) @class.around.start
-
-(_ (statement_block) @block.inside) @block.around
+(class
+  (class_body) @class.inside) @class.around
 
 (formal_parameters
   ((_) @parameter.inside . ","? @parameter.around) @parameter.around)
