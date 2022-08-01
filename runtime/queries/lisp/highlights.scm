d2c59e4b0c8d5e4b9c7d9fc3bb92827b9e06b341
Alexander Brevig <alexanderbrevig@gmail.com>
2022-08-02 01:18:57 +0200

    feat: support (common) lisp

---
+++ runtime/queries/lisp/highlights.scm
@@ -0,0 +1,19 @@
+;highlights.scm
+(nil) @constant
+(string) @string
+(comment) @comment
+(number) @constant
+(symbol) @function
+(identifier) @variable
+(keyword) @keyword
+(function_namespace) @special
+
+[
+  "("
+  ")"
+] @punctuation
+
+[
+  (quote_ident)
+  (quote)
+] @type
