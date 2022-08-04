b4edea4bf13415cdb4fd38033c991393e0d450fa
Alexander Brevig <alexanderbrevig@gmail.com>
2022-08-04 08:26:03 +0200

    fix: save work for later

--- xtask/src/themelint.rs
+++ xtask/src/themelint.rs
@@ -1,7 +1,10 @@
 use crate::path;
 use crate::DynError;
+use helix_view::theme::Color;
 use helix_view::theme::Style;
 use helix_view::Theme;
+
+const CONTRAST_REQUIRED: f64 = 2.;
 
 pub fn lint_all() -> Result<(), DynError> {
     let files = std::fs::read_dir(path::themes())
@@ -93,9 +96,59 @@ pub fn lint(file: String) -> Result<(), DynError> {
         ],
         vec!["markup.raw.inline", "markup.heading"],
     ];
+
+    let lumin_color = |c: Color| -> i64 {
+        println!("{:?}", c);
+        match c {
+            Color::Black => 1,
+            _ => 1000,
+        }
+    };
+    let lumin = |r: u8, g: u8, b: u8| -> f64 {
+        (0.2126 * r as f64) + (0.7152 * g as f64) + (0.0722 * b as f64) * 1000. + 1.
+    };
+    let get_fg = |s: Style| -> Color { s.fg.expect("Must have fg") };
+    let get_bg = |s: Style| -> Color { s.bg.expect("Must have bg") };
+    let lint_rules = vec![
+        (("ui.text", get_fg), ("ui.background", get_bg)),
+        (("ui.statusline", get_fg), ("ui.statusline", get_bg)),
+    ];
+
+    let mut lint_warnings = vec![];
+    lint_rules.into_iter().for_each(|rule| {
+        let from_rule = rule.0;
+        let from_color = from_rule.1(theme.get(from_rule.0));
+        let to_rule = rule.1;
+        let to_color = to_rule.1(theme.get(to_rule.0));
+        let from_lumin = if let Color::Rgb(r, g, b) = from_color {
+            lumin(r, g, b) as i64
+        } else {
+            lumin_color(from_color)
+        };
+        let to_lumin = if let Color::Rgb(r, g, b) = to_color {
+            lumin(r, g, b) as i64
+        } else {
+            lumin_color(from_color)
+        };
+        let contrast = std::cmp::max(from_lumin, to_lumin) as f64
+            / (std::cmp::min(from_lumin, to_lumin) as f64 + 0.000001);
+        let mut message = String::from(contrast.to_string());
+        if from_lumin == 0 {
+            message.push_str("from0");
+        }
+        if to_lumin == 0 {
+            message.push_str("to0");
+        }
+        if from_lumin != 0 && to_lumin != 0 && contrast < CONTRAST_REQUIRED {
+            message.push_str("LOW")
+        }
+        lint_warnings.push(message);
+    });
+    println!("{:?}", lint_warnings);
     struct ScopeWithError {
         error: bool,
         scope: String,
+        messages: Vec<String>,
     }
 
     let lint_errors: Vec<String> = check
@@ -105,19 +158,22 @@ pub fn lint(file: String) -> Result<(), DynError> {
                 ScopeWithError {
                     error: false,
                     scope: String::new(),
+                    messages: Vec::new(),
                 },
                 |mut acc, path| {
                     let style = theme.get(path);
-                    if !style.eq(&Style::default()) {
+                    if style.eq(&Style::default()) {
                         acc.error = true;
-                    } else if acc.scope.len() == 0 {
+                    }
+                    if acc.scope.len() == 0 {
                         acc.scope = path.to_string();
                     }
+
                     acc
                 },
             )
         })
-        .filter_map(|s| if !s.error { Some(s.scope) } else { None })
+        .filter_map(|s| if s.error { Some(s.scope) } else { None })
         .collect();
 
     if lint_errors.len() > 0 {
@@ -129,6 +185,7 @@ pub fn lint(file: String) -> Result<(), DynError> {
         Ok(())
     }
 }
+
 fn print_json_arr(arr: Vec<String>) {
     println!("[");
     let mut first = true;
