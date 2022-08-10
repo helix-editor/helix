4f734d0d2b3e60bfcdc4376eaebdadac6fa9d44c
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-08-09 19:44:37 -0500

    Revert 3121353c6ab2fbc5fced28f075c7fc45b53b661e

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -4552,18 +4552,14 @@ fn shell_impl(
     use std::process::{Command, Stdio};
     ensure!(!shell.is_empty(), "No shell set");
 
-    let mut process = Command::new(&shell[0]);
-    process
+    let mut process = match Command::new(&shell[0])
         .args(&shell[1..])
         .arg(cmd)
         .stdout(Stdio::piped())
-        .stderr(Stdio::piped());
-
-    if input.is_some() {
-        process.stdin(Stdio::piped());
-    }
-
-    let mut process = match process.spawn() {
+        .stderr(Stdio::piped())
+        .stdin(Stdio::piped())
+        .spawn()
+    {
         Ok(process) => process,
         Err(e) => {
             log::error!("Failed to start shell: {}", e);
