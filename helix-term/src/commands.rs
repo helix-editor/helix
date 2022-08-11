f503a3fb8e2a502631b2921df6c885ac7ea93ab4
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-08-11 18:29:40 -0500

    Switch to conditional compilation

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -4552,14 +4552,23 @@ fn shell_impl(
     use std::process::{Command, Stdio};
     ensure!(!shell.is_empty(), "No shell set");
 
-    let mut process = match Command::new(&shell[0])
+let mut process = Command::new(&shell[0]);
+    process
         .args(&shell[1..])
         .arg(cmd)
         .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .stdin(Stdio::piped())
-        .spawn()
+        .stderr(Stdio::piped());
+
+    #[cfg(not(target_family = "windows"))]
+    if input.is_some() {
+        process.stdin(Stdio::piped());
+    }
+    #[cfg(target_family = "windows")]
     {
+        process.stdin(Stdio::piped());
+    }
+
+    let mut process = match process.spawn() {
         Ok(process) => process,
         Err(e) => {
             log::error!("Failed to start shell: {}", e);
