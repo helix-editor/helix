18909aafe21ed32f11dcdf646423b43b1a951010
Charlie Groves <c@sevorg.org>
2022-08-22 01:29:30 +0000

    Update to crossterm-0.25 (#3390)

--- helix-term/src/ui/editor.rs
+++ helix-term/src/ui/editor.rs
@@ -1253,6 +1253,7 @@ impl Component for EditorView {
             }
 
             Event::Mouse(event) => self.handle_mouse_event(event, &mut cx),
+            Event::FocusGained | Event::FocusLost => EventResult::Ignored(None),
         }
     }
 
