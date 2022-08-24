85b8e2d050cd884817c0a1f386b9b80886e9d0f9
greym0uth <thejadenjack@gmail.com>
2022-08-23 17:13:23 -0700

    add esdl language support

---
+++ runtime/queries/esdl/highlights.scm
@@ -0,0 +1,78 @@
+; Keywords
+[
+  "module"
+  "using"
+  "single"
+  "multi"
+  "link"
+  "property"
+  "constraint"
+  "tuple"
+  "annotation"
+  "abstract"
+  "scalar"
+  "type"
+  "required"
+  "optional"
+  "extension"
+  "function"
+] @keyword
+
+(modifier) @keyword
+(extending) @keyword
+
+(module name: (identifier) @namespace)
+(object_type) @type
+
+(comment) @comment
+
+; Properties
+(property) @property
+(link) @property
+
+; Links
+(identifier) @variable
+(string) @string
+(edgeql_fragment) @string
+
+; Annotations
+; (annotation) @property
+
+; Builtins
+
+(type) @type
+[
+  "str"
+  "bool"
+  "int16"
+  "int32"
+  "int64"
+  "float32"
+  "float64"
+  "bigint"
+  "decimal"
+  "json"
+  "uuid"
+  "bytes"
+  "datetime"
+  "duration"
+  "sequence"
+  "anytype"
+] @type.builtin
+
+(true) @constant.builtin
+(false) @constant.builtin
+(null) @constant.builtin
+
+; Delimiters
+[
+  ";"
+  ","
+] @punctuation.delimiter
+
+; Operators
+[
+  "->"
+  ":="
+] @operator
+
