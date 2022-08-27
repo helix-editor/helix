61d89a038edc0190e79898196a5376d014d1e822
A-Walrus <ofek.r.guy@gmail.com>
2022-08-27 12:43:07 +0300

    Fix failing test

--- helix-view/src/input.rs
+++ helix-view/src/input.rs
@@ -555,8 +555,6 @@ mod test {
 
     #[test]
     fn parsing_unsupported_named_keys() {
-        assert!(str::parse::<KeyEvent>("lt").is_err());
-        assert!(str::parse::<KeyEvent>("gt").is_err());
         assert!(str::parse::<KeyEvent>("plus").is_err());
         assert!(str::parse::<KeyEvent>("percent").is_err());
         assert!(str::parse::<KeyEvent>("semicolon").is_err());
