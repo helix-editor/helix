6d52117877f70e095ece375aaa9ec3e6073758e2
Alexander Brevig <alexanderbrevig@gmail.com>
2022-08-29 19:09:05 +0200

    fix: render highlights over rulers

--- helix-term/src/ui/editor.rs
+++ helix-term/src/ui/editor.rs
@@ -128,6 +128,7 @@ impl EditorView {
             Box::new(highlights)
         };
 
+        Self::render_rulers(editor, doc, view, inner, surface, theme);
         Self::render_text_highlights(
             doc,
             view.offset,
@@ -138,7 +139,6 @@ impl EditorView {
             &editor.config(),
         );
         Self::render_gutter(editor, doc, view, view.area, surface, theme, is_focused);
-        Self::render_rulers(editor, doc, view, inner, surface, theme);
 
         if is_focused {
             Self::render_focused_view_elements(view, doc, inner, theme, surface);
