ccedf678a14c8f095f2dfc96b5c812e6a65f9a6b
Saber Haj Rabiee <saber.hajrabiee@gmail.com>
2022-08-30 03:21:40 -0700

    fix: prevents storing last prompt if is top of stack

--- helix-term/src/ui/prompt.rs
+++ helix-term/src/ui/prompt.rs
@@ -529,18 +529,24 @@ impl Component for Prompt {
                     self.recalculate_completion(cx.editor);
                     self.exit_selection();
                 } else {
+                    let last_item = self
+                        .history_register
+                        .and_then(|reg| cx.editor.registers.last(reg).cloned())
+                        .map(|entry| entry.into())
+                        .unwrap_or_else(|| Cow::from(""));
+
                     // handle executing with last command in history if nothing entered
                     let input: Cow<str> = if self.line.is_empty() {
-                        // latest value in the register list
-                        self.history_register
-                            .and_then(|reg| cx.editor.registers.last(reg).cloned())
-                            .map(|entry| entry.into())
-                            .unwrap_or_else(|| Cow::from(""))
+                        last_item
                     } else {
-                        if let Some(register) = self.history_register {
+                        if last_item != self.line {
                             // store in history
-                            let register = cx.editor.registers.get_mut(register);
-                            register.push(self.line.clone());
+                            if let Some(register) = self.history_register {
+                                cx.editor
+                                    .registers
+                                    .get_mut(register)
+                                    .push(self.line.clone());
+                            };
                         }
 
                         self.line.as_str().into()
