e3e71fa36be14659f0eabe1d4c80ef7e68135a6a
Gokul Soumya <gokulps15@gmail.com>
2022-08-31 21:53:45 +0530

    Show clipboard info in --health output (#2947)
    * Show clipboard info in --health output
    
    * health: Separate 'languages' category from 'all'
    
    Co-authored-by: Michael Davis <mcarsondavis@gmail.com>

--- helix-term/src/main.rs
+++ helix-term/src/main.rs
@@ -61,8 +61,9 @@ ARGS:
 FLAGS:
     -h, --help                     Prints help information
     --tutor                        Loads the tutorial
-    --health [LANG]                Checks for potential errors in editor setup
-                                   If given, checks for config errors in language LANG
+    --health [CATEGORY]            Checks for potential errors in editor setup
+                                   CATEGORY can be a language or one of 'clipboard', 'languages'
+                                   or 'all'. 'all' is the default if not specified.
     -g, --grammar {{fetch|build}}    Fetches or builds tree-sitter grammars listed in languages.toml
     -c, --config <file>            Specifies a file to use for configuration
     -v                             Increases logging verbosity each use for up to 3 times
