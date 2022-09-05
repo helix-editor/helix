464d7c541823cda2cc3016e9f3edfa84c3a5dca2
Alexander Brevig <alexanderbrevig@gmail.com>
2022-09-05 12:38:50 +0200

    fix: add difference check for statusline normal,insert,select

--- xtask/src/themelint.rs
+++ xtask/src/themelint.rs
@@ -10,33 +10,44 @@ struct Rule {
     check_both: bool,
 }
 
+enum Require {
+    Existence(Rule),
+    Difference(&'static str, &'static str),
+}
+
 // Placed in an fn here, so it's the first thing you see
-fn get_rules() -> Vec<Rule> {
+fn get_rules() -> Vec<Require> {
     vec![
         // Check for ui.selection, which is required
-        Rule::has_either("ui.selection"),
+        Require::Existence(Rule::has_either("ui.selection")),
         // Check for planned readable text
-        Rule::has_fg("ui.text"),
-        Rule::has_bg("ui.background"),
+        Require::Existence(Rule::has_fg("ui.text")),
+        Require::Existence(Rule::has_bg("ui.background")),
         // Check for complete editor.statusline bare minimum
-        Rule::has_both("ui.statusline"),
-        Rule::has_both("ui.statusline.inactive"),
+        Require::Existence(Rule::has_both("ui.statusline")),
+        Require::Existence(Rule::has_both("ui.statusline.inactive")),
         // Check for editor.color-modes
-        Rule::has_either("ui.statusline.insert"),
-        Rule::has_either("ui.statusline.normal"),
-        Rule::has_either("ui.statusline.select"),
+        Require::Existence(Rule::has_either("ui.statusline.normal")),
+        Require::Existence(Rule::has_either("ui.statusline.insert")),
+        Require::Existence(Rule::has_either("ui.statusline.select")),
+        Require::Difference("ui.statusline.normal", "ui.statusline.insert"),
+        Require::Difference("ui.statusline.normal", "ui.statusline.select"),
         // Check for editor.cursorline
-        Rule::has_bg("ui.cursorline.primary"),
+        Require::Existence(Rule::has_bg("ui.cursorline.primary")),
+        // Check for editor.whitespace
+        Require::Existence(Rule::has_both("ui.virtual.whitespace")),
+        // Check fir rulers
+        Require::Existence(Rule::has_either("ui.virtual.indent-guide")),
         // Check for editor.rulers
-        Rule::has_either("ui.virtual.ruler"),
+        Require::Existence(Rule::has_either("ui.virtual.ruler")),
         // Check for menus and prompts
-        Rule::has_both("ui.menu"),
-        Rule::has_both("ui.help"),
-        Rule::has_bg("ui.popup"),
-        Rule::has_either("ui.window"),
+        Require::Existence(Rule::has_both("ui.menu")),
+        Require::Existence(Rule::has_both("ui.help")),
+        Require::Existence(Rule::has_bg("ui.popup")),
+        Require::Existence(Rule::has_either("ui.window")),
         // Check for visible cursor
-        Rule::has_bg("ui.cursor.primary"),
-        Rule::has_either("ui.cursor.match"),
+        Require::Existence(Rule::has_bg("ui.cursor.primary")),
+        Require::Existence(Rule::has_either("ui.cursor.match")),
     ]
 }
 
@@ -69,19 +80,21 @@ impl Rule {
             check_both: true,
         }
     }
-    fn found_impl(theme: &Theme, find: Option<&'static str>) -> bool {
-        if let Some(fg) = &find {
+    fn found_fg(&self, theme: &Theme) -> bool {
+        if let Some(fg) = &self.fg {
             if theme.get(fg).fg.is_none() && theme.get(fg).add_modifier == Modifier::empty() {
                 return false;
             }
         }
         return true;
     }
-    fn found_fg(&self, theme: &Theme) -> bool {
-        return Rule::found_impl(theme, self.fg);
-    }
     fn found_bg(&self, theme: &Theme) -> bool {
-        return Rule::found_impl(theme, self.bg);
+        if let Some(bg) = &self.bg {
+            if theme.get(bg).bg.is_none() && theme.get(bg).add_modifier == Modifier::empty() {
+                return false;
+            }
+        }
+        return true;
     }
     fn rule_name(&self) -> &'static str {
         if self.fg.is_some() {
@@ -129,9 +142,16 @@ pub fn lint(file: String) -> Result<(), DynError> {
     let theme: Theme = toml::from_slice(&theme).expect("Failed to parse theme");
 
     let mut messages: Vec<String> = vec![];
-    get_rules()
-        .iter()
-        .for_each(|rule| rule.validate(&theme, &mut messages));
+    get_rules().iter().for_each(|lint| match lint {
+        Require::Existence(rule) => rule.validate(&theme, &mut messages),
+        Require::Difference(a, b) => {
+            let theme_a = theme.get(a);
+            let theme_b = theme.get(b);
+            if theme_a == theme_b {
+                messages.push(format!("$THEME: `{}` and `{}` cannot be equal", a, b));
+            }
+        }
+    });
 
     if messages.len() > 0 {
         messages.iter().for_each(|m| {
