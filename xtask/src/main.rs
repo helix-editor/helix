a264aefb6282c9966849bb6d6ae6a80120a6234f
Alexander Brevig <alexanderbrevig@gmail.com>
2022-09-09 21:47:59 +0200

    refactor: querycheck and clippy

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,6 +1,7 @@
 mod docgen;
 mod helpers;
 mod path;
+mod querycheck;
 mod themelint;
 
 use std::{env, error::Error};
@@ -10,6 +11,7 @@ type DynError = Box<dyn Error>;
 pub mod tasks {
     use crate::docgen::{lang_features, typable_commands, write};
     use crate::docgen::{LANG_SUPPORT_MD_OUTPUT, TYPABLE_COMMANDS_MD_OUTPUT};
+    use crate::querycheck::query_check;
     use crate::themelint::{lint, lint_all};
     use crate::DynError;
 
@@ -19,47 +21,15 @@ pub mod tasks {
         Ok(())
     }
 
-
     pub fn themelint(file: Option<String>) -> Result<(), DynError> {
         match file {
             Some(file) => lint(file),
             None => lint_all(),
         }
     }
-  
-    pub fn query_check() -> Result<(), String> {
-        use crate::helpers::lang_config;
-        use helix_core::{syntax::read_query, tree_sitter::Query};
-        use helix_loader::grammar::get_language;
 
-        let query_files = [
-            "highlights.scm",
-            "locals.scm",
-            "injections.scm",
-            "textobjects.scm",
-            "indents.scm",
-        ];
-
-        for language in lang_config().language {
-            let language_name = language.language_id.to_ascii_lowercase();
-            let grammar_name = language.grammar.unwrap_or(language.language_id);
-            for query_file in query_files {
-                let language = get_language(&grammar_name);
-                let query_text = read_query(&language_name, query_file);
-                if !query_text.is_empty() && language.is_ok() {
-                    if let Err(reason) = Query::new(language.unwrap(), &query_text) {
-                        return Err(format!(
-                            "Failed to parse {} queries for {}: {}",
-                            query_file, language_name, reason
-                        ));
-                    }
-                }
-            }
-        }
-
-        println!("Query check succeeded");
-
-        Ok(())
+    pub fn querycheck() -> Result<(), DynError> {
+        query_check()
     }
 
     pub fn print_help() {
@@ -83,7 +53,7 @@ fn main() -> Result<(), DynError> {
         Some(t) => match t.as_str() {
             "docgen" => tasks::docgen()?,
             "themelint" => tasks::themelint(env::args().nth(2))?,
-            "query-check" => tasks::query_check()?,
+            "query-check" => tasks::querycheck()?,
             invalid => return Err(format!("Invalid task name: {}", invalid).into()),
         },
     };
