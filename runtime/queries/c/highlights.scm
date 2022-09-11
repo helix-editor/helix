b3809e436aad38f1a71c6ee43af5dc15b4454685
Shafkath Shuhan <shafkathshuhannyc@gmail.com>
2022-09-10 20:56:15 -0400

    update C queries and grammar

--- runtime/queries/c/highlights.scm
+++ runtime/queries/c/highlights.scm
@@ -1,72 +1,147 @@
 (storage_class_specifier) @keyword.storage
 
-"goto" @keyword
-"register" @keyword
-"break" @keyword
-"case" @keyword
-"continue" @keyword
-"default" @keyword
-"do" @keyword
-"else" @keyword
-"enum" @keyword
-"extern" @keyword
-"for" @keyword
-"if" @keyword
-"inline" @keyword
-"return" @keyword
-"sizeof" @keyword
-"struct" @keyword
-"switch" @keyword
-"typedef" @keyword
-"union" @keyword
-"volatile" @keyword
-"while" @keyword
-"const" @keyword
+[
+  "const"
+  "default"
+  "enum"
+  "extern"
+  "inline"
+  "static"
+  "struct"
+  "typedef"
+  "union"
+  "volatile"
+  "goto"
+  "register"
+] @keyword
+
+"sizeof" @keyword.operator
+"return" @keyword.control.return
 
 [
- "#define"
- "#elif"
- "#else"
- "#endif"
- "#if"
- "#ifdef"
- "#ifndef"
- "#include"
- (preproc_directive)
+  "while"
+  "for"
+  "do"
+  "continue"
+  "break"
+] @keyword.control.repeat
+
+[
+  "if"
+  "else"
+  "case"
+  "switch"
+] @keyword.control.conditional
+(conditional_expression [ "?" ":" ] @keyword.control.conditional)
+
+[
+  "#define"
+  "#include"
+  "#if"
+  "#ifdef"
+  "#ifndef"
+  "#else"
+  "#elif"
+  "#endif"
+  (preproc_directive)
 ] @keyword.directive
 
-"--" @operator
-"-" @operator
-"-=" @operator
-"->" @operator
-"=" @operator
-"!=" @operator
-"*" @operator
-"&" @operator
-"&&" @operator
-"+" @operator
-"++" @operator
-"+=" @operator
-"<" @operator
-"==" @operator
-">" @operator
-"||" @operator
-">=" @operator
-"<=" @operator
+[ ";" ":" "," ] @punctuation.delimiter
+[ "(" ")" "[" "]" "{" "}"] @punctuation.bracket
+"..." @punctuation.special
 
-"." @punctuation.delimiter
-";" @punctuation.delimiter
+[
+  "="
 
-[(true) (false)] @constant.builtin.boolean
+  "-"
+  "*"
+  "/"
+  "+"
+  "%"
 
-(enumerator) @type.enum.variant
+  "~"
+  "|"
+  "&"
+  "^"
+  "<<"
+  ">>"
+
+  "->"
+  "."
+
+  "<"
+  "<="
+  ">="
+  ">"
+  "=="
+  "!="
+
+  "!"
+  "&&"
+  "||"
+
+  "-="
+  "+="
+  "*="
+  "/="
+  "%="
+  "|="
+  "&="
+  "^="
+  ">>="
+  "<<="
+  "--"
+  "++"
+] @operator
+
+;; Make sure the comma operator is given a highlight group after the comma
+;; punctuator so the operator is highlighted properly.
+(comma_expression [ "," ] @operator)
+
+[
+  (true)
+  (false)
+] @constant.builtin.boolean
 
 (string_literal) @string
-(system_lib_string) @string
+(system_lib_string) @string.special.path
 
-(null) @constant
-(number_literal) @constant.numeric.integer
+(escape_sequence) @constant.character.escape
 (char_literal) @constant.character
+(number_literal) @constant.numeric
+(null) @constant.builtin
+
+[
+  (preproc_arg)
+  (preproc_defined)
+] @function.macro
+
+(statement_identifier) @label
+
+[
+ (type_identifier)
+ (sized_type_specifier)
+ (type_descriptor)
+] @type
+(primitive_type) @type.builtin
+
+(sizeof_expression value: (parenthesized_expression (identifier) @type))
+(enumerator
+  name: (identifier) @type.enum.variant)
+
+((identifier) @constant
+ (#match? @constant "^[A-Z][A-Z\\d_]*$"))
+
+(case_statement
+  value: (identifier) @constant)
+
+;; Preproc def / undef
+(preproc_def
+  name: (_) @constant)
+(preproc_call
+  directive: (preproc_directive) @_u
+  argument: (_) @constant
+  (#eq? @_u "#undef"))
 
 (call_expression
   function: (identifier) @function)
@@ -76,17 +151,29 @@
 (function_declarator
   declarator: (identifier) @function)
 (preproc_function_def
-  name: (identifier) @function.special)
-
-(field_identifier) @variable.other.member
-(statement_identifier) @label
-(type_identifier) @type
-(primitive_type) @type
-(sized_type_specifier) @type
-
-((identifier) @constant
- (#match? @constant "^[A-Z][A-Z\\d_]*$"))
-
-(identifier) @variable
+  name: (identifier) @function.macro)
 
 (comment) @comment
+
+;; Parameters
+(parameter_declaration
+  declarator: (identifier) @variable.parameter)
+(parameter_declaration
+  declarator: (pointer_declarator) @variable.parameter)
+(preproc_params (identifier) @variable.parameter)
+
+[
+  "__attribute__"
+  "__cdecl"
+  "__clrcall"
+  "__stdcall"
+  "__fastcall"
+  "__thiscall"
+  "__vectorcall"
+  "_unaligned"
+  "__unaligned"
+  "__declspec"
+  (attribute_declaration)
+] @attribute
+
+(field_identifier) @variable.other.member
