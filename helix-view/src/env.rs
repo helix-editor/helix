800c62f02abe479cf6b13b5bc58eeeedac30a6c0
Alexander Brevig <alexanderbrevig@gmail.com@gmail.com>
2022-09-16 20:52:45 +0200

    feat: add $pwd and some error handling

--- helix-view/src/env.rs
+++ helix-view/src/env.rs
@@ -7,7 +7,19 @@ where
     T: Iterator,
     <T as Iterator>::Item: Display,
 {
-    let path = doc.path().and_then(|p| p.to_str()).unwrap_or_default();
+    let path = doc.path().and_then(|p| p.to_str()).unwrap_or_else(|| {
+        log::error!("No $path found for document: {:?}", doc.url());
+        "ENV:NO_PATH"
+    });
+    let pwd = std::env::current_dir()
+        .unwrap_or_default()
+        .into_os_string()
+        .into_string()
+        .unwrap_or_else(|err| {
+            log::error!("No $pwd found for: {:?}", err);
+            "ENV:NO_PATH".to_string()
+        });
 
-    strs.map(|s| s.to_string().replace("$file", path)).collect()
+    strs.map(|s| s.to_string().replace("$file", path).replace("$pwd", &pwd))
+        .collect()
 }
