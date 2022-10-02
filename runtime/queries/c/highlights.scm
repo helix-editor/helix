2f5dd93653c7801399b93601b7d9e29efc07b0c3
Chickenkeeper <chrism.chickenkeeper24@gmail.com>
2022-10-02 22:35:43 +0100

    Update highlights.scm

--- runtime/queries/c/highlights.scm
+++ runtime/queries/c/highlights.scm
@@ -1,61 +1,75 @@
 (storage_class_specifier) @keyword.storage
 
-"goto" @keyword
-"register" @keyword
-"break" @keyword
-"case" @keyword
-"continue" @keyword
-"default" @keyword
-"do" @keyword
-"else" @keyword
-"enum" @keyword
-"extern" @keyword
-"for" @keyword
-"if" @keyword
-"inline" @keyword
-"return" @keyword
-"sizeof" @keyword
-"struct" @keyword
-"switch" @keyword
-"typedef" @keyword
-"union" @keyword
-"volatile" @keyword
-"while" @keyword
-"const" @keyword
+[
+  "register"
+  "const"
+  "enum"
+  "extern"
+  "inline"
+  "sizeof"
+  "struct"
+  "typedef"
+  "union"
+  "volatile"
+] @keyword
 
 [
- "#define"
- "#elif"
- "#else"
- "#endif"
- "#if"
- "#ifdef"
- "#ifndef"
- "#include"
- (preproc_directive)
+  "for"
+  "do"
+  "while"
+  "break"
+  "continue"
+] @keyword.control.repeat
+
+[
+  "goto"
+  "if"
+  "else"
+  "switch"
+  "case"
+  "default"
+] @keyword.control.conditional
+
+"return" @keyword.control.return
+
+[
+  "defined"
+  "#define"
+  "#elif"
+  "#else"
+  "#endif"
+  "#if"
+  "#ifdef"
+  "#ifndef"
+  "#include"
+  (preproc_directive)
 ] @keyword.directive
 
-"--" @operator
-"-" @operator
-"-=" @operator
-"->" @operator
-"=" @operator
-"!=" @operator
-"*" @operator
-"&" @operator
-"&&" @operator
-"+" @operator
-"++" @operator
-"+=" @operator
-"<" @operator
-"==" @operator
-">" @operator
-"||" @operator
-">=" @operator
-"<=" @operator
+[
+  "--"
+  "-"
+  "-="
+  "->"
+  "="
+  "!="
+  "*"
+  "&"
+  "&&"
+  "+"
+  "++"
+  "+="
+  "<"
+  "=="
+  ">"
+  "||"
+  ">="
+  "<="
+  "::"
+] @operator
 
-"." @punctuation.delimiter
-";" @punctuation.delimiter
+["," "." ":" ";"] @punctuation.delimiter
+
+["(" ")" "[" "]" "{" "}"] @punctuation.bracket
 
 [(true) (false)] @constant.builtin.boolean
 
@@ -73,20 +87,23 @@
 (call_expression
   function: (field_expression
     field: (field_identifier) @function))
+(function_declarator) @function
 (function_declarator
-  declarator: (identifier) @function)
+  parameters: (parameter_list) @variable.parameter)
 (preproc_function_def
   name: (identifier) @function.special)
 
+(compound_statement) @variable
+(init_declarator) @variable
 (field_identifier) @variable.other.member
 (statement_identifier) @label
+(struct_specifier) @type
+(type_definition) @type
 (type_identifier) @type
-(primitive_type) @type
+(primitive_type) @type.builtin
 (sized_type_specifier) @type
 
 ((identifier) @constant
  (#match? @constant "^[A-Z][A-Z\\d_]*$"))
-
-(identifier) @variable
 
 (comment) @comment
