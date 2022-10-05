cb40b60350bd8678a6f618855f9ea33f7c63d278
Chickenkeeper <chrism.chickenkeeper24@gmail.com>
2022-10-06 00:38:26 +0100

    Removed unnecessary highlights and comments

--- runtime/queries/c/highlights.scm
+++ runtime/queries/c/highlights.scm
@@ -105,9 +105,6 @@
 (number_literal) @constant.numeric.integer
 (char_literal) @constant.character
 
-((identifier) @constant
-  (#match? @constant "^[A-Z][A-Z\\d_]*$"))
-
 (call_expression
   function: (identifier) @function)
 (call_expression
@@ -123,36 +120,16 @@
     declarator: (identifier) @variable.parameter))
 (preproc_function_def
   name: (identifier) @function.special)
-; (preproc_arg) @error
 
 (field_identifier) @variable.other.member
 (statement_identifier) @label
-(struct_specifier) @type
-(type_definition) @type
 (type_identifier) @type
 (primitive_type) @type.builtin
 (sized_type_specifier) @type
 
-(init_declarator (identifier) @variable)
-(binary_expression left: (identifier) @variable)
-(binary_expression right: (identifier) @variable)
-(compound_statement (declaration (identifier) @variable))
-(for_statement (declaration (identifier) @variable))
-(field_expression (identifier) @variable)
-(pointer_declarator (identifier) @variable)
-(pointer_expression (identifier) @variable)
-(assignment_expression (identifier) @variable)
-(unary_expression (identifier) @variable)
-(sizeof_expression (parenthesized_expression (identifier) @type))
-(parenthesized_expression (identifier) @variable)
-(initializer_list (identifier) @variable)
-(initializer_pair (identifier) @variable)
-(return_statement (identifier) @variable)
-(subscript_expression (identifier) @variable)
-(cast_expression (identifier) @variable)
-(update_expression (identifier) @variable)
-(conditional_expression (identifier) @variable)
+((identifier) @constant
+  (#match? @constant "^[A-Z][A-Z\\d_]*$"))
 
-; (identifier) @error
+(identifier) @variable
 
 (comment) @comment
