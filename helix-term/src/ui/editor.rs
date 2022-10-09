183721b9352ac8feef871a2c19ed66456ea5447c
Iven Winkelmann <winkelmann.iven@gmail.com>
2022-10-09 22:14:02 +0200

    Fixed my ui.cursor setting collission

--- helix-term/src/ui/editor.rs
+++ helix-term/src/ui/editor.rs
@@ -338,9 +338,9 @@ impl EditorView {
         let primary_cursor_scope: usize = {
             if true {
                 match mode {
-                    Mode::Insert => theme.find_scope_index("ui.cursor.insert"),
-                    Mode::Select => theme.find_scope_index("ui.cursor.select"),
-                    Mode::Normal => theme.find_scope_index("ui.cursor.normal"),
+                    Mode::Insert => theme.find_scope_index("ui.cursor.color.insert"),
+                    Mode::Select => theme.find_scope_index("ui.cursor.color.select"),
+                    Mode::Normal => theme.find_scope_index("ui.cursor.color.normal"),
                 }
                 .unwrap_or(cursor_scope)
             } else {
