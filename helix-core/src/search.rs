cdc351bcb7a325cf15cade6af741857fb0e211e7
Thaqib Mohammed <thaqibdamani0@gmail.com>
2022-10-11 15:59:08 -0400

    fix fmt

--- helix-core/src/search.rs
+++ helix-core/src/search.rs
@@ -28,14 +28,14 @@ pub fn find_nth_next<M: CharMatcher>(
     }
 
     let mut chars = text.chars_at(pos);
-    let mut count = 0; 
+    let mut count = 0;
 
     while count < n {
         let c = chars.next()?;
         if char_matcher.char_match(c) {
             count += 1;
         }
-        pos += 1; 
+        pos += 1;
     }
 
     Some(pos - 1)
@@ -45,21 +45,21 @@ pub fn find_nth_prev<M: CharMatcher>(
     text: RopeSlice,
     char_matcher: M,
     mut pos: usize,
-    n: usize
+    n: usize,
 ) -> Option<usize> {
     if pos == 0 || n == 0 {
         return None;
     }
 
     let mut chars = text.chars_at(pos);
-    let mut count = 0; 
+    let mut count = 0;
 
     while count < n {
         let c = chars.prev()?;
         if char_matcher.char_match(c) {
-            count += 1; 
+            count += 1;
         }
-        pos -= 1; 
+        pos -= 1;
     }
 
     Some(pos)
