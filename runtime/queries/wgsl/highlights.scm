372a52b133071f9466494721d9fcbc3be352d6c9
Chickenkeeper <chrism.chickenkeeper24@gmail.com>
2022-10-13 05:13:00 +0100

    Improved variable & type highlighting
    Fixed cases where `compound_statement` could cause text modifiers to leak to the wrong places, and colours generic function types as `@type` instead of `@function`

--- runtime/queries/wgsl/highlights.scm
+++ runtime/queries/wgsl/highlights.scm
@@ -2,24 +2,12 @@
 (float_literal) @constant.numeric.float
 (bool_literal) @constant.builtin.boolean
 
-(global_constant_declaration) @variable
-(global_variable_declaration) @variable
-(compound_statement) @variable
-(const_expression) @function
-
-(variable_identifier_declaration
-  (identifier) @variable
-  (type_declaration) @type)
-
 (function_declaration
-  (identifier) @function
-  (function_return_type_declaration
-    (type_declaration) @type))
+  (identifier) @function)
 
 (parameter
   (variable_identifier_declaration
-    (identifier) @variable.parameter
-    (type_declaration) @type))
+    (identifier) @variable.parameter))
 
 (struct_declaration
   (identifier) @type)
@@ -27,11 +15,17 @@
 (struct_declaration
   (struct_member
     (variable_identifier_declaration
-      (identifier) @variable.other.member
-      (type_declaration) @type)))
+      (identifier) @variable.other.member)))
+
+(type_constructor_or_function_call_expression
+  (type_declaration (identifier) @function))
 
 (type_constructor_or_function_call_expression
   (type_declaration) @function)
+
+(type_declaration (type_declaration) @type)
+(type_declaration (identifier) @type)
+(type_declaration) @type
 
 [
   "bitcast"
@@ -130,3 +124,5 @@
   (identifier) @attribute)
 
 (comment) @comment
+
+(identifier) @variable
