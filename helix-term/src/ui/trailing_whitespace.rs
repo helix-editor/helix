977512470c20e2f73a820c04ae06140dfb5a78b8
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2022-10-17 09:06:38 +0200

    fix typos and give consistent namings

--- helix-term/src/ui/trailing_whitespace.rs
+++ helix-term/src/ui/trailing_whitespace.rs
@@ -7,10 +7,10 @@ pub enum WhitespaceKind {
 }
 
 pub struct TrailingWhitespaceTracker {
-    enabled: bool,
-    is_space_enable: bool,
-    nbsp_enabled: bool,
-    tab_enabled: bool,
+    tracker_enabled: bool,
+    is_space_enabled: bool,
+    is_nbsp_enabled: bool,
+    is_tab_enabled: bool,
 
     is_tracking: bool,
     tracking_from: u16,
@@ -24,10 +24,10 @@ impl TrailingWhitespaceTracker {
         let tab_enabled = cfg.render.tab() == WhitespaceRenderValue::Trailing;
         let enabled = space_enabled || nbsp_enabled || tab_enabled;
         Self {
-            enabled,
-            is_space_enable: space_enabled,
-            nbsp_enabled,
-            tab_enabled,
+            tracker_enabled: enabled,
+            is_space_enabled: space_enabled,
+            is_nbsp_enabled: nbsp_enabled,
+            is_tab_enabled: tab_enabled,
 
             is_tracking: false,
             tracking_from: 0,
@@ -36,7 +36,7 @@ impl TrailingWhitespaceTracker {
     }
 
     pub fn is_enabled(&self) -> bool {
-        self.enabled
+        self.tracker_enabled
     }
 
     pub fn track_nonwhitespace(&mut self) {
@@ -61,17 +61,21 @@ impl TrailingWhitespaceTracker {
         tab_char: &str,
         tab: &str,
     ) -> Option<(u16, String)> {
-        if !self.enabled || !self.is_tracking {
+        if !self.tracker_enabled || !self.is_tracking {
             return None;
         }
 
         self.is_tracking = false;
-        let trailing_space = if self.is_space_enable {
+        let trailing_space = if self.is_space_enabled {
             space_char
         } else {
             space
         };
-        let trailing_nbsp = if self.nbsp_enabled { nbsp_char } else { nbsp };
+        let trailing_nbsp = if self.is_nbsp_enabled {
+            nbsp_char
+        } else {
+            nbsp
+        };
         let trailing_whitespace = self
             .tracked
             .iter()
@@ -79,7 +83,7 @@ impl TrailingWhitespaceTracker {
                 WhitespaceKind::Space => trailing_space,
                 WhitespaceKind::NonBreakingSpace => trailing_nbsp,
                 WhitespaceKind::Tab(original_width, trailing_width) => {
-                    if self.tab_enabled {
+                    if self.is_tab_enabled {
                         &tab_char[..*trailing_width]
                     } else {
                         &tab[..*original_width]
