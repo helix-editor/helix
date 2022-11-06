helix-editor/helix

commit d6323b7cbc21a9d3ba29738c76581dad93f9f415

Select text inserted by shell or paste (#4458)
This follows changes in Kakoune to the same effects:

* p/<space>p: https://github.com/mawww/kakoune/commit/266d1c37d0d970a7eff747f5e6a5773a3cea39d8
* !/<A-!>: https://github.com/mawww/kakoune/commit/85b78dda2e29d70b620836b04224b104426bdbae

Selecting the new data inserted by shell or pasting is often more
useful than retaining a selection of the pre-paste/insert content.

helix-term/src/commands.rs
@@ -3471,6 +3471,8 @@ fn paste_impl(values: &[String], doc: &mut Document, view: &mut View, action: Pa
     let text = doc.text();
     let selection = doc.selection(view.id);
 
+    let mut ranges = SmallVec::with_capacity(selection.len());
+
     let transaction = Transaction::change_by_selection(text, selection, |range| {
         let pos = match (action, linewise) {
             // paste linewise before
@@ -3487,8 +3489,21 @@ fn paste_impl(values: &[String], doc: &mut Document, view: &mut View, action: Pa
             // paste at cursor
             (Paste::Cursor, _) => range.cursor(text.slice(..)),
         };
-        (pos, pos, values.next())
+
+        let value = values.next();
+
+        let value_len = value
+            .as_ref()
+            .map(|content| content.chars().count())
+            .unwrap_or_default();
+
+        ranges.push(Range::new(pos, pos + value_len));
+
+        (pos, pos, value)
     });
+
+    let transaction = transaction.with_selection(Selection::new(ranges, selection.primary_index()));
+
     apply_transaction(&transaction, doc, view);
 }
 
@@ -4742,6 +4757,7 @@ fn shell(cx: &mut compositor::Context, cmd: &str, behavior: &ShellBehavior) {
     let selection = doc.selection(view.id);
 
     let mut changes = Vec::with_capacity(selection.len());
+    let mut ranges = SmallVec::with_capacity(selection.len());
     let text = doc.text().slice(..);
 
     for range in selection.ranges() {
@@ -4765,11 +4781,13 @@ fn shell(cx: &mut compositor::Context, cmd: &str, behavior: &ShellBehavior) {
             ShellBehavior::Append => (range.to(), range.to()),
             _ => (range.from(), range.from()),
         };
+        ranges.push(Range::new(to, to + output.chars().count()));
         changes.push((from, to, Some(output)));
     }
 
     if behavior != &ShellBehavior::Ignore {
-        let transaction = Transaction::change(doc.text(), changes.into_iter());
+        let transaction = Transaction::change(doc.text(), changes.into_iter())
+            .with_selection(Selection::new(ranges, selection.primary_index()));
         apply_transaction(&transaction, doc, view);
         doc.append_changes_to_history(view.id);
     }

commit 94deeedd57f516cdfb5ac72b57db331494ff986b

Fix range offsets for multiple shell insertions
d6323b7cbc21a9d3ba29738c76581dad93f9f415 introduced a regression for
shell commands like `|`, `!`, and `<A-!>` which caused the new
selections to be incorrect. This caused a panic when piping (`|`)
would cause the new range to extend past the document end.

The paste version of this bug was fixed in
48a3965ab43718ce2a49724cbcc294b04c328b81.

This change also inherits the direction of the new range from the old
range and adds integration tests to ensure that the behavior isn't
broken in the future.

helix-term/src/commands.rs
@@ -4769,6 +4769,8 @@ fn shell(cx: &mut compositor::Context, cmd: &str, behavior: &ShellBehavior) {
     let mut ranges = SmallVec::with_capacity(selection.len());
     let text = doc.text().slice(..);
 
+    let mut offset = 0isize;
+
     for range in selection.ranges() {
         let fragment = range.slice(text);
         let (output, success) = match shell_impl(shell, cmd, pipe.then(|| fragment.into())) {
@@ -4784,13 +4786,23 @@ fn shell(cx: &mut compositor::Context, cmd: &str, behavior: &ShellBehavior) {
             return;
         }
 
-        let (from, to) = match behavior {
-            ShellBehavior::Replace => (range.from(), range.to()),
-            ShellBehavior::Insert => (range.from(), range.from()),
-            ShellBehavior::Append => (range.to(), range.to()),
-            _ => (range.from(), range.from()),
+        let output_len = output.chars().count();
+
+        let (from, to, deleted_len) = match behavior {
+            ShellBehavior::Replace => (range.from(), range.to(), range.len()),
+            ShellBehavior::Insert => (range.from(), range.from(), 0),
+            ShellBehavior::Append => (range.to(), range.to(), 0),
+            _ => (range.from(), range.from(), 0),
         };
-        ranges.push(Range::new(to, to + output.chars().count()));
+
+        // These `usize`s cannot underflow because selection ranges cannot overlap.
+        // Once the MSRV is 1.66.0 (mixed_integer_ops is stabilized), we can use checked
+        // arithmetic to assert this.
+        let anchor = (to as isize + offset - deleted_len as isize) as usize;
+        let new_range = Range::new(anchor, anchor + output_len).with_direction(range.direction());
+        ranges.push(new_range);
+        offset = offset + output_len as isize - deleted_len as isize;
+
         changes.push((from, to, Some(output)));
     }
 

helix-term/tests/test/commands.rs
@@ -215,3 +215,71 @@ async fn test_multi_selection_paste() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_multi_selection_shell_commands() -> anyhow::Result<()> {
+    // pipe
+    test((
+        platform_line(indoc! {"\
+            #[|lorem]#
+            #(|ipsum)#
+            #(|dolor)#
+            "})
+        .as_str(),
+        "|echo foo<ret>",
+        platform_line(indoc! {"\
+            #[|foo
+            ]#
+            #(|foo
+            )#
+            #(|foo
+            )#
+            "})
+        .as_str(),
+    ))
+    .await?;
+
+    // insert-output
+    test((
+        platform_line(indoc! {"\
+            #[|lorem]#
+            #(|ipsum)#
+            #(|dolor)#
+            "})
+        .as_str(),
+        "!echo foo<ret>",
+        platform_line(indoc! {"\
+            #[|foo
+            ]#lorem
+            #(|foo
+            )#ipsum
+            #(|foo
+            )#dolor
+            "})
+        .as_str(),
+    ))
+    .await?;
+
+    // append-output
+    test((
+        platform_line(indoc! {"\
+            #[|lorem]#
+            #(|ipsum)#
+            #(|dolor)#
+            "})
+        .as_str(),
+        "<A-!>echo foo<ret>",
+        platform_line(indoc! {"\
+            lorem#[|foo
+            ]#
+            ipsum#(|foo
+            )#
+            dolor#(|foo
+            )#
+            "})
+        .as_str(),
+    ))
+    .await?;
+
+    Ok(())
+}