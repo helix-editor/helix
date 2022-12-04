14613f3681e7802f43c272d91571285091e6b9f0
Justin Rhoades <jrhoades93@gmail.com>
2022-12-04 10:49:47 -0700

    Added yuck language and scheme with test file

---
+++ runtime/queries/yuck/highlights.scm
@@ -0,0 +1,59 @@
+; Yuck is the configuration language for EWW widgets
+
+; widgets
+[
+  "widget"
+  "combo-box-text"
+  "expander"
+  "revealer"
+  "checkbox"
+  "color-button"
+  "color-chooser"
+  "scale"
+  "progress"
+  "input"
+  "button"
+  "image"
+  "box"
+  "overlay"
+  "centerbox"
+  "scroll"
+  "eventbox"
+  "label"
+  "literal"
+  "calendar"
+  "transform"
+  "circular-progress"
+  "graph"
+] @keyword
+
+; definitions
+[
+  "var"
+  "defwidget"
+  "defwindow"
+  "defpoll"
+  "deflisten"
+] @keyword.storage.type
+
+[
+  "+"
+  "-"
+  "*"
+  "/"
+  "%"
+  "||"
+  "&&"
+  "!"
+  "?:"
+  "?."
+] @operators
+
+[
+  "("
+  ")"
+  "]"
+  "]"
+  "{"
+  "}"
+] @punction.bracket
