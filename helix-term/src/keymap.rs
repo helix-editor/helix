f187f2d5942c541c5a605136223b50b5fa6a08cd
gibbz00 <gabrielhansson00@gmail.com>
2022-12-17 23:53:07 +0100

    Keymap infobox: Place in correct order from start:
    Infobox body was being filled with description
    then KeyEvent list to only later be iterated over
    for the purpose of flippingin this order.

--- helix-term/src/keymap.rs
+++ helix-term/src/keymap.rs
@@ -68,7 +68,7 @@ impl KeyTrieNode {
     }
 
     pub fn infobox(&self) -> Info {
-        let mut body: Vec<(&str, Vec<KeyEvent>)> = Vec::with_capacity(self.len());
+        let mut body: Vec<(Vec<KeyEvent>, &str)> = Vec::with_capacity(self.len());
         for (&key, trie) in self.iter() {
             let desc = match trie {
                 KeyTrie::Leaf(cmd) => {
@@ -80,11 +80,11 @@ impl KeyTrieNode {
                 KeyTrie::Node(n) => n.name(),
                 KeyTrie::Sequence(_) => "[Multiple commands]",
             };
-            match body.iter().position(|(d, _)| d == &desc) {
+            match body.iter().position(|(_, d)| d == &desc) {
                 Some(pos) => {
-                    body[pos].1.push(key);
+                    body[pos].0.push(key);
                 }
-                None => body.push((desc, Vec::from([key]))),
+                None => body.push((Vec::from([key]), desc)),
             }
         }
 
