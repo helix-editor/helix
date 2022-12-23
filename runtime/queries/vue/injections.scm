a5488c38f660e4904490685af7b1ce814dd4c0a3
farwyler <1705805+farwyler@users.noreply.github.com>
2022-12-23 14:28:53 +0100

    adds vue preprocessor injections

--- runtime/queries/vue/injections.scm
+++ runtime/queries/vue/injections.scm
@@ -8,13 +8,39 @@
   (raw_text) @injection.content)
  (#set! injection.language "javascript"))
 
+; <script>
 ((script_element
-  (raw_text) @injection.content)
- (#set! injection.language "javascript"))
+    (start_tag) @_no_lang
+    (raw_text) @injection.content)
+  (#not-match? @_no_lang "lang=")
+  (#set! injection.language "javascript"))
 
+; <script lang="js|javascript">
+((script_element
+    (start_tag (attribute (quoted_attribute_value (attribute_value) @_lang)))
+    (raw_text) @injection.content)
+    (#match? @_lang "^(js|javascript)$")
+    (#set! injection.language "javascript"))
+
+; <script lang="ts|typescript">
+((script_element
+    (start_tag (attribute (quoted_attribute_value (attribute_value) @_lang)))
+    (raw_text) @injection.content)
+    (#match? @_lang "^(ts|typescript)$")
+    (#set! injection.language "typescript"))
+
+; <style>
 ((style_element
-  (raw_text) @injection.content)
- (#set! injection.language "css"))
+    (start_tag) @_no_lang
+    (raw_text) @injection.content)
+  (#not-match? @_no_lang "lang=")
+  (#set! injection.language "css"))
+
+; <style lang="...">
+((style_element
+    (start_tag (attribute (quoted_attribute_value (attribute_value) @injection.language)))
+    (raw_text) @injection.content)
+    (#match? @injection.language "^(css|scss)$"))
 
 ((comment) @injection.content
  (#set! injection.language "comment"))
