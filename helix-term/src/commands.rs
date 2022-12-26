db663bdb0807fb7b46a0ee057e8ef34e78c70f1a
Nikodem Rabuliński <nikodem@rabulinski.com>
2022-12-26 12:21:10 +0100

    Added commiting to history after executing a command from the palette

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -2509,7 +2509,23 @@ pub fn command_palette(cx: &mut Context) {
                     on_next_key_callback: None,
                     jobs: cx.jobs,
                 };
+                let (view, _) = current!(ctx.editor);
+                let focus = view.id;
+
                 command.execute(&mut ctx);
+
+                if ctx.editor.tree.contains(focus) {
+                    let config = ctx.editor.config();
+                    let mode = ctx.editor.mode();
+                    let view = view_mut!(ctx.editor, focus);
+                    let doc = doc_mut!(ctx.editor, &view.doc);
+
+                    view.ensure_cursor_in_view(doc, config.scrolloff);
+
+                    if mode != Mode::Insert {
+                        doc.append_changes_to_history(view);
+                    }
+                }
             });
             compositor.push(Box::new(overlayed(picker)));
         },
