faec82729a50236f394b046b5e4c11453b247a99
gibbz00 <gabrielhansson00@gmail.com>
2022-12-26 17:19:33 +0100

    keymap.rs file splitting, cleanup and descriptive naming:
    * Descriptive naming and correct use of data structure terminology
      example translations:
        keymap.reverse_map -> keymap.command_list
        keytrienode -> keytrie (struct)
          search->traverse
          merge->merge_keytrie
        keytrie -> keytrienode (enum describing node variants)
          Leaf -> MappableCommand
          Sequence -> CommandSequence
          Node -> KeyTrie
          (A leaf is a node, and the sequence was also a node.
    	  So old naming made absolutely no sense whatsoever.)
    * Splitting parts of the keymap.rs file into
      {keytrienode, keytrie, keymaps, tests}.rs.
      (Should be self-explanatory by looking at the old keymap.rs.)
    * Removed KeytrieNode enum functions node(&self) -> Option<&KeyTrie>
      and node_mut(&mut self) -> Option<&mut KeyTrie> from KeyTrieNode
      as they served no purpose that could not be used from elsewhere.
      Was also a bit strange to return a "parent struct" from an enum
      "building block".
    * Removed getters that could be achieved by making fields public for
      now. (E.g .root(), .map(), .name())
    * Removed keymap.merge() and keytrienode.merge_nodes()
      All merging could be handled by keytrie.merge() and
      keymaps.merge_with_default(), unnecessary to have a second
      unused system that does about the same thing.
      We also don't want functions that can cause panics as
      merge_nodes() could.
    * Initial simplification and formatting of command palette.
      Formatting is done during the creation of the command lists,
      removes the need for the creation and and traversal of an
      intermediary Vec<Vec<String>>.
      Keyevent formatting changed from "(<space>w<C-q>)" to ["space>w>C-q"].
      Clarifying how commands are triggered by moving through
      submenus/keytries.

---
+++ helix-term/src/keymap/tests.rs
@@ -0,0 +1,181 @@
+#[macro_use]
+#[cfg(test)]
+mod tests {
+    use arc_swap::access::Constant;
+    use helix_core::hashmap;
+    use helix_view::{document::Mode, input::KeyEvent};
+    use crate::config::Config;
+    use crate::commands::MappableCommand;
+    use crate::keymap::*;
+    use keymaps::{Keymaps, KeymapResult};
+    use macros::*;
+
+    #[test]
+    #[should_panic]
+    fn duplicate_keys_should_panic() {
+        keymap!({ "Normal mode"
+            "i" => normal_mode,
+            "i" => goto_definition,
+        });
+    }
+
+    #[test]
+    fn check_duplicate_keys_in_default_keymap() {
+        // will panic on duplicate keys, assumes that `Keymaps` uses keymap! macro
+        Keymaps::default();
+    }
+
+    #[test]
+    fn merge_partial_keys() {
+        let config = Config {
+            keys: hashmap! {
+                Mode::Normal => Keymap::new(
+                    keymap!({ "Normal mode"
+                        "i" => normal_mode,
+                        "无" => insert_mode,
+                        "z" => jump_backward,
+                        "g" => { "Merge into goto mode"
+                            "$" => goto_line_end,
+                            "g" => delete_char_forward,
+                        },
+                    })
+                )
+            },
+            ..Default::default()
+        };
+        let mut merged_config = merge_keys(config.clone());
+        assert_ne!(config, merged_config);
+
+        let mut keymap = Keymaps::new(Box::new(Constant(merged_config.keys.clone())));
+        assert_eq!(
+            keymap.get(Mode::Normal, key!('i')),
+            KeymapResult::Matched(MappableCommand::normal_mode),
+            "New mappable command should ovveride default."
+        );
+        assert_eq!(
+            keymap.get(Mode::Normal, key!('无')),
+            KeymapResult::Matched(MappableCommand::insert_mode),
+            "New mappable command should be present in merged keymap."
+        );
+        // Assumes that z is a node in the default keymap
+        assert_eq!(
+            keymap.get(Mode::Normal, key!('z')),
+            KeymapResult::Matched(MappableCommand::jump_backward),
+            "New Mappable command should replace default sub keytrie."
+        );
+
+        let keymap = merged_config.keys.get_mut(&Mode::Normal).unwrap();
+        // Assumes that `g` is a node in default keymap
+        assert_eq!(
+            keymap.root_node.traverse(&[key!('g'), key!('$')]).unwrap(),
+            KeyTrieNode::MappableCommand(MappableCommand::goto_line_end),
+            "Mappable command should be present in merged keytrie."
+        );
+        // Assumes that `gg` is in default keymap
+        assert_eq!(
+            keymap.root_node.traverse(&[key!('g'), key!('g')]).unwrap(),
+            KeyTrieNode::MappableCommand(MappableCommand::delete_char_forward),
+            "Mappable command should replace default in merged keytrie."
+        );
+        // Assumes that `ge` is in default keymap
+        assert_eq!(
+            keymap.root_node.traverse(&[key!('g'), key!('e')]).unwrap(),
+            KeyTrieNode::MappableCommand(MappableCommand::goto_last_line),
+            "Mappable commands from default keytrie should still be present in merged merged keytrie unless overridden."
+        );
+
+        assert!(merged_config.keys.get(&Mode::Normal).unwrap().len() > 1);
+        assert!(merged_config.keys.get(&Mode::Insert).unwrap().len() > 0);
+    }
+
+    #[test]
+    fn order_should_be_set() {
+        let config = Config {
+            keys: hashmap! {
+                Mode::Normal => Keymap::new(
+                    keymap!({ "Normal mode"
+                        "space" => { ""
+                            "s" => { ""
+                                "v" => vsplit,
+                                "c" => hsplit,
+                            },
+                        },
+                    })
+                )
+            },
+            ..Default::default()
+        };
+        let mut merged_config = merge_keys(config.clone());
+        assert_ne!(config, merged_config);
+        let keymap_normal = merged_config.keys.get_mut(&Mode::Normal).unwrap();
+        // Make sure mapping works
+        assert_eq!(
+            keymap_normal
+                .root_node
+                .traverse(&[key!(' '), key!('s'), key!('v')])
+                .unwrap(),
+            KeyTrieNode::MappableCommand(MappableCommand::vsplit),
+            "Mappable command should be present in merged keytrie."
+        );
+    }
+
+    #[test]
+    fn aliased_modes_are_same_in_default_keymap() {
+        let keymaps = Keymaps::default().keymaps;
+        let root = keymaps.load().get(&Mode::Normal).unwrap().root_node.clone();
+        assert_eq!(
+            root.traverse(&[key!(' '), key!('w')]).unwrap(),
+            root.traverse(&["C-w".parse::<KeyEvent>().unwrap()]).unwrap(),
+            "Mismatch for window mode on `Space-w` and `Ctrl-w`."
+        );
+        assert_eq!(
+            root.traverse(&[key!('z')]).unwrap(),
+            root.traverse(&[key!('Z')]).unwrap(),
+            "Mismatch for view mode on `z` and `Z`."
+        );
+    }
+
+    #[test]
+    fn command_list() {
+        let normal_mode = keymap!({ "Normal mode"
+            "i" => insert_mode,
+            "g" => { "Goto"
+                "g" => goto_file_start,
+                "e" => goto_file_end,
+            },
+            "j" | "k" => move_line_down,
+        });
+        let keymap = Keymap::new(normal_mode);
+        let mut command_list = keymap.command_list();
+
+        // sort keybindings in order to have consistent tests
+        // HashMaps can be compared but we can still get different ordering of bindings
+        // for commands that have multiple bindings assigned
+        for v in command_list.values_mut() {
+            v.sort()
+        }
+
+        assert_eq!(
+            command_list,
+            HashMap::from([
+                (    
+                    "insert_mode".to_string(),
+                    vec![key!('i').to_string()]
+                ),
+                (
+                    "goto_file_start".to_string(),
+                    vec![format!("{}>{}", key!('g'), key!('g'))]
+                ),
+                (
+                    "goto_file_end".to_string(),
+                    vec![format!("{}>{}", key!('g'), key!('e'))]
+                ),
+                (
+                    "move_line_down".to_string(),
+                    vec![key!('j').to_string(), key!('k').to_string()]
+                )
+            ]),
+            "Mismatch"
+        )
+    }
+}
