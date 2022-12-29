8918553db57bdfb73bb303098c5a9c9760ba8c81
gibbz00 <gabrielhansson00@gmail.com>
2022-12-29 18:58:53 +0100

    Unify config loading pt 2:
    * Config::merge_in_default_config()
      takes in self as parameter.

--- helix-term/src/main.rs
+++ helix-term/src/main.rs
@@ -128,7 +128,7 @@ FLAGS:
 
     let config = match std::fs::read_to_string(helix_loader::config_file()) {
         Ok(config) => toml::from_str(&config)
-            .map(Config::merge_in_default_keymap)
+            .map(|config: Config| config.merge_in_default_keymap())
             .unwrap_or_else(|err| {
                 eprintln!("Bad config: {}", err);
                 eprintln!("Press <ENTER> to continue with default config");
