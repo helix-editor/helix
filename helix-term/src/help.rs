cab5aac9c8d6fb90783fd2439b3ec1ad86a90a5b
gibbz00 <gabrielhansson00@gmail.com>
2023-01-14 21:32:36 +0100

    help.rs: use named parameters

--- helix-term/src/help.rs
+++ helix-term/src/help.rs
@@ -1,9 +1,8 @@
-    pub fn help() -> String {
- format!(
-        "\
-{} {}
-{}
-{}
+pub fn help() -> String {
+    format!("\
+{pkg_name} {version}
+{authors}
+{description}
 
 USAGE:
     hx [FLAGS] [files]...
@@ -21,15 +20,15 @@ FLAGS:
     -c, --config <file>            Specifies a file to use for configuration
     -v                             Increases logging verbosity each use for up to 3 times
     --log                          Specifies a file to use for logging
-                                   (default file: {})
+                                   (default file: {log_file_path})
     -V, --version                  Prints version information
     --vsplit                       Splits all given files vertically into different windows
     --hsplit                       Splits all given files horizontally into different windows
 ",
-        env!("CARGO_PKG_NAME"),
-        helix_loader::VERSION_AND_GIT_HASH,
-        env!("CARGO_PKG_AUTHORS"),
-        env!("CARGO_PKG_DESCRIPTION"),
-        helix_loader::log_file().display(),
-)   
+        pkg_name = env!("CARGO_PKG_NAME"),
+        version = helix_loader::VERSION_AND_GIT_HASH,
+        authors = env!("CARGO_PKG_AUTHORS"),
+        description = env!("CARGO_PKG_DESCRIPTION"),
+        log_file_path = helix_loader::log_file().display(),
+    )
 }
