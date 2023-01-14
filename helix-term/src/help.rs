6f4301cff95539bb61a5eebc9cbf7087a9d9a073
gibbz00 <gabrielhansson00@gmail.com>
2023-01-14 21:24:40 +0100

    Move --help message to separate file.

---
+++ helix-term/src/help.rs
@@ -0,0 +1,35 @@
+    pub fn help() -> String {
+ format!(
+        "\
+{} {}
+{}
+{}
+
+USAGE:
+    hx [FLAGS] [files]...
+
+ARGS:
+    <files>...    Sets the input file to use, position can also be specified via file[:row[:col]]
+
+FLAGS:
+    -h, --help                     Prints help information
+    --tutor                        Loads the tutorial
+    --health [CATEGORY]            Checks for potential errors in editor setup
+                                   CATEGORY can be a language or one of 'clipboard', 'languages'
+                                   or 'all'. 'all' is the default if not specified.
+    -g, --grammar {{fetch|build}}    Fetches or builds tree-sitter grammars listed in languages.toml
+    -c, --config <file>            Specifies a file to use for configuration
+    -v                             Increases logging verbosity each use for up to 3 times
+    --log                          Specifies a file to use for logging
+                                   (default file: {})
+    -V, --version                  Prints version information
+    --vsplit                       Splits all given files vertically into different windows
+    --hsplit                       Splits all given files horizontally into different windows
+",
+        env!("CARGO_PKG_NAME"),
+        helix_loader::VERSION_AND_GIT_HASH,
+        env!("CARGO_PKG_AUTHORS"),
+        env!("CARGO_PKG_DESCRIPTION"),
+        helix_loader::log_file().display(),
+)   
+}
