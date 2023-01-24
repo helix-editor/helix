8059c107bfa9581c48aa2228963a59292297e8e6
Mog Nesbitt <mog@seriousorange.com>
2023-01-25 07:37:35 +1300

    Rewrite integration test to work with new prompt history system
    C-n would previously always take you back to the last entry; test
    data changed so it will still do this because it matches the currently
    entered prompt.

--- helix-term/tests/test/prompt.rs
+++ helix-term/tests/test/prompt.rs
@@ -4,7 +4,7 @@ use super::*;
 async fn test_history_completion() -> anyhow::Result<()> {
     test_key_sequence(
         &mut AppBuilder::new().build()?,
-        Some(":asdf<ret>:theme d<C-n><tab>"),
+        Some(":testingtheme dark<ret>:theme d<C-n><tab>"),
         Some(&|app| {
             assert!(!app.editor.is_err());
         }),
