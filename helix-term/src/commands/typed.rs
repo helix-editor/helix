d22173e0fd8fda2e7f672f2a7b238aa7ca07efcd
Davide Galassi <davxy@datawok.net>
2023-03-02 18:32:26 +0100

    Cargo fmt

--- helix-term/src/commands/typed.rs
+++ helix-term/src/commands/typed.rs
@@ -1248,7 +1248,7 @@ fn lsp_stop(
 
     for doc in cx.editor.documents_mut() {
         if doc.language_server().map_or(false, |ls| ls.id() == ls_id) {
-            doc.set_language_server(None);       
+            doc.set_language_server(None);
             doc.set_diagnostics(Default::default());
         }
     }
