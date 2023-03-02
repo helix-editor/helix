b52f3c0ebb3ae8f51d1dd473c3127bc5e2af4f65
Pascal Kuthe <pascal.kuthe@semimod.de>
2023-03-02 19:10:47 +0100

    save selection before completion savepoint
    Currently, the selection is not saved/restored when completion
    checkpoints are applied. This is usually fine because undoing changes
    usually restores maps selections back in insert mode. But this is not
    always the case and especially problematic in the presence of
    multi-cursor completions (since completions are applied relative to
    the selection/cursor) and snippets (which can change the selection)

--- helix-term/src/ui/completion.rs
+++ helix-term/src/ui/completion.rs
@@ -193,7 +193,7 @@ impl Completion {
                     );
 
                     // initialize a savepoint
-                    doc.savepoint();
+                    doc.savepoint(&view);
                     doc.apply(&transaction, view.id);
 
                     editor.last_completion = Some(CompleteAction {
