324776509add64a70881a058c31095266059a26f
exp80 <62665970+exp80@users.noreply.github.com>
2023-03-15 19:01:32 +0100

    Fix highlighting in picker with multiple columns

--- helix-term/src/ui/picker.rs
+++ helix-term/src/ui/picker.rs
@@ -812,7 +812,10 @@ impl<T: Item + 'static> Component for Picker<T> {
                 for cell in row.cells.iter_mut() {
                     let spans = match cell.content.lines.get(0) {
                         Some(s) => s,
-                        None => continue,
+                        None => {
+                            cell_start_byte_offset += TEMP_CELL_SEP.len();
+                            continue;
+                        }
                     };
 
                     let mut cell_len = 0;
