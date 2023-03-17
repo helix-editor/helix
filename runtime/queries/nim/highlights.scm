de259bb2de1b4ee358a49216a6012d96684a8889
JJ <git@toki.la>
2023-03-16 17:38:53 -0700

    Reorder, better document, and add edge cases to Nim queries

--- runtime/queries/nim/highlights.scm
+++ runtime/queries/nim/highlights.scm
@@ -1,147 +1,33 @@
-(staticStmt (keyw) @keyword)
-(deferStmt (keyw) @keyword)
-(asmStmt (keyw) @keyword)
-(bindStmt (keyw) @keyword)
-(mixinStmt (keyw) @keyword)
+;; Constants, Comments, and Literals
 
-(blockStmt
-  (keyw) @keyword.control
-  (symbol) @label)
+(comment) @comment.line
+(multilineComment) @comment.block
+(docComment) @comment.documentation
+(multilineDocComment) @comment.block.documentation
+; comments
 
-(ifStmt (keyw) @keyword.control.conditional)
-(whenStmt (keyw) @keyword.control.conditional)
-(elifStmt (keyw) @keyword.control.conditional)
-(elseStmt (keyw) @keyword.control.conditional)
-(caseStmt (keyw) @keyword.control.conditional)
-(ofBranch (keyw) @keyword.control.conditional)
-(inlineIfStmt (keyw) @keyword.control.conditional)
-(inlineWhenStmt (keyw) @keyword.control.conditional)
-; todo: do block
-
-(forStmt
-  (keyw) @keyword.control.repeat
-  (symbol) @variable
-  (keyw) @keyword.control.repeat)
-(whileStmt (keyw) @keyword.control.repeat)
-
-(importStmt
-  (keyw) @keyword.control.import
-  (expr (primary (symbol) @namespace)))
-(importExceptStmt
-  (keyw) @keyword.control.import
-  (expr (primary (symbol) @namespace)))
-(exportStmt
-  (keyw) @keyword.control.import
-  (expr (primary (symbol) @namespace)))
-(fromStmt
-  (keyw) @keyword.control.import
-  (expr (primary (symbol) @namespace)))
-(includeStmt
-  (keyw) @keyword.control.import
-  (expr (primary (symbol) @namespace)))
-; FIXME: entries in std/[one, two] get highlighted as variables
-
-(returnStmt (keyw) @keyword.control.repeat)
-(yieldStmt (keyw) @keyword.control.repeat)
-(discardStmt (keyw) @keyword.control.repeat)
-(breakStmt (keyw) @keyword.control.repeat)
-(continueStmt (keyw) @keyword.control.repeat)
-
-(raiseStmt (keyw) @keyword.control.exception)
-(tryStmt (keyw) @keyword.control.exception)
-(tryExceptStmt (keyw) @keyword.control.exception)
-(tryFinallyStmt (keyw) @keyword.control.exception)
-(inlineTryStmt (keyw) @keyword.control.exception)
-; (inlineTryExceptStmt (keyw) @keyword.control.exception)
-; (inlineTryFinallyStmt (keyw) @keyword.control.exception)
+[(literal) (generalizedLit)] @constant
+[(nil_lit)] @constant.builtin
+[(bool_lit)] @constant.builtin.boolean
+[(char_lit)] @constant.character
+[(char_esc_seq) (str_esc_seq)] @constant.character.escape
+[(custom_numeric_lit)] @constant.numeric
+[(int_lit) (int_suffix)] @constant.numeric.integer
+[(float_lit) (float_suffix)] @constant.numeric.float
+; literals
+; note: somewhat irritatingly for testing, lits have the same syntax highlighting as types
 
 [
-  "and"
-  "or"
-  "xor"
-  "not"
-  "in"
-  "notin"
-  "is"
-  "isnot"
-  "div"
-  "mod"
-  "shl"
-  "shr"
-] @keyword.operator
-; todo: better to just use (operator) and (opr)?
-
-(typeDef
-  (keyw) @keyword.storage.type
-  (symbol) @type)
-
-(primarySuffix
-  (indexSuffix
-    (exprColonEqExprList
-      (exprColonEqExpr
-        (expr
-          (primary
-            (symbol) @type))))))
-; types in brackets, i.e. seq[string]
-; FIXME: this is overzealous. seq[tuple[a, b: int]] matches both a and b when it shouldn't.
-
-(primaryTypeDef
-  (symbol) @type)
-; primary types of type declarations (nested types in brackets are matched with above)
-
-(primaryTypeDesc
-  (symbol) @type)
-; type annotations, on declarations or in objects
-
-(primaryTypeDesc
-  (primaryPrefix
-    (keyw) @type))
-; var types
-
-(genericParamList
-  (genericParam
-    (symbol) @type))
-; types in generic blocks
-
-(enumDecl
-  (keyw) @keyword.storage.type
-  (enumElement
-    (symbol) @type.enum.variant))
-
-(tupleDecl
-  (keyw) @keyword.storage.type)
-
-(objectDecl
-  (keyw) @keyword.storage.type)
-
-(objectPart
-  (symbol) @variable.other.member)
-; object field
-
-(objectCase
-  (keyw) @keyword.control.conditional
-  (symbol) @variable.other.member
-  (objectBranches
-    ; (objectWhen (keyw) @keyword.control.conditional)?
-    (objectElse (keyw) @keyword.control.conditional)?
-    (objectElif (keyw) @keyword.control.conditional)?
-    (objectBranch (keyw) @keyword.control.conditional)?))
-
-(conceptDecl
-  (keyw) @keyword.storage.type
-  (conceptParam
-    (symbol) @variable))
-
-((exprStmt
-  (primary (symbol))
-  (operator) @operator
-  (primary (symbol) @type))
- (#match? @operator "is"))
-; "x is t" means t is either a type or a type variable
-
-; distinct?
-
-[(operator) (opr) "="] @operator
+  (str_lit)
+  (triplestr_lit)
+  (rstr_lit)
+  (generalized_str_lit)
+  (generalized_triplestr_lit)
+  (interpolated_str_lit)
+  (interpolated_triplestr_lit)
+] @string
+; [] @string.regexp
+; string literals
 
 [
   "."
@@ -161,44 +47,199 @@
   "#["
   "]#"
 ] @punctuation.bracket
-(interpolated_str_lit ["&" "{" "}"] @punctuation.special)
+(interpolated_str_lit "&" @punctuation.special)
+(interpolated_str_lit "{" @punctuation.special)
+(interpolated_str_lit "}" @punctuation.special)
+; punctuation
 
-[(literal) (generalizedLit)] @constant
-[(nil_lit)] @constant.builtin
-[(bool_lit)] @constant.builtin.boolean
-[(char_lit)] @constant.character
-[(char_esc_seq) (str_esc_seq)] @constant.character.escape
-[(custom_numeric_lit)] @constant.numeric
-[(int_lit) (int_suffix)] @constant.numeric.integer
-[(float_lit) (float_suffix)] @constant.numeric.float
-; note: somewhat irritatingly for testing, lits have the same syntax highlighting as types
+[
+  "and"
+  "or"
+  "xor"
+  "not"
+  "in"
+  "notin"
+  "is"
+  "isnot"
+  "div"
+  "mod"
+  "shl"
+  "shr"
+] @keyword.operator
+; operators: we list them explicitly to deliminate them from symbolic operators
 
-[(str_lit) (triplestr_lit) (rstr_lit)] @string
-; [] @string.regexp
-[(generalized_str_lit) (generalized_triplestr_lit) (interpolated_str_lit) (interpolated_triplestr_lit)] @string.special
-; [] @string.special.path
-; [] @string.special.url
-; [] @string.special.symbol
-
-(comment) @comment.line
-(multilineComment) @comment.block
-(docComment) @comment.documentation
-(multilineDocComment) @comment.block.documentation
-; comments
+[(operator) (opr) "="] @operator
+; all operators (must come after @keyword.operator)
 
 (pragma) @attribute
+; pragmas
+
+(ERROR) @error
+; syntax or parser errors
+
+
+;; Imports and Exports
+
+(importStmt
+  (keyw) @keyword.control.import
+  (expr (primary (symbol) @namespace))?
+  (expr (primary (arrayConstr (exprColonExprList (exprColonExpr (expr (primary (symbol) @namespace)))))))?)
+(exportStmt
+  (keyw) @keyword.control.import
+  (expr (primary (symbol) @namespace))?
+  (expr (primary (arrayConstr (exprColonExprList (exprColonExpr (expr (primary (symbol) @namespace)))))))?)
+(fromStmt
+  (keyw) @keyword.control.import
+  (expr (primary (symbol) @namespace))?
+  (expr (primary (arrayConstr (exprColonExprList (exprColonExpr (expr (primary (symbol) @namespace)))))))?)
+(includeStmt
+  (keyw) @keyword.control.import
+  (expr (primary (symbol) @namespace))?
+  (expr (primary (arrayConstr (exprColonExprList (exprColonExpr (expr (primary (symbol) @namespace)))))))?)
+(importExceptStmt
+  (keyw) @keyword.control.import
+  (expr (primary (symbol) @namespace))?
+  (expr (primary (arrayConstr (exprColonExprList (exprColonExpr (expr (primary (symbol) @namespace)))))))?)
+; import statements
+; yeah, this is a bit gross.
+
+
+;; Control Flow
+
+(ifStmt (keyw) @keyword.control.conditional)
+(whenStmt (keyw) @keyword.control.conditional)
+(elifStmt (keyw) @keyword.control.conditional)
+(elseStmt (keyw) @keyword.control.conditional)
+(caseStmt (keyw) @keyword.control.conditional)
+(ofBranch (keyw) @keyword.control.conditional)
+(inlineIfStmt (keyw) @keyword.control.conditional)
+(inlineWhenStmt (keyw) @keyword.control.conditional)
+; conditional statements
+; todo: do block
+
+(forStmt
+  . (keyw) @keyword.control.repeat
+  . (symbol) @variable
+  . (keyw) @keyword.control.repeat)
+(whileStmt (keyw) @keyword.control.repeat)
+; loop statements
+
+(returnStmt (keyw) @keyword.control.repeat)
+(yieldStmt (keyw) @keyword.control.repeat)
+(discardStmt (keyw) @keyword.control.repeat)
+(breakStmt (keyw) @keyword.control.repeat)
+(continueStmt (keyw) @keyword.control.repeat)
+; control flow statements
+
+(raiseStmt (keyw) @keyword.control.exception)
+(tryStmt (keyw) @keyword.control.exception)
+(tryExceptStmt (keyw) @keyword.control.exception)
+(tryFinallyStmt (keyw) @keyword.control.exception)
+(inlineTryStmt (keyw) @keyword.control.exception)
+; (inlineTryExceptStmt (keyw) @keyword.control.exception)
+; (inlineTryFinallyStmt (keyw) @keyword.control.exception)
+; exception handling statements
+
+(staticStmt (keyw) @keyword)
+(deferStmt (keyw) @keyword)
+(asmStmt (keyw) @keyword)
+(bindStmt (keyw) @keyword)
+(mixinStmt (keyw) @keyword)
+; miscellaneous blocks
+
+(blockStmt
+  (keyw) @keyword.control
+  (symbol) @label)
+; block statements
+
+
+;; Types and Type Declarations
+
+(typeDef
+  (keyw) @keyword.storage.type
+  (symbol) @type)
+; names of new types type declarations
+
+(exprColonEqExpr
+  . (expr (primary (symbol) @variable))
+  . (expr (primary (symbol) @type)))
+; variables in inline tuple declarations
+
+(primarySuffix
+  (indexSuffix
+    (exprColonEqExprList
+      (exprColonEqExpr
+        (expr
+          (primary
+            (symbol) @type))))))
+; nested types in brackets, i.e. seq[string]
+
+(primaryTypeDef (symbol) @type)
+; primary types of type declarations (NOT nested types)
+
+(primaryTypeDef (primaryPrefix (keyw) @type))
+; for consistency
+
+(primaryTypeDesc (symbol) @type)
+; type annotations, on declarations or in objects
+
+(primaryTypeDesc (primaryPrefix (keyw) @type))
+; var types etc
+
+(genericParamList (genericParam (symbol) @type))
+; types in generic blocks
+
+(enumDecl (keyw) @keyword.storage.type)
+(enumElement (symbol) @type.enum.variant)
+; enum declarations and elements
+
+(tupleDecl (keyw) @keyword.storage.type)
+; tuple declarations
+
+(objectDecl (keyw) @keyword.storage.type)
+(objectPart (symbol) @variable.other.member)
+; object declarations and fields
+
+(objectCase
+  (keyw) @keyword.control.conditional
+  (symbol) @variable.other.member)
+(objectBranch (keyw) @keyword.control.conditional)
+(objectElif (keyw) @keyword.control.conditional)
+(objectElse (keyw) @keyword.control.conditional)
+(objectWhen (keyw) @keyword.control.conditional)
+; variant objects
+
+(conceptDecl (keyw) @keyword.storage.type)
+(conceptParam (keyw) @type)
+(conceptParam (symbol) @variable)
+; concept declarations, parameters, and qualifiers on those parameters
+
+((expr
+  (primary (symbol))
+  (operator) @operator
+  (primary (symbol) @type))
+ (#match? @operator "is"))
+((exprStmt
+  (primary (symbol))
+  (operator) @operator
+  (primary (symbol) @type))
+ (#match? @operator "is"))
+; symbols likely to be types: "x is t" means t is either a type or a type variable
+
+; distinct?
+
+
+;; Functions
 
 (routine
   . (keyw) @keyword.function
   . (symbol) @function)
 ; function declarations
 
-(routineExpr
-  (keyw) @keyword.function)
+(routineExpr (keyw) @keyword.function)
 ; discarded function
 
-(routineExprTypeDesc
-  (keyw) @keyword.function)
+(routineExprTypeDesc (keyw) @keyword.function)
 ; function declarations as types
 
 (primary
@@ -217,6 +258,11 @@
 ; uniform function call syntax calls
 
 (primary
+  (primarySuffix (qualifiedSuffix (symbol) @function.call))
+  . (primarySuffix (cmdCall)))
+; just in case
+
+(primary
   (symbol) @constructor
   (primarySuffix (objectConstr)))
 ; object constructor
@@ -227,20 +273,19 @@
 ; [] @function.macro
 ; [] @function.special
 
-(paramList
-  (paramColonEquals
-    (symbol) @variable.parameter))
+
+;; Variables
+
+(paramList (paramColonEquals (symbol) @variable.parameter))
 ; parameter identifiers
 
 (identColon (ident) @variable.other.member)
 ; named parts of tuples
 
-(symbolColonExpr
-  (symbol) @variable)
+(symbolColonExpr (symbol) @variable)
 ; object constructor parameters
 
-(symbolEqExpr
-  (symbol) @variable)
+(symbolEqExpr (symbol) @variable)
 ; named parameters
 
 (variable
@@ -253,24 +298,21 @@
 ; `result` is an implicit builtin variable inside function scopes
 
 ((primary (symbol) @type)
- (#match? @type "[A-Z].+"))
+ (#match? @type "^[A-Z]"))
 ; assume PascalCase identifiers to be types
 
 ((primary
   (primarySuffix
     (qualifiedSuffix
       (symbol) @type)))
- (#match? @type "[A-Z].+"))
+ (#match? @type "^[A-Z]"))
 ; assume PascalCase member variables to be enum entries
 
 (primary (symbol) @variable)
-; overzealous, matches generic symbols
+; overzealous, matches variables
 
-(primary
-  (primarySuffix
-    (qualifiedSuffix
-      (symbol) @variable.other.member)))
-; overzealous, matches x in foo.x
+(primary (primarySuffix (qualifiedSuffix (symbol) @variable.other.member)))
+; overzealous, matches member variables: i.e. x in foo.x
 
 (keyw) @keyword
 ; more specific matches are done above whenever possible
