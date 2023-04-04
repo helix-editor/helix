50b5d9a53031004fc3c7f18950c0c192a16d1fe1
Danillo Melo <danillos@gmail.com>
2023-04-04 10:59:17 -0300

    update ruby highlights

--- runtime/queries/ruby/injections.scm
+++ runtime/queries/ruby/injections.scm
@@ -1,2 +1,8 @@
 ((comment) @injection.content
  (#set! injection.language "comment"))
+
+((heredoc_beginning) @name 
+ (heredoc_body 
+  (heredoc_content) @injection.content
+   (#set! injection.language "sql")) 
+ (#eq? @name "<<~SQL"))
