513fe57c8df0935805397e1d1c93f9188dadf834
ff2400t <84376771+ff2400t@users.noreply.github.com>
2023-05-01 16:54:01 +0530

    added support for self closing elements in xml

--- runtime/queries/xml/textobjects.scm
+++ runtime/queries/xml/textobjects.scm
@@ -1,3 +1,5 @@
-(element (start_tag) (_)* @xml_element.inside (end_tag)) @xml_element.around
+(element (start_tag) (_)* @xml_element.inside (end_tag))
+
+(element) @xml_element.around
 
 (comment) @comment.around   
