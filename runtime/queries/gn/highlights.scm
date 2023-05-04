155df00d7678f39637166fc86be17d21a88ba7ff
Dan Field <dnfield@google.com>
2023-05-04 14:22:15 -0700

    Review

--- runtime/queries/gn/highlights.scm
+++ runtime/queries/gn/highlights.scm
@@ -12,18 +12,18 @@
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
 
-(identifier) @variable
+(identifier) @variable.builtin
 ;((identifier) @variable (#is? @variable definition)) ; This doesn't work
 
 ; This is really buggy
 ; (call target: (identifier) @_target (#eq? @_target "declare_args")
 ;     (block (assignment target: (identifier) @parameter)))
 
-(scope_access field: (_) @field)
+(scope_access field: (_) @variable.other.member)
 
 (call target: (_) @function)
 
-[ "if" "else" ] @conditional
+[ "if" "else" ] @keyword.control.conditional
 
 [
     (assign_op)
@@ -39,10 +39,9 @@
 [ "." "," ] @punctuation.delimiter
 
 (string) @string
-(string_escape) @string.escape
-(string_expansion [ "$" "${" "}" ] @string.escape)
-[ (integer) (hex) ] @number
-(boolean) @boolean
+(string_escape) @constant.character.escape
+(string_expansion [ "$" "${" "}" ] @constant.character.escape)
+[ (integer) (hex) ] @constant.numeric
+(boolean) @constant.builtin.boolean
 
-(comment) @comment
-((comment) @todo (#match? @todo "TODO")) ; This doesn't work
+((comment) @injection.content (#set! injection.language "comment"))
--- runtime/queries/gn/locals.scm
+++ runtime/queries/gn/locals.scm
@@ -12,5 +12,5 @@
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
 
-(source_file) @scope
-(assignment target: (identifier) @definition.var)
+(source_file) @local.scope
+(assignment target: (identifier) @local.definition.var)
