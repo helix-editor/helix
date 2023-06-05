67099d1a77f6868e5774b84a3e220d719747d02c
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2023-06-05 11:30:45 +0200

    feedback: fix bug, use  to ensure it only runs when there's no syntax

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -4534,11 +4534,10 @@ fn match_brackets(cx: &mut Context) {
 
     let selection = doc.selection(view.id).clone().transform(|range| {
         let pos = range.cursor(text_slice);
-        if let Some(matched_pos) = doc
-            .syntax()
-            .and_then(|syntax| match_brackets::find_matching_bracket_fuzzy(syntax, text, pos))
-            .or_else(|| match_brackets::find_matching_bracket_current_line_plaintext(text, pos))
-        {
+        if let Some(matched_pos) = doc.syntax().map_or_else(
+            || match_brackets::find_matching_bracket_current_line_plaintext(text, pos),
+            |syntax| match_brackets::find_matching_bracket_fuzzy(syntax, text, pos),
+        ) {
             range.put_cursor(text_slice, matched_pos, is_select)
         } else {
             range
