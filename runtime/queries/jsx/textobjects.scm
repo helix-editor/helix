be1aba8d02d0341f3fa82108482b63e20d1414eb
Yusuph Gammut <metaskopia@gmail.com>
2023-06-27 20:58:52 -0500

    Remove information comments and add Readme to ecma

--- runtime/queries/jsx/textobjects.scm
+++ runtime/queries/jsx/textobjects.scm
@@ -1,14 +1,3 @@
-; Ecma based languages share many traits. Because of this we want to share as
-; many queries as possible while avoiding nested inheritance that can make
-; query behavior unpredictable due to unexpected precedence. To achieve that,
-; some ecma related languages have "public" and "private" versions that share
-; the same name, but the "private" version name starts with an underscore (with
-; the exception of ecma, that works as the base "private" language). This
-; allows the "private" versions to host the specific queries of the language
-; excluding any "inherits" statement, in order to make them safe to be
-; inherited by the "public" version of the same language and other languages
-; as well.
-; If you plan to add queries to this language, please consider adding them to
-; any of the inherited languages listed below.
+; See runtime/queries/ecma/README.md for more info.
 
 ; inherits: _jsx,_javascript,ecma
