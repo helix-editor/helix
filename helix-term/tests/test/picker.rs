ba7f3128e1e1a22fe688f1e6484a41472f258fd3
woojiq <yurii.shymon@gmail.com>
2023-08-01 12:32:48 +0300

    fix: picker integrational test

--- helix-term/tests/test/picker.rs
+++ helix-term/tests/test/picker.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use helix_core::Range;
+use helix_core::{path::get_canonicalized_path, Range};
 use helix_loader::{current_working_dir, set_current_working_dir};
 use helix_view::{current_ref, editor::Action};
 use tempfile::{Builder, TempDir};
@@ -28,13 +28,18 @@ async fn test_picker_alt_ret() -> anyhow::Result<()> {
         Builder::new().prefix("1").tempfile_in(&tmp_dir)?,
         Builder::new().prefix("2").tempfile_in(&tmp_dir)?,
     ];
-    fs::write(files[0].path(), "1\n2\n3\n4")?;
-    fs::write(files[1].path(), "first\nsecond")?;
+    let paths = files
+        .iter()
+        .map(|f| get_canonicalized_path(f.path()).unwrap())
+        .collect::<Vec<_>>();
+
+    fs::write(&paths[0], "1\n2\n3\n4")?;
+    fs::write(&paths[1], "first\nsecond")?;
 
     log::debug!(
         "created and writed two temporary files: {:?} & {:?}",
-        files[0].path(),
-        files[1].path()
+        paths[0],
+        paths[1]
     );
 
     // Manually open to save the offset, otherwise we won't be able to change the state in the Fn trait
@@ -50,7 +55,7 @@ async fn test_picker_alt_ret() -> anyhow::Result<()> {
                 Some("<A-ret><esc>"),
                 Some(&|app| {
                     let (view, doc) = current_ref!(app.editor);
-                    assert_eq!(doc.path().unwrap(), files[0].path());
+                    assert_eq!(doc.path().unwrap(), &paths[0]);
                     let select_ranges = doc.selection(view.id).ranges();
                     assert_eq!(select_ranges[0], Range::new(0, 1));
                     assert_eq!(view.offset, view_offset);
@@ -60,7 +65,7 @@ async fn test_picker_alt_ret() -> anyhow::Result<()> {
                 Some(":buffer<minus>next<ret>"),
                 Some(&|app| {
                     let (view, doc) = current_ref!(app.editor);
-                    assert_eq!(doc.path().unwrap(), files[1].path());
+                    assert_eq!(doc.path().unwrap(), &paths[1]);
                     let select_ranges = doc.selection(view.id).ranges();
                     assert_eq!(select_ranges.len(), 1);
                     assert_eq!(select_ranges[0], Range::new(6, 12));
