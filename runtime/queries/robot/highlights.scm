d4e18d41bdb57d5540e6a6fecb61d8d1909f593c
Tomas Sandven <tomas@sandven.email>
2023-08-17 00:00:43 +0200

    Update tree-sitter-robot

--- runtime/queries/robot/highlights.scm
+++ runtime/queries/robot/highlights.scm
@@ -1,21 +1,57 @@
-(comment) @comment
-(ellipses) @punctuation.delimiter
+[
+  (comment)
+  (extra_text)
+] @comment
 
-(section_header) @keyword
-(extra_text) @comment
-
-(setting_statement) @keyword
+[
+  (section_header)
+  (setting_statement)
+  (keyword_setting)
+  (test_case_setting)
+] @keyword
 
 (variable_definition (variable_name) @variable)
-
 (keyword_definition (name) @function)
-(keyword_definition (body (keyword_setting) @keyword))
+(test_case_definition (name) @function)
 
-(test_case_definition (name) @property)
+(keyword_invocation (keyword) @function.call)
+(ellipses) @punctuation.delimiter
 
-(keyword_invocation (keyword) @function)
+(text_chunk) @string
+(inline_python_expression) @string.special
+[
+  (scalar_variable)
+  (list_variable)
+  (dictionary_variable)
+] @variable
 
-(argument (text_chunk) @string)
-(argument (scalar_variable) @string.special)
-(argument (list_variable) @string.special)
-(argument (dictionary_variable) @string.special)
+; Control structures
+
+[
+  "FOR"
+  "IN"
+  "IN RANGE"
+  "IN ENUMERATE"
+  "IN ZIP"
+  (break_statement)
+  (continue_statement)
+] @repeat
+(for_statement "END" @repeat)
+
+"WHILE" @repeat
+(while_statement "END" @repeat)
+
+[
+  "IF"
+  "ELSE IF"
+] @conditional
+(if_statement "END" @conditional)
+(if_statement (else_statement "ELSE" @conditional))
+
+[
+  "TRY"
+  "EXCEPT"
+  "FINALLY"
+] @exception
+(try_statement "END" @exception)
+(try_statement (else_statement "ELSE" @exception))
