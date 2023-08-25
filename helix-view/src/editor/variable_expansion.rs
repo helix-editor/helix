3bf311a5ad18e7e95b5f0374c41476547fb3f6d6
Jesus R <roldanr.jesus@gmail.com>
2023-08-24 21:57:05 -0600

    Change expansion method

--- helix-view/src/editor/variable_expansion.rs
+++ helix-view/src/editor/variable_expansion.rs
@@ -2,182 +2,127 @@ use std::borrow::Cow;
 
 use crate::Editor;
 
-static EXPAND_VARIABLES_REGEX: once_cell::sync::Lazy<helix_core::regex::Regex> =
-    once_cell::sync::Lazy::new(|| {
-        helix_core::regex::Regex::new(r"%(\w+)\{([^{}]*(\{[^{}]*\}[^{}]*)*)\}").unwrap()
-    });
-
 pub fn expand_variables<'a>(editor: &Editor, input: &'a str) -> anyhow::Result<Cow<'a, str>> {
     let (view, doc) = current_ref!(editor);
     let shell = &editor.config().shell;
 
-    replace_all(
-        &EXPAND_VARIABLES_REGEX,
-        Cow::Borrowed(input),
-        move |keyword, body| match keyword.trim() {
-            "val" => match body.trim() {
-                "filename" => Ok(doc
-                    .path()
-                    .and_then(|it| it.to_str())
-                    .unwrap_or(crate::document::SCRATCH_BUFFER_NAME)
-                    .to_string()),
-                "dirname" => doc
-                    .path()
-                    .and_then(|p| p.parent())
-                    .and_then(std::path::Path::to_str)
-                    .map_or(
-                        Err(anyhow::anyhow!("Current buffer has no path or parent")),
-                        |v| Ok(v.to_string()),
-                    ),
-                "line_number" => Ok((doc
-                    .selection(view.id)
-                    .primary()
-                    .cursor_line(doc.text().slice(..))
-                    + 1)
-                .to_string()),
-                _ => anyhow::bail!("Unknown variable: {body}"),
-            },
-            "sh" => tokio::task::block_in_place(move || {
-                helix_lsp::block_on(async move {
-                    let args = &expand_variables(editor, body)?[..];
+    let mut output: Option<String> = None;
 
-                    let mut command = tokio::process::Command::new(&shell[0]);
-                    command.args(&shell[1..]).arg(args);
+    let mut chars = input.char_indices();
+    let mut last_push_end: usize = 0;
 
-                    let output = command
-                        .output()
-                        .await
-                        .map_err(|_| anyhow::anyhow!("Shell command failed: {args}"))?;
+    while let Some((index, char)) = chars.next() {
+        if char == '%' {
+            if let Some((_, char)) = chars.next() {
+                if char == '{' {
+                    while let Some((end, char)) = chars.next() {
+                        if char == '}' {
+                            if output == None {
+                                output = Some(String::with_capacity(input.len()))
+                            }
 
-                    if output.status.success() {
-                        String::from_utf8(output.stdout)
-                            .map_err(|_| anyhow::anyhow!("Process did not output valid UTF-8"))
-                    } else if output.stderr.is_empty() {
-                        Err(anyhow::anyhow!("Shell command failed: {args}"))
-                    } else {
-                        let stderr = String::from_utf8_lossy(&output.stderr);
+                            if let Some(o) = output.as_mut() {
+                                o.push_str(&input[last_push_end..index]);
+                                last_push_end = end + 1;
 
-                        Err(anyhow::anyhow!("{stderr}"))
+                                let value = match &input[index + 2..end] {
+                                    "filename" => doc
+                                        .path()
+                                        .and_then(|it| it.to_str())
+                                        .unwrap_or(crate::document::SCRATCH_BUFFER_NAME)
+                                        .to_owned(),
+                                    "dirname" => doc
+                                        .path()
+                                        .and_then(|p| p.parent())
+                                        .and_then(std::path::Path::to_str)
+                                        .unwrap()
+                                        .to_owned(),
+                                    "linenumber" => (doc
+                                        .selection(view.id)
+                                        .primary()
+                                        .cursor_line(doc.text().slice(..))
+                                        + 1)
+                                    .to_string(),
+                                    _ => anyhow::bail!("Unknown variable"),
+                                };
+
+                                o.push_str(&value);
+
+                                break;
+                            }
+                        }
                     }
-                })
-            }),
-            _ => anyhow::bail!("Unknown keyword {keyword}"),
-        },
-    )
-}
+                } else if char == 's' {
+                    if let (Some((_, 'h')), Some((_, '{'))) = (chars.next(), chars.next()) {
+                        let mut right_bracket_remaining = 1;
+                        while let Some((end, char)) = chars.next() {
+                            if char == '}' {
+                                right_bracket_remaining -= 1;
 
-// Copy of regex::Regex::replace_all to allow using result in the replacer function
-fn replace_all<'a>(
-    regex: &helix_core::regex::Regex,
-    text: Cow<'a, str>,
-    matcher: impl Fn(&str, &str) -> anyhow::Result<String>,
-) -> anyhow::Result<Cow<'a, str>> {
-    let mut it = regex.captures_iter(&text).peekable();
+                                if right_bracket_remaining == 0 {
+                                    if output == None {
+                                        output = Some(String::with_capacity(input.len()))
+                                    }
 
-    if it.peek().is_none() {
-        return Ok(text);
-    }
+                                    if let Some(o) = output.as_mut() {
+                                        let body =
+                                            expand_variables(editor, &input[index + 4..end])?;
 
-    let mut new = String::with_capacity(text.len());
-    let mut last_match = 0;
+                                        let output = tokio::task::block_in_place(move || {
+                                            helix_lsp::block_on(async move {
+                                                let mut command =
+                                                    tokio::process::Command::new(&shell[0]);
+                                                command.args(&shell[1..]).arg(&body[..]);
 
-    for cap in it {
-        let m = cap.get(0).unwrap();
-        new.push_str(&text[last_match..m.start()]);
+                                                let output =
+                                                    command.output().await.map_err(|_| {
+                                                        anyhow::anyhow!(
+                                                            "Shell command failed: {body}"
+                                                        )
+                                                    })?;
 
-        let replace = matcher(cap.get(1).unwrap().as_str(), cap.get(2).unwrap().as_str())?;
+                                                if output.status.success() {
+                                                    String::from_utf8(output.stdout).map_err(|_| {
+                                                        anyhow::anyhow!(
+                                                            "Process did not output valid UTF-8"
+                                                        )
+                                                    })
+                                                } else if output.stderr.is_empty() {
+                                                    Err(anyhow::anyhow!(
+                                                        "Shell command failed: {body}"
+                                                    ))
+                                                } else {
+                                                    let stderr =
+                                                        String::from_utf8_lossy(&output.stderr);
 
-        new.push_str(&replace);
+                                                    Err(anyhow::anyhow!("{stderr}"))
+                                                }
+                                            })
+                                        });
+                                        o.push_str(&input[last_push_end..index]);
+                                        last_push_end = end + 1;
 
-        last_match = m.end();
-    }
+                                        o.push_str(&output?);
 
-    new.push_str(&text[last_match..]);
-
-    replace_all(regex, Cow::Owned(new), matcher)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn matcher(keyword: &str, body: &str) -> anyhow::Result<String> {
-        match keyword {
-            "val" => match body {
-                "filename" => Ok(String::from("[scratch]")),
-                "dirname" => Ok(String::from("[parent_dir]")),
-                _ => Err(anyhow::anyhow!("Unknown variable")),
-            },
-            "sh" => Ok(format!(
-                "|{}|",
-                replace_all(&EXPAND_VARIABLES_REGEX, Cow::Borrowed(body), matcher)?
-            )),
-            _ => Err(anyhow::anyhow!("Unknown keyword")),
+                                        break;
+                                    }
+                                }
+                            } else if char == '{' {
+                                right_bracket_remaining += 1;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
-    // Doesn't allocate for non-matching input
-    #[test]
-    fn variable_expansion_does_not_allocate() {
-        let input = "cd dir";
-        let cow: Cow<str> = Cow::Borrowed(input);
-
-        assert!(matches!(
-            replace_all(&EXPAND_VARIABLES_REGEX, cow, matcher).unwrap(),
-            Cow::Borrowed(_)
-        ));
+    if let Some(o) = output.as_mut() {
+        o.push_str(&input[last_push_end..]);
     }
 
-    // Does allocate for matching input
-    #[test]
-    fn variable_expansion_does_allocate() {
-        let input = "cd %val{dirname}";
-        let cow: Cow<str> = Cow::Borrowed(input);
-
-        assert!(matches!(
-            replace_all(&EXPAND_VARIABLES_REGEX, cow, matcher).unwrap(),
-            Cow::Owned(_)
-        ));
-    }
-
-    #[test]
-    fn variable_expansion_fails() {
-        assert!(replace_all(
-            &EXPAND_VARIABLES_REGEX,
-            Cow::Owned(String::from("%key{dirname}")),
-            matcher
-        )
-        .is_err());
-    }
-
-    #[test]
-    fn variable_expansion_succeeds() {
-        let list: Vec<(Cow<str>, String)> = vec![
-            (
-                Cow::Owned(String::from("%val{filename}")),
-                String::from("[scratch]"),
-            ),
-            (
-                Cow::Owned(String::from("%sh{body}")),
-                String::from("|body|"),
-            ),
-            (
-                Cow::Owned(String::from("%sh{cp %val{filename} %val{dirname}/../copy}")),
-                String::from("|cp [scratch] [parent_dir]/../copy|"),
-            ),
-            (
-                Cow::Owned(String::from(
-                    "%sh{%sh{cat %val{filename}} | grep test >> %val{dirname}/copy}",
-                )),
-                String::from("||cat [scratch]| | grep test >> [parent_dir]/copy|"),
-            ),
-        ];
-
-        for item in list {
-            assert_eq!(
-                replace_all(&EXPAND_VARIABLES_REGEX, item.0, matcher).unwrap(),
-                item.1
-            );
-        }
+    match output {
+        Some(o) => Ok(std::borrow::Cow::Owned(o)),
+        None => Ok(std::borrow::Cow::Borrowed(input)),
     }
 }
