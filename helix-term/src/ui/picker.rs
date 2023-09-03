01184ca358eb91ec5da1f4893ee72d760aee7814
Anshul <anshulusr@gmail.com>
2023-09-03 18:15:26 +0530

    Fixed confusing ASCII diagram

--- helix-term/src/ui/picker.rs
+++ helix-term/src/ui/picker.rs
@@ -824,7 +824,7 @@ impl<T: Item + 'static + Send + Sync> Component for Picker<T> {
         //        | title |
         // +------+--+ +--+------+
         // |prompt   | |preview  |
-        // +---------+ |         |
+        // |---------| |         |
         // |picker   | |         |
         // |         | |         |
         // +---------+ +---------+
