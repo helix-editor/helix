f520b16fcaa2bc13d8d72c09694d88717055d0fd
A-Walrus <58790821+A-Walrus@users.noreply.github.com>
2023-09-25 18:42:42 +0300

    Style Bold/Italic/Strikethrough markdown in docs (#8385)
    * Style Bold/Italic/Strikthrough markdown in docs
    
    * Flatten to single match

--- helix-term/src/ui/markdown.rs
+++ helix-term/src/ui/markdown.rs
@@ -14,6 +14,7 @@ use helix_core::{
 };
 use helix_view::{
     graphics::{Margin, Rect, Style},
+    theme::Modifier,
     Theme,
 };
 
@@ -275,17 +276,21 @@ impl Markdown {
                         );
                         lines.extend(tui_text.lines.into_iter());
                     } else {
-                        let style = if let Some(Tag::Heading(level, ..)) = tags.last() {
-                            match level {
+                        let style = match tags.last() {
+                            Some(Tag::Heading(level, ..)) => match level {
                                 HeadingLevel::H1 => heading_styles[0],
                                 HeadingLevel::H2 => heading_styles[1],
                                 HeadingLevel::H3 => heading_styles[2],
                                 HeadingLevel::H4 => heading_styles[3],
                                 HeadingLevel::H5 => heading_styles[4],
                                 HeadingLevel::H6 => heading_styles[5],
+                            },
+                            Some(Tag::Emphasis) => text_style.add_modifier(Modifier::ITALIC),
+                            Some(Tag::Strong) => text_style.add_modifier(Modifier::BOLD),
+                            Some(Tag::Strikethrough) => {
+                                text_style.add_modifier(Modifier::CROSSED_OUT)
                             }
-                        } else {
-                            text_style
+                            _ => text_style,
                         };
                         spans.push(Span::styled(text, style));
                     }
