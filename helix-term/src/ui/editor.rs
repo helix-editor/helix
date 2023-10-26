47751cebec6c7ddadac11c018a6d83c8afd47c72
makemeunsee <marc-antoine.galilee@etik.com>
2023-10-26 20:59:25 +0200

    rebased on master

--- helix-term/src/ui/editor.rs
+++ helix-term/src/ui/editor.rs
@@ -372,6 +372,7 @@ impl EditorView {
         let mut warning_vec = Vec::new();
         let mut error_vec = Vec::new();
 
+        #[cfg(feature = "dap_lsp")]
         for diagnostic in doc.shown_diagnostics() {
             // Separate diagnostics into different Vecs by severity.
             let (vec, scope) = match diagnostic.severity {
