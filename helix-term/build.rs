778e1e40c464f4f5ef7e5905948beb76be15260e
makemeunsee <marc-antoine.galilee@etik.com>
2023-10-26 20:59:25 +0200

    wasm32 build fix & GH pipeline

--- helix-term/build.rs
+++ helix-term/build.rs
@@ -1,7 +1,13 @@
 use helix_loader::grammar::{build_grammars, fetch_grammars};
 
 fn main() {
-    #[cfg(not(target_arch = "wasm32"))]
+    let wasm32_build = match std::env::var("CARGO_CFG_TARGET_ARCH") {
+        Ok(s) => s == "wasm32",
+        _ => false,
+    };
+    if wasm32_build {
+        return;
+    }
     if std::env::var("HELIX_DISABLE_AUTO_GRAMMAR_BUILD").is_err() {
         fetch_grammars().expect("Failed to fetch tree-sitter grammars");
         build_grammars(Some(std::env::var("TARGET").unwrap()))
