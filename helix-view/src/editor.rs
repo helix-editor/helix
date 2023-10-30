c4d1d0908c684c849e17bb5525a659868aefbb5e
Muhammad <tjmnkrajyej@gmail.com>
2023-10-30 00:38:52 +0000

    Add default value for `Config::indent`.

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -302,8 +302,7 @@ pub struct Config {
     pub rulers: Vec<u16>,
     #[serde(default)]
     pub whitespace: WhitespaceConfig,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub indent: Option<IndentationConfiguration>,
+    pub indent: IndentationConfiguration,
     /// Persistently display open buffers along the top
     pub bufferline: BufferLine,
     /// Vertical indent width guides.
@@ -877,7 +876,7 @@ impl Default for Config {
             terminal: get_terminal_provider(),
             rulers: Vec::new(),
             whitespace: WhitespaceConfig::default(),
-            indent: None,
+            indent: IndentationConfiguration::default(),
             bufferline: BufferLine::default(),
             indent_guides: IndentGuidesConfig::default(),
             color_modes: false,
