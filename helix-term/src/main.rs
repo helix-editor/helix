f670a34edc624a40f12757226f45576a29b6ad74
Bjorn Ove Hay Andersen <bjrnove@gmail.com>
2023-10-31 08:00:38 +0100

    Resolve args.files before changing directory

--- helix-term/src/main.rs
+++ helix-term/src/main.rs
@@ -116,6 +116,11 @@ FLAGS:
 
     setup_logging(args.verbosity).context("failed to initialize logging")?;
 
+    // Before setting the working directory, resolve all the paths in args.files
+    for (path, _) in args.files.iter_mut() {
+        *path = helix_core::path::get_canonicalized_path(path);
+    }
+
     // NOTE: Set the working directory early so the correct configuration is loaded. Be aware that
     // Application::new() depends on this logic so it must be updated if this changes.
     if let Some(path) = &args.working_directory {
