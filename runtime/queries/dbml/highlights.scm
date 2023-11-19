4b5133149cfa8306312d4fa742cca937eee959db
ghashy <obsidian.musicwork@gmail.com>
2023-11-19 13:45:21 +0300

    DBML language support, highlights.scm added

---
+++ runtime/queries/dbml/highlights.scm
@@ -0,0 +1,48 @@
+
+; comments highlighting
+(comment) @comment
+
+; keyword highlighting
+(keyword_def) @keyword
+(keyword_enum) @keyword
+(keyword_ref) @keyword
+
+; identify blocks and definitions
+(block) @class
+(definition) @function
+
+; for identifiers
+(identifier) @variable
+(type) @keyword
+
+; Highlight special types for database/data types
+("Project" ) @type
+("Table" ) @type
+("TableGroup" ) @type
+("database_type" ) @variable
+
+; Index related highlighting
+(index_block) @class
+
+; string and number constants
+("'''") @string.escape
+(string) @string
+(number) @constant  
+
+; brackets
+[
+  "("
+  ")"
+  "{"
+  "}"
+  "["
+  "]"
+] @punctuation.bracket
+
+; brackets
+[
+  ":"
+  "."
+  ","
+] @punctuation.delimeter
+
