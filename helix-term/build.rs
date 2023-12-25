9aa49cae09edc99793008da6779e3ecc7a50bdee
NewtonChutney <70827815+NewtonChutney@users.noreply.github.com>
2023-12-25 22:34:43 +0530

    injecting the icon through a resource file, no extra deps

--- helix-term/build.rs
+++ helix-term/build.rs
@@ -6,4 +6,14 @@ fn main() {
         build_grammars(Some(std::env::var("TARGET").unwrap()))
             .expect("Failed to compile tree-sitter grammars");
     }
+
+    // link icon to windows executable
+    if cfg!(target_os = "windows") {
+        // fetch manifest dir from env var set by Cargo:
+        let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR should be set by Cargo");
+
+        // link against the helix-icon-windows library from contrib dir:
+        println!("cargo:rustc-link-search=native={}", manifest_dir.replace("helix-term", "contrib"));
+        println!("cargo:rustc-link-lib=dylib=helix-icon-windows");
+    }
 }
