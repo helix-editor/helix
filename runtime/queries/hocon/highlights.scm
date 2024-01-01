cd2414b07b6bcdb97de904d8e59fb2b0310393c9
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2024-01-02 01:19:02 +0200

    Add HOCON language support

---
+++ runtime/queries/hocon/highlights.scm
@@ -0,0 +1,33 @@
+(comment) @comment
+
+(null) @constant.builtin
+[(true) (false)] @constant.builtin.boolean
+(number) @constant.numeric
+(string) @string
+(multiline_string) @string
+(string (escape_sequence) @constant.character.escape)
+(unquoted_string) @string
+
+(value [":" "=" "+=" ] @operator)
+
+(substitution (_) @string)
+(substitution ["${" "${?" "}"] @punctuation.special)
+
+[ 
+  "url"
+  "file"
+  "classpath"
+  "required"
+] @function.builtin
+
+(include "include" @include)
+
+[ "(" ")" "[" "]" "{" "}" ]  @punctuation.bracket
+
+(unit) @keyword
+(path (_) @keyword)
+(unquoted_path "." @punctuation.delimiter)
+[ "," ] @punctuation.delimiter
+
+(ERROR) @error
+
