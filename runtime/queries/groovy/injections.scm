53570dbf60aeb23d93df9421329a811131082fb7
Volodymyr Chernetskyi <volodymyr_chernetskyi@goodyear.com>
2024-01-16 19:44:28 +0200

    Simplify Groovy injections
    Co-authored-by: Michael Davis <mcarsondavis@gmail.com>

--- runtime/queries/groovy/injections.scm
+++ runtime/queries/groovy/injections.scm
@@ -1,5 +1,2 @@
-((block_comment) @injection.content
- (#set! injection.language "comment"))
-
-((line_comment) @injection.content
+([(line_comment) (block_comment)] @injection.content
  (#set! injection.language "comment"))
