62208821c9a734dfbbac84cd096b22718a0765f0
TheRealLorenz <lollobellina@gmail.com>
2024-02-08 19:33:24 +0100

    Update lsp-info appearence

--- helix-term/src/ui/lsp_info.rs
+++ helix-term/src/ui/lsp_info.rs
@@ -1,6 +1,8 @@
+use std::fmt::Display;
+
 use crate::compositor::{Component, Compositor, Context, EventResult};
 use crate::key;
-use helix_core::syntax::LanguageServerConfiguration;
+use helix_core::syntax::{FormatterConfiguration, LanguageServerConfiguration};
 use helix_view::graphics::Rect;
 use helix_view::info::{Info, Location};
 use helix_view::input::Event;
@@ -9,93 +11,148 @@ use tui::buffer::Buffer as Surface;
 
 pub struct LspInfo(Info);
 
-struct ServerOk;
-struct ServerError(String);
-
-struct ServerState<'a> {
+struct ServerInfo<'a> {
     name: &'a str,
-    status: Result<ServerOk, ServerError>,
+    config: &'a LanguageServerConfiguration,
+    client: Option<&'a helix_lsp::Client>,
 }
 
 impl LspInfo {
     pub fn new(editor: &Editor) -> Self {
-        LspInfo(Info::new(
+        Self(Info::new(
             "Lsp Info",
             LspInfo::get_info(editor),
             Location::Center,
         ))
     }
 
-    fn get_server_states(editor: &Editor) -> Vec<ServerState> {
+    fn get_server_info(editor: &Editor) -> Vec<ServerInfo> {
         let doc = doc!(editor);
 
-        let connected_names = doc
-            .language_servers()
-            .map(|ls| ls.name())
-            .collect::<Vec<_>>();
-
-        let server_configs = editor.syn_loader.language_server_configs();
-
-        doc.language_config()
+        let configured_names = doc
+            .language_config()
             .map(|config| {
                 config
                     .language_servers
                     .iter()
-                    .map(|ls| match connected_names.contains(&ls.name.as_str()) {
-                        true => ServerState {
-                            name: &ls.name,
-                            status: Ok(ServerOk),
-                        },
-                        false => ServerState {
-                            name: &ls.name,
-                            status: Err(ServerError(LspInfo::get_error_message(
-                                &ls.name,
-                                server_configs.get(&ls.name),
-                            ))),
-                        },
-                    })
+                    .map(|ls| ls.name.as_str())
                     .collect::<Vec<_>>()
             })
-            .unwrap_or_default()
+            .unwrap_or_default();
+
+        editor
+            .syn_loader
+            .language_server_configs()
+            .iter()
+            .filter(|(name, _)| configured_names.contains(&name.as_str()))
+            .map(|(name, config)| {
+                let client = doc.language_servers().find(|client| client.name() == name);
+
+                ServerInfo {
+                    name,
+                    config,
+                    client,
+                }
+            })
+            .collect::<Vec<_>>()
+    }
+
+    fn format_info(info: ServerInfo) -> String {
+        let (icon, id, message) = match info.client {
+            Some(client) => ("✓", Some(client.id()), None),
+            None => ("✘", None, Some(Self::get_error_message(&info))),
+        };
+        let command = info.config.command.clone()
+            + info
+                .config
+                .args
+                .iter()
+                .map(|s| String::from(" ") + s.as_str())
+                .collect::<String>()
+                .as_str();
+
+        format!(
+            r#"  {icon} {name} (id: {id})
+    command: `{command}`
+    {message}
+"#,
+            name = info.name,
+            id = id.map(|id| id.to_string()).unwrap_or("none".into()),
+            message = message.unwrap_or_default(),
+        )
+    }
+
+    fn pretty_print_array<T: Display>(array: &[T]) -> String {
+        match array.len() {
+            0 => "[]".into(),
+            1 => array[0].to_string(),
+            _ => format!(
+                "[{}]",
+                array
+                    .iter()
+                    .map(|item| item.to_string())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            ),
+        }
+    }
+
+    fn pretty_print_formatter(formatter: Option<&FormatterConfiguration>) -> String {
+        match formatter {
+            Some(config) => {
+                let command = config.command.clone()
+                    + config
+                        .args
+                        .iter()
+                        .map(|s| String::from(" ") + s.as_str())
+                        .collect::<String>()
+                        .as_str();
+
+                format!("`{command}`")
+            }
+            None => "No formatter configured".into(),
+        }
     }
 
     fn get_info(editor: &Editor) -> String {
-        let server_states = LspInfo::get_server_states(editor);
+        let doc = doc!(editor);
+        if doc.language_config().is_none() {
+            return r#"Press q or Esc to close this window
 
-        let status = server_states
-            .iter()
-            .map(|state| {
-                let prefix = match state.status {
-                    Ok(_) => "✓",
-                    Err(_) => "✘",
-                };
+Document has no associated lsp configuration"#
+                .to_string();
+        }
 
-                let error = if let Err(e) = &state.status {
-                    "\n      ".to_string() + e.0.as_str()
-                } else {
-                    "".to_string()
-                };
+        let config = doc.language_config().unwrap();
+        let server_info = Self::get_server_info(editor)
+            .into_iter()
+            .map(Self::format_info)
+            .collect::<String>();
 
-                format!("  {} {}{}", prefix, state.name, error)
-            })
-            .reduce(|acc, i| acc + "\n" + i.as_str())
-            .map(|s| "Server status:\n".to_string() + s.as_str())
-            .unwrap_or_else(|| "No configured server".to_string());
+        format!(
+            r#"Press q or Esc to close this window
 
-        format!("{}\n\nPress q or Esc to close", status)
+Detected language: {lang}
+File-type(s): {ft}
+Root(s): {roots}
+Formatter: {formatter}
+
+Detected clients:
+{server_info}
+"#,
+            lang = config.language_id,
+            ft = Self::pretty_print_array(&config.file_types),
+            roots = Self::pretty_print_array(&config.roots),
+            formatter = Self::pretty_print_formatter(config.formatter.as_ref()),
+        )
     }
 
-    fn get_error_message(_name: &str, config: Option<&LanguageServerConfiguration>) -> String {
-        match config {
-            Some(config) => {
-                if let Err(e) = which::which(&config.command) {
-                    return e.to_string();
-                }
-
-                "Unknown error".to_string()
-            }
-            None => "No configuration supplied".to_string(),
+    fn get_error_message(info: &ServerInfo) -> String {
+        if let Err(e) = which::which(&info.config.command) {
+            return e.to_string();
         }
+
+        "Unknown error, check the logs".to_string()
     }
 }
 
