529af7ac5b25e42757593fc08c909c0a81bafb94
Ingrid <git@ingrids.email>
2024-02-11 22:44:24 +0100

    store ViewPosition and Selection directly in FileHistoryEntry

--- helix-view/src/view.rs
+++ helix-view/src/view.rs
@@ -15,6 +15,7 @@ use helix_core::{
     Transaction,
     VisualOffsetError::{PosAfterMaxRow, PosBeforeAnchorRow},
 };
+use serde::{Deserialize, Serialize};
 
 use std::{
     collections::{HashMap, VecDeque},
@@ -100,7 +101,7 @@ impl JumpList {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Copy, Default)]
+#[derive(Clone, Debug, PartialEq, Eq, Copy, Default, Serialize, Deserialize)]
 pub struct ViewPosition {
     pub anchor: usize,
     pub horizontal_offset: usize,
