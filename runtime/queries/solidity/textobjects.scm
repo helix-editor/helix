2baf31527fab15c86fe43ec22623f919004df977
beeb <703631+beeb@users.noreply.github.com>
2024-03-11 12:56:28 +0100

    feat: add textobject queries for solidity

---
+++ runtime/queries/solidity/textobjects.scm
@@ -0,0 +1,36 @@
+(function_definition
+  body: (_) @function.inside) @function.around
+
+(constructor_definition
+  body: (_) @function.inside) @function.around
+
+(fallback_receive_definition
+  body: (_) @function.inside) @function.around
+
+(function_definition 
+  ((parameter) @parameter.inside . ","? @parameter.around) @parameter.around)
+
+(constructor_definition 
+  ((parameter) @parameter.inside . ","? @parameter.around) @parameter.around)
+
+(return_type_definition 
+  ((parameter) @parameter.inside . ","? @parameter.around) @parameter.around)
+
+(modifier_definition 
+  ((parameter) @parameter.inside . ","? @parameter.around) @parameter.around)
+
+(event_definition 
+  ((event_parameter) @parameter.inside . ","? @parameter.around) @parameter.around)
+
+(error_declaration 
+  ((error_parameter) @parameter.inside . ","? @parameter.around) @parameter.around)
+
+(struct_declaration
+  body: (_) @class.inside) @class.around
+
+(enum_declaration
+  body: (_) @class.inside) @class.around
+
+(comment) @comment.inside
+
+(comment)+ @comment.around
