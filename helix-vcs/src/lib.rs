5312d6bf184a07354ef2d3b74a9fab8f7c2f3b8f
Skyler Hawthorne <skyler@dead10ck.com>
2024-03-18 16:12:53 -0400

    Factor out line ending handling in integration tests
    Prior to this change, every integration test which wanted its line
    endings to be handled transparently across platforms, i.e. test with
    the same input that has its platform's line feed characters, converting
    the line endings was up to each individual test by calling the
    `platform_line` helper function. This significantly increases the amount
    of boilerplate one has to copy between all the tests.
    
    However, there are some test cases that need to exert strict control
    over the exact input text without being manipulated behind the scenes by
    the test framework.
    
    So, with this change, the line feed conversions are factored into
    the `TestCase` struct. By default, line endings of the input text
    are converted to the platform's native line feed ending, but one can
    explicitly specify in their test case when the input text should be left
    alone and tested as is.

--- helix-vcs/src/lib.rs
+++ helix-vcs/src/lib.rs
@@ -46,8 +46,8 @@ impl DiffProviderRegistry {
             .find_map(|provider| match provider.get_diff_base(file) {
                 Ok(res) => Some(res),
                 Err(err) => {
-                    log::info!("{err:#?}");
-                    log::info!("failed to open diff base for {}", file.display());
+                    log::debug!("{err:#?}");
+                    log::debug!("failed to open diff base for {}", file.display());
                     None
                 }
             })
@@ -59,8 +59,8 @@ impl DiffProviderRegistry {
             .find_map(|provider| match provider.get_current_head_name(file) {
                 Ok(res) => Some(res),
                 Err(err) => {
-                    log::info!("{err:#?}");
-                    log::info!("failed to obtain current head name for {}", file.display());
+                    log::debug!("{err:#?}");
+                    log::debug!("failed to obtain current head name for {}", file.display());
                     None
                 }
             })
