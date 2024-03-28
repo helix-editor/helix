049e7abc4d00f8de18cfe11e6db60d5b21eacef9
Boris Petrov <boris.v.petrov@protonmail.com>
2024-03-28 09:54:57 +0100

    Update tree-sitter-julia
    Update julia parser to latest version, along with:
    - updating the queries,
    - pulling changes from `nvim-treesitter`’s queries (as the maintainters
      of the parser update the queries there),
    - reversing the queries’ order to be compatible with upstream.

--- runtime/queries/julia/locals.scm
+++ runtime/queries/julia/locals.scm
@@ -2,43 +2,100 @@
 ; Definitions
 ; -----------
 
-; Imports
-(import_statement
-  (identifier) @local.definition)
-  
+; Variables
+(assignment
+  (identifier) @local.definition.var)
+
+(assignment
+  (tuple_expression
+    (identifier) @local.definition.var))
+
 ; Constants
 (const_statement
-  (variable_declaration
+  (assignment
     . (identifier) @local.definition))
+
+; let/const bindings
+(let_binding
+  (identifier) @local.definition.var)
+
+(let_binding
+  (tuple_expression
+    (identifier) @local.definition.var))
+
+; For bindings
+(for_binding
+  (identifier) @local.definition.var)
+
+(for_binding
+  (tuple_expression
+    (identifier) @local.definition.var))
+
+; Types
+(struct_definition
+  name: (identifier) @local.definition.type)
+
+(abstract_definition
+  name: (identifier) @local.definition.type)
+
+(abstract_definition
+  name: (identifier) @local.definition.type)
+
+(type_parameter_list
+  (identifier) @local.definition.type)
+
+; Module imports
+(import_statement
+  (identifier) @local.definition.import)
 
 ; Parameters
 (parameter_list
-  (identifier) @local.definition)
+  (identifier) @local.definition.parameter)
 
-(typed_parameter
-  . (identifier) @local.definition)
-
-(optional_parameter .
-  (identifier) @local.definition)
+(optional_parameter
+  .
+  (identifier) @local.definition.parameter)
 
 (slurp_parameter
-  (identifier) @local.definition)
+  (identifier) @local.definition.parameter)
 
+(typed_parameter
+  parameter: (identifier) @local.definition.parameter
+  (_))
+
+; Single parameter arrow function
 (function_expression
-  . (identifier) @local.definition)
- 
-; ------
-; Scopes
-; ------
+  .
+  (identifier) @local.definition.parameter)
 
-[
-  (function_definition)
-  (short_function_definition)
-  (macro_definition)
-] @local.scope
+; Function/macro definitions
+(function_definition
+  name: (identifier) @local.definition.function) @local.scope
+
+(short_function_definition
+  name: (identifier) @local.definition.function) @local.scope
+
+(macro_definition
+  name: (identifier) @local.definition.macro) @local.scope
 
 ; ----------
 ; References
 ; ----------
 
 (identifier) @local.reference
+ 
+; ------
+; Scopes
+; ------
+
+[
+  (for_statement)
+  (while_statement)
+  (try_statement)
+  (catch_clause)
+  (finally_clause)
+  (let_statement)
+  (quote_statement)
+  (do_clause)
+] @local.scope 
+
