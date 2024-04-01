727a76f3e6fa9220f8c11df0f16613765b0830ab
Manuel Mendez <github@i.m.mmlb.dev>
2024-04-01 13:15:37 -0400

    Use any-of instead of match in yaml injections
    Go back to original intent when this code was first written now that the
    tree-sitter version used supports any-of?.

--- runtime/queries/yaml/injections.scm
+++ runtime/queries/yaml/injections.scm
@@ -22,20 +22,20 @@
 
 ;; Github actions ("run") / Gitlab CI ("scripts")
 (block_mapping_pair
-  key: (flow_node) @_run (#match? @_run "^(run|script|before_script|after_script)$")
+  key: (flow_node) @_run (#any-of? @_run "run" "script" "before_script" "after_script")
   value: (flow_node
            (plain_scalar
              (string_scalar) @injection.content)
            (#set! injection.language "bash")))
 
 (block_mapping_pair
-  key: (flow_node) @_run (#match? @_run "^(run|script|before_script|after_script)$")
+  key: (flow_node) @_run (#any-of? @_run "run" "script" "before_script" "after_script")
   value: (block_node
            (block_scalar) @injection.content
            (#set! injection.language "bash")))
 
 (block_mapping_pair
-  key: (flow_node) @_run (#match? @_run "^(run|script|before_script|after_script)$")
+  key: (flow_node) @_run (#any-of? @_run "run" "script" "before_script" "after_script")
   value: (block_node
            (block_sequence
              (block_sequence_item
@@ -45,7 +45,7 @@
                 (#set! injection.language "bash")))))
 
 (block_mapping_pair
-  key: (flow_node) @_run (#match? @_run "^(run|script|before_script|after_script)$")
+  key: (flow_node) @_run (#any-of? @_run "run" "script" "before_script" "after_script")
   value: (block_node
            (block_sequence
              (block_sequence_item
