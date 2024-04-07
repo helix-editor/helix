06d3e34c071230ffd659563464b97428311029d5
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2024-04-07 22:27:19 +0530

    add yd as alias for yank_diagnostic

--- helix-term/src/commands/typed.rs
+++ helix-term/src/commands/typed.rs
@@ -3063,8 +3063,8 @@ pub const TYPABLE_COMMAND_LIST: &[TypableCommand] = &[
     },
     TypableCommand {
         name: "yank-diagnostic",
-        aliases: &[],
-        doc: "Yank diagnostic(s) under primary cursor to register, or clipboard by default",
+        aliases: &["yd"],
+        doc: "Yank diagnostic(s) under primary cursor to register, or clipboard by default.",
         fun: yank_diagnostic,
         signature: CommandSignature::all(completers::register),
     },
