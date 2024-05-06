3251971ad2bbdd8c4632c6fbdc928fcbe3f410d9
Michael Davis <mcarsondavis@gmail.com>
2024-05-06 17:44:02 -0400

    Turn off edoc injections in Erlang

--- runtime/queries/erlang/injections.scm
+++ runtime/queries/erlang/injections.scm
@@ -1,7 +1,7 @@
-((line_comment (comment_content) @injection.content)
- (#set! injection.language "edoc")
- (#set! injection.include-children)
- (#set! injection.combined))
+; ((line_comment (comment_content) @injection.content)
+;  (#set! injection.language "edoc")
+;  (#set! injection.include-children)
+;  (#set! injection.combined))
 
 ((comment (comment_content) @injection.content)
  (#set! injection.language "comment"))
