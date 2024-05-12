7957aa2ae7ec33368a02ccea620d1656e59748a1
Dmitry Salin <dmitry.salin@gmail.com>
2024-05-12 22:11:37 +0300

    Update grammar

--- runtime/queries/mojo/highlights.scm
+++ runtime/queries/mojo/highlights.scm
@@ -1,10 +1,27 @@
-; Imports
+(attribute attribute: (identifier) @property)
+(type (identifier) @type)
 
-(dotted_name
-  (identifier)* @namespace)
+; Function calls
 
-(aliased_import
-  alias: (identifier) @namespace)
+(decorator) @function
+
+(call
+  function: (attribute attribute: (identifier) @function.method))
+(call
+  function: (identifier) @function)
+
+; Function definitions
+
+(function_definition
+  name: (identifier) @function)
+
+; Identifier naming conventions
+
+((identifier) @type
+ (#match? @type "^[A-Z]"))
+
+((identifier) @constant
+ (#match? @constant "^_*[A-Z][A-Z\\d_]*$"))
 
 ; Builtin functions
 
@@ -12,125 +29,45 @@
   function: (identifier) @function.builtin)
  (#match?
    @function.builtin
-   "^(abs|all|any|ascii|bin|bool|breakpoint|bytearray|bytes|callable|chr|classmethod|compile|complex|delattr|dict|dir|divmod|enumerate|eval|exec|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|isinstance|issubclass|iter|len|list|locals|map|max|memoryview|min|next|object|oct|open|ord|pow|print|property|range|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|vars|zip|__import__|__mlir_attr|__mlir_op|__mlir_type)$"))
-
-; Function calls
-
-[
-  "def"
-  "lambda"
-  "fn"
-  "class"
-  "struct"
-  "trait"
-  "var"
-  "let"
-  "alias"
-  "async"
-  "owned"
-  "borrowed"
-  "inout"
-] @keyword.function
-
-(call
-  function: (attribute attribute: (identifier) @constructor)
- (#match? @constructor "^[A-Z]"))
-(call
-  function: (identifier) @constructor
- (#match? @constructor "^[A-Z]"))
-
-(call
-  function: (attribute attribute: (identifier) @function.method))
-
-(call
-  function: (identifier) @function)
-
-; Function definitions
-
-(function_definition
-  name: (identifier) @constructor
- (#match? @constructor "^(__new__|__init__)$"))
-
-(function_definition
-  name: (identifier) @function)
-
-; Decorators
-
-(decorator) @function
-(decorator (identifier) @function)
-(decorator (attribute attribute: (identifier) @function))
-(decorator (call
-  function: (attribute attribute: (identifier) @function)))
-
-; Parameters
-
-((identifier) @variable.builtin
- (#match? @variable.builtin "^(self|cls)$"))
-
-(parameters (identifier) @variable.parameter)
-(parameters (typed_parameter (identifier) @variable.parameter))
-(parameters (default_parameter name: (identifier) @variable.parameter))
-(parameters (typed_default_parameter name: (identifier) @variable.parameter))
-
-(parameters
-  (list_splat_pattern ; *args
-    (identifier) @variable.parameter))
-(parameters
-  (dictionary_splat_pattern ; **kwargs
-    (identifier) @variable.parameter))
-
-(lambda_parameters
-  (identifier) @variable.parameter)
-
-; Types
-
-((identifier) @type.builtin
- (#match?
-   @type.builtin
-   "^(bool|bytes|dict|float|frozenset|int|list|set|str|tuple)$"))
-
-; In type hints make everything types to catch non-conforming identifiers
-; (e.g., datetime.datetime) and None
-(type [(identifier) (none)] @type)
-; Handle [] . and | nesting 4 levels deep
-(type
-  (_ [(identifier) (none)]? @type
-    (_ [(identifier) (none)]? @type
-      (_ [(identifier) (none)]? @type
-        (_ [(identifier) (none)]? @type)))))
-
-(class_definition name: (identifier) @type)
-(class_definition superclasses: (argument_list (identifier) @type))
-
-; Variables
-
-((identifier) @constant
- (#match? @constant "^_*[A-Z][A-Z\\d_]*$"))
-
-((identifier) @type
- (#match? @type "^[A-Z]"))
-
-(attribute attribute: (identifier) @variable.other.member)
-(identifier) @variable
+   "^(abs|all|always_inline|any|ascii|bin|bool|breakpoint|bytearray|bytes|callable|chr|classmethod|compile|complex|constrained|delattr|dict|dir|divmod|enumerate|eval|exec|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|isinstance|issubclass|iter|len|list|locals|map|max|memoryview|min|next|object|oct|open|ord|pow|print|property|range|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|unroll|vars|zip|__mlir_attr|__mlir_op|__mlir_type|__import__)$"))
 
 ; Literals
-(none) @constant.builtin
+
 [
+  (none)
   (true)
   (false)
-] @constant.builtin.boolean
+] @constant.builtin
 
-(integer) @constant.numeric.integer
-(float) @constant.numeric.float
+[
+  (integer)
+  (float)
+] @number
+
 (comment) @comment
 (string) @string
-(escape_sequence) @constant.character.escape
+(escape_sequence) @escape
 
-["," "." ":" ";" (ellipsis)] @punctuation.delimiter
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
 (interpolation
   "{" @punctuation.special
   "}" @punctuation.special) @embedded
-["(" ")" "[" "]" "{" "}"] @punctuation.bracket
+
+; Docstrings.
+(function_definition
+  "async"?
+  "def"
+  name: (_)
+  (parameters)?
+  body: (block (expression_statement (string) @string.doc)))
 
 [
   "-"
@@ -145,17 +82,14 @@
   "//="
   "/="
   "&"
-  "&="
   "%"
   "%="
   "^"
-  "^="
   "+"
   "->"
   "+="
   "<"
   "<<"
-  "<<="
   "<="
   "<>"
   "="
@@ -164,78 +98,55 @@
   ">"
   ">="
   ">>"
-  ">>="
   "|"
-  "|="
   "~"
-  "@="
+  "and"
+  "in"
+  "is"
+  "not"
+  "or"
+  "is not"
+  "not in"
 ] @operator
 
 [
   "as"
+  "alias"
   "assert"
+  "async"
   "await"
-  "from"
-  "pass"
-
-  "with"
-] @keyword.control
-
-[
-  "if"
+  "borrowed"
+  "break"
+  "class"
+  "continue"
+  "def"
+  "del"
   "elif"
   "else"
+  "except"
+  "exec"
+  "finally"
+  "fn"
+  "for"
+  "from"
+  "global"
+  "if"
+  "import"
+  "inout"
+  "lambda"
+  "nonlocal"
+  "owned"
+  "pass"
+  "print"
+  "raise"
+  "raises"
+  "return"
+  "struct"
+  "try"
+  "var"
+  "while"
+  "with"
+  "yield"
   "match"
   "case"
-] @keyword.control.conditional
-
-[
-  "while"
-  "for"
-  "break"
-  "continue"
-] @keyword.control.repeat
-
-[
-  "return"
-  "yield"
-] @keyword.control.return
-(yield "from" @keyword.control.return)
-
-[
-  "raise"
-  "try"
-  "except"
-  "finally"
-] @keyword.control.except
-(raise_statement "from" @keyword.control.except)
-"import" @keyword.control.import
-
-(for_statement "in" @keyword.control)
-(for_in_clause "in" @keyword.control)
-
-[
-  "async"
-  "class"
-  "exec"
-  "global"
-  "nonlocal"
-  "print"
-  "type"
 ] @keyword
-[
-  "and"
-  "or"
-  "not in"
-  "in"
-  "not"
-  "del"
-  "is not"
-  "is"
-] @keyword.operator
-
-((identifier) @type.builtin
-  (#match? @type.builtin
-    "^(BaseException|Exception|ArithmeticError|BufferError|LookupError|AssertionError|AttributeError|EOFError|FloatingPointError|GeneratorExit|ImportError|ModuleNotFoundError|IndexError|KeyError|KeyboardInterrupt|MemoryError|NameError|NotImplementedError|OSError|OverflowError|RecursionError|ReferenceError|RuntimeError|StopIteration|StopAsyncIteration|SyntaxError|IndentationError|TabError|SystemError|SystemExit|TypeError|UnboundLocalError|UnicodeError|UnicodeEncodeError|UnicodeDecodeError|UnicodeTranslateError|ValueError|ZeroDivisionError|EnvironmentError|IOError|WindowsError|BlockingIOError|ChildProcessError|ConnectionError|BrokenPipeError|ConnectionAbortedError|ConnectionRefusedError|ConnectionResetError|FileExistsError|FileNotFoundError|InterruptedError|IsADirectoryError|NotADirectoryError|PermissionError|ProcessLookupError|TimeoutError|Warning|UserWarning|DeprecationWarning|PendingDeprecationWarning|SyntaxWarning|RuntimeWarning|FutureWarning|ImportWarning|UnicodeWarning|BytesWarning|ResourceWarning)$"))
-
-(ERROR) @error
