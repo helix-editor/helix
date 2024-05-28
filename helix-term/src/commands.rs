3db8ca65b4e2ef58e439c635bf0e704a0598a811
Mark Stosberg <mark@stosberg.com>
2024-05-28 16:13:44 -0400

    docs: improve `last_picker` docs
    - Consistency: Before it was described as "fuzzy" in place but not another.
      All the pickers seem to be fuzzy pickers, so remove that word for consistency.
    
    - Clarity: Document how it's different from other pickers and adds value
      by restoring the selected entry when it opens-- a valuable feature to make
      more discoverable.

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -337,7 +337,7 @@ impl MappableCommand {
         workspace_symbol_picker, "Open workspace symbol picker",
         diagnostics_picker, "Open diagnostic picker",
         workspace_diagnostics_picker, "Open workspace diagnostic picker",
-        last_picker, "Open last picker",
+        last_picker, "Open last picker, preserving selected entry",
         insert_at_line_start, "Insert at start of line",
         insert_at_line_end, "Insert at end of line",
         open_below, "Open new line below selection",
