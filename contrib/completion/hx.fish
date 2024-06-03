38ab1acf0619aae415f97bb7d36c058325b53f19
Yuntao Zhao <yuntaz@proton.me>
2024-06-03 11:30:29 -0700

    feat: improve health check completion
    - remove header
    - remove check/x characters

--- contrib/completion/hx.fish
+++ contrib/completion/hx.fish
@@ -3,7 +3,7 @@
 
 complete -c hx -s h -l help -d "Prints help information"
 complete -c hx -l tutor -d "Loads the tutorial"
-complete -c hx -l health -xa "(__hx_langs_ops)" -d "Checks for errors in editor setup"
+complete -c hx -l health -xa "(__hx_langs_ops)" -d "Checks for errors"
 complete -c hx -s g -l grammar -x -a "fetch build" -d "Fetch or build tree-sitter grammars"
 complete -c hx -s v -o vv -o vvv -d "Increases logging verbosity"
 complete -c hx -s V -l version -d "Prints version information"
@@ -14,5 +14,5 @@ complete -c hx -l log -r -d "Specifies a file to use for logging"
 complete -c hx -s w -l working-dir -d "Specify initial working directory" -xa "(__fish_complete_directories)"
 
 function __hx_langs_ops
-    hx --health | tail -n '+7' | awk '{print $1}' | sed 's/\x1b\[[0-9;]*m//g'
+    hx --health | tail -n '+11' | string replace -fr '^(\S+) .*' '$1'
 end
