4f1d35dfbe027af0914050ecd457447372494cde
Gabor Pihaj <gabor.pihaj@gmail.com>
2024-07-06 16:30:25 +0100

    Follow naming conventions

--- helix-term/src/commands/engine/steel.rs
+++ helix-term/src/commands/engine/steel.rs
@@ -693,10 +693,10 @@ fn load_editor_api(engine: &mut Engine, generate_sources: bool) {
     let mut module = BuiltInModule::new("helix/core/editor");
 
     // Types
-    module.register_fn("Action::Load", || Action::Load);
-    module.register_fn("Action::Replace", || Action::Replace);
-    module.register_fn("Action::HorizontalSplit", || Action::HorizontalSplit);
-    module.register_fn("Action::VerticalSplit", || Action::VerticalSplit);
+    module.register_fn("Action/Load", || Action::Load);
+    module.register_fn("Action/Replace", || Action::Replace);
+    module.register_fn("Action/HorizontalSplit", || Action::HorizontalSplit);
+    module.register_fn("Action/VerticalSplit", || Action::VerticalSplit);
 
     // Arity 0
     module.register_fn("editor-focus", cx_current_focus);
@@ -747,10 +747,10 @@ fn load_editor_api(engine: &mut Engine, generate_sources: bool) {
             ));
         };
 
-        template_function_type_constructor("Action::Load");
-        template_function_type_constructor("Action::Replace");
-        template_function_type_constructor("Action::HorizontalSplit");
-        template_function_type_constructor("Action::VerticalSplit");
+        template_function_type_constructor("Action/Load");
+        template_function_type_constructor("Action/Replace");
+        template_function_type_constructor("Action/HorizontalSplit");
+        template_function_type_constructor("Action/VerticalSplit");
 
         let mut template_function_arity_0 = |name: &str| {
             builtin_editor_command_module.push_str(&format!(
