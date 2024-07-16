535351067c2ac018ee2fef6cc685f49065617bd1
RoloEdits <RoloEdits@gmail.com>
2024-07-15 20:29:44 -0700

    fix(commands): change `pipe`-like output trimming (#11183)

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -5745,14 +5745,20 @@ fn shell(cx: &mut compositor::Context, cmd: &str, behavior: &ShellBehavior) {
         let output = if let Some(output) = shell_output.as_ref() {
             output.clone()
         } else {
-            let fragment = range.slice(text);
-            match shell_impl(shell, cmd, pipe.then(|| fragment.into())) {
-                Ok(result) => {
-                    let result = Tendril::from(result.trim_end());
-                    if !pipe {
-                        shell_output = Some(result.clone());
+            let input = range.slice(text);
+            match shell_impl(shell, cmd, pipe.then(|| input.into())) {
+                Ok(mut output) => {
+                    if !input.ends_with("\n") && !output.is_empty() && output.ends_with('\n') {
+                        output.pop();
+                        if output.ends_with('\r') {
+                            output.pop();
+                        }
                     }
-                    result
+
+                    if !pipe {
+                        shell_output = Some(output.clone());
+                    }
+                    output
                 }
                 Err(err) => {
                     cx.editor.set_error(err.to_string());
