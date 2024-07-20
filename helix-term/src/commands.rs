fa50e510eadef9566322f570389296b5e22d74af
TornaxO7 <tornax@proton.me>
2024-07-20 05:59:53 +0200

    single-line-comment: improve performance

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -1672,7 +1672,7 @@ fn has_line_comment(config: &Config, doc: &Document, line_num: usize) -> Option<
                 .clone();
             // Use case (example): Rust supports `//` and `///`. We're reverse-sorting them to priorize `///` before `//`
             // because otherwise a doc comment `/// like this` would get `//` as the next comment-token
-            line_tokens.sort_by(|a, b| a.len().partial_cmp(&b.len()).unwrap().reverse());
+            line_tokens.sort_unstable_by(|a, b| b.len().partial_cmp(&a.len()).unwrap());
             line_tokens
         };
 
