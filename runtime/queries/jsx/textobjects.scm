ed0f012ae48cd8c30a6fe45bd0bdeeeef838fd28
Björn Ganslandt <b.ganslandt@gmail.com>
2024-08-28 15:53:06 +0200

    ref: Make textobjects usable by jsx and tsx

--- runtime/queries/jsx/textobjects.scm
+++ runtime/queries/jsx/textobjects.scm
@@ -1,9 +1,3 @@
 ; See runtime/queries/ecma/README.md for more info.
 
 ; inherits: _jsx,_javascript,ecma
-
-(jsx_self_closing_element) @xml_element.around @xml_element.inside
-
-(jsx_element (jsx_opening_element) (_)* @xml_element.inside (jsx_closing_element))
-
-(jsx_element) @xml_element.around
