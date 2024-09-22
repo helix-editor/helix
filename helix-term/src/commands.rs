24f16734b6a8aeb9341e98e007bc445dc5c70077
Jonathan Lopez <jonathanglopez@gmail.com>
2024-09-22 07:31:33 -0400

    Include boolean increment

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -6005,7 +6005,7 @@ fn increment_impl(cx: &mut Context, increment_direction: IncrementDirection) {
     for range in selection {
         let selected_text: Cow<str> = range.fragment(text);
         let new_from = ((range.from() as i128) + cumulative_length_diff) as usize;
-        let incremented = [increment::integer, increment::date_time]
+        let incremented = [increment::integer, increment::date_time, increment::boolean]
             .iter()
             .find_map(|incrementor| incrementor(selected_text.as_ref(), amount));
 
