5f7f4f628a6222f28efe3688ee998153b3a7373c
Ian Hobson <ian.r.hobson@gmail.com>
2024-10-04 09:35:01 +0200

    Replace newlines with double spaces in the eol diagnostic message
    Rather than discarding additional message lines, use double spaces to
    separate them visually.

--- helix-term/src/ui/text_decorations/diagnostics.rs
+++ helix-term/src/ui/text_decorations/diagnostics.rs
@@ -102,11 +102,11 @@ impl Renderer<'_, '_> {
             return 0;
         }
         let col = (col - self.renderer.offset.col) as u16;
-        let message = diag.message.lines().next().unwrap_or_default();
+        let message = diag.message.replace('\n', "  ");
         let (new_col, _) = self.renderer.set_string_truncated(
             self.renderer.viewport.x + col + 1,
             row,
-            message,
+            &message,
             width.saturating_sub(col + 1) as usize,
             |_| style,
             true,
