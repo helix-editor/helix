c8e5ad74fc301ea85de5252216805a2fffd04cc5
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-10-04 20:34:23 +0200

    feat: Add support for cylc configuration files

---
+++ runtime/queries/cylc/indents.scm
@@ -0,0 +1,24 @@
+[
+    (top_section) 
+    (sub_section_1) 
+    (sub_section_2) 
+    (graph_section) 
+    (runtime_section) 
+    (task_section)
+] @indent
+
+[
+    (top_section) 
+    (sub_section_1) 
+    (sub_section_2) 
+    (graph_section) 
+    (runtime_section) 
+    (task_section) 
+] @extend
+
+; Doesn't work ?
+; (unquoted_string
+;     (string_content) @anchor) @align
+
+(line_continuation) @indent.always
+
