cc5716cdc6cc7a81c2c07f56623c1790319f6186
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-11-22 11:10:30 +0000

    feat: update title

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   integrations: [
     starlight({
-      title: "Helix Editor",
+      title: "Helix",
       logo: {
         src: "./public/favicon.svg",
       },
