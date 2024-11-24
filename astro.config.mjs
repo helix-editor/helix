d773ed30e2f9a385d45e7459b7a6e72e4507650c
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-11-24 14:45:05 +0000

    feat: add key-remapping docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -48,6 +48,7 @@ export default defineConfig({
           items: [
             "reference/keymap",
             "reference/typed-commands",
+            "reference/key-remapping",
             "reference/editor-configuration",
             "reference/language-configuration",
             "reference/list-of-themes",
