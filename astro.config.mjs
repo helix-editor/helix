a2b9d2e1619b66cd2023556996b81aee1b30b0af
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-11-25 20:14:37 +0000

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,91 +8,95 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://helix.vercel.app",
   vite: {
     optimizeDeps: { include: ["asciinema-player"] },
   },
-  integrations: [starlight({
-    plugins: [
-      starlightLinksValidator(),
-      starlightBlog({ title: "News", prefix: "news" }),
-    ],
-    title: "Helix",
-    logo: {
-      src: "./public/favicon.svg",
-    },
-    social: {
-      github: "https://github.com/helix-editor/helix",
-      matrix: "https://matrix.to/#/#helix-community:matrix.org",
-    },
-    components: {
-      ThemeProvider: "./src/components/ThemeProvider.astro",
-      ThemeSelect: "./src/components/ThemeSelect.astro",
-    },
-    editLink: {
-      baseUrl:
-        "https://github.com/nikitarevenco/helix-better-docs/edit/main/",
-    },
-    sidebar: [
-      {
-        label: "Getting Started",
-        items: ["getting-started/installation", "getting-started/basics"],
+  integrations: [
+    starlight({
+      plugins: [
+        starlightLinksValidator(),
+        starlightBlog({ title: "News", prefix: "news" }),
+      ],
+      title: "Helix",
+      logo: {
+        src: "./public/favicon.svg",
       },
-      {
-        label: "Usage",
-        items: [
-          "usage/multiple-cursors",
-          "usage/text-objects",
-          "usage/surround",
-          "usage/language-support",
-          "usage/pickers",
-          "usage/registers",
-        ],
+      social: {
+        github: "https://github.com/helix-editor/helix",
+        matrix: "https://matrix.to/#/#helix-community:matrix.org",
       },
-      {
-        label: "Configuration",
-        items: [
-          "configuration/editor",
-          "configuration/languages",
-          "configuration/remapping",
-        ],
+      components: {
+        ThemeProvider: "./src/components/ThemeProvider.astro",
+        ThemeSelect: "./src/components/ThemeSelect.astro",
       },
-      {
-        label: "Reference",
-        items: [
-          "reference/keymap",
-          "reference/typed-commands",
-          "reference/list-of-themes",
-          "reference/custom-themes",
-          "reference/language-servers",
-          "reference/formatters",
-          "reference/debuggers",
-        ],
+      editLink: {
+        baseUrl:
+          "https://github.com/nikitarevenco/helix-better-docs/edit/main/",
       },
-      {
-        label: "Help",
-        items: [
-          "help/recipes",
-          "help/faq",
-          "help/troubleshooting",
-          "help/terminal-support",
-          "help/language-defaults",
-          "help/refactor-examples",
-        ],
-      },
-      {
-        label: "Contributing to Helix",
-        items: [
-          "contributing/vision",
-          "contributing/this-site",
-          "contributing/releases",
-          "contributing/architecture",
-          "contributing/languages",
-          "contributing/textobject-queries",
-          "contributing/indent-queries",
-          "contributing/injection-queries",
-        ],
-      },
-    ],
-    customCss: ["./src/globals.css"],
-  }), sitemap()],
+      sidebar: [
+        {
+          label: "Getting Started",
+          items: ["getting-started/installation", "getting-started/basics"],
+        },
+        {
+          label: "Usage",
+          items: [
+            "usage/multiple-cursors",
+            "usage/text-objects",
+            "usage/surround",
+            "usage/language-support",
+            "usage/pickers",
+            "usage/registers",
+          ],
+        },
+        {
+          label: "Configuration",
+          items: [
+            "configuration/editor",
+            "configuration/languages",
+            "configuration/remapping",
+          ],
+        },
+        {
+          label: "Reference",
+          items: [
+            "reference/keymap",
+            "reference/typed-commands",
+            "reference/list-of-themes",
+            "reference/custom-themes",
+            "reference/language-servers",
+            "reference/formatters",
+            "reference/debuggers",
+          ],
+        },
+        {
+          label: "Help",
+          items: [
+            "help/recipes",
+            "help/faq",
+            "help/troubleshooting",
+            "help/terminal-support",
+            "help/language-defaults",
+            "help/refactor-examples",
+          ],
+        },
+        {
+          label: "Contributing to Helix",
+          items: [
+            "contributing/vision",
+            "contributing/this-site",
+            "contributing/releases",
+            "contributing/architecture",
+            "contributing/languages",
+            "contributing/textobject-queries",
+            "contributing/indent-queries",
+            "contributing/injection-queries",
+          ],
+        },
+      ],
+      customCss: ["./src/globals.css"],
+    }),
+    sitemap(),
+  ],
 });
