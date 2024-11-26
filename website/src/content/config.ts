92b0cd5a7092721597662eb4aa16c2352da2939a
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-11-26 12:34:46 +0000

    fix: do not force description

--- website/src/content/config.ts
+++ website/src/content/config.ts
@@ -1,15 +1,11 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
 import { blogSchema } from "starlight-blog/schema";
 
 export const collections = {
   docs: defineCollection({
     schema: docsSchema({
-      extend: (context) =>
-        z.intersection(
-          blogSchema(context),
-          z.object({ description: z.string() }),
-        ),
+      extend: (context) => blogSchema(context),
     }),
   }),
 };
