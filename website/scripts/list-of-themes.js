7a7651db112dc65b3f82f8f9f8108df3d34a095f
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-11-28 02:33:00 +0000

    feat: basic implementation for showcasing every single theme

--- website/scripts/list-of-themes.js
+++ website/scripts/list-of-themes.js
@@ -4,10 +4,9 @@
  * It automatically runs on `pnpm dev`, `pnpm build` and `pnpm preview` and can be manually run with `pnpm termshots`
  */
 
-import util from "node:util";
 import toml from "@iarna/toml";
 import fs from "node:fs";
-import path, { delimiter } from "node:path";
+import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 function isObject(item) {
@@ -73,7 +72,6 @@ const resolveInheritedTheme = ([themeName, theme]) => {
     // theme A inherits theme B.
     // recursively override theme B's styles with theme A's styles
     const inheritedTheme = themeMap.get(theme.inherits);
-    const merged = mergeDeep(inheritedTheme, theme);
     return [themeName, mergeDeep(inheritedTheme, theme)];
   } else {
     return [themeName, theme];
@@ -88,142 +86,18 @@ const themes = unmergedThemes
   // We could make a more robust algorithm but in reality just calling the function twice is fine, since there isn't any chain of A -> B -> C -> D inheritance and this is a simple script
   .map(resolveInheritedTheme);
 
-const setIntersection = (a, b) =>
-  new Set([...a].filter((element) => b.has(element)));
+const writeDirectory = path.join(
+  scriptDirectory,
+  "..",
+  "src",
+  "termshots",
+  "generated",
+  "list-of-themes",
+);
 
-const parsedThemes = themes
-  .flatMap(([themeName, theme], i) => {
-    if (themeName !== "catppuccin_mocha") {
-      return [];
-    }
-    const background = {
-      bg: theme["ui.background"].bg,
-      fg: theme["ui.background"].fg || "#ffaaaa",
-    };
-
-    const getColor = (key, fallback) => {
-      const value = theme[key];
-      if (typeof value === "string") {
-        return { fg: value };
-      } else if (!value) {
-        return fallback();
-      }
-      return value;
-    };
-
-    const getFallbackColor = (primaryKey, fallbackFn) => {
-      const value = theme[primaryKey];
-      if (typeof value === "string") {
-        return { fg: value };
-      } else if (!value) {
-        return fallbackFn();
-      }
-      return value;
-    };
-
-    const getUiBackground = () => background;
-    const getVariable = () => getColor("variable", () => background.fg);
-    const getFunction = () => getFallbackColor("function", getVariable);
-    const getMacro = () => getFallbackColor("function.macro", getFunction);
-    const getKeyword = () => getFallbackColor("keyword", getVariable);
-    const getUiLinenr = () => getColor("ui.linenr", () => background.fg);
-    const getUiLinenrSelected = () =>
-      getFallbackColor("ui.linenr.selected", getHighlight);
-    const getOperator = () => getColor("operator", () => background.fg);
-    const getPunctuation = () => getColor("punctuation", () => background.fg);
-    const getConstant = () => getFallbackColor("constant", getVariable);
-    const getConstantNumeric = () =>
-      getFallbackColor("constant.numeric", getConstant);
-    const getPunctuationDelimiter = () =>
-      getFallbackColor("punctuation.delimiter", getPunctuation);
-    const getVariableParameter = () =>
-      getFallbackColor("variable.parameter", getVariable);
-    const getComment = () => getColor("comment", () => background.fg);
-    const getType = () => getFallbackColor("type", getVariable);
-    const getTypeBuiltin = () => getFallbackColor("type.builtin", getType);
-
-    const getUiStatusline = () => {
-      const uiStatusline = theme["ui.statusline"];
-      if (!uiStatusline) return {};
-      return {
-        fg: uiStatusline.fg || background.fg,
-        bg: uiStatusline.bg || background.bg,
-      };
-    };
-
-    const getHighlight = () => getColor("ui.highlight", getUiBackground);
-    const getUiCursorline = () =>
-      getFallbackColor("ui.cursorline", getHighlight);
-    const getUiCursorlinePrimary = () =>
-      getFallbackColor("ui.cursorline.primary", getUiCursorline);
-
-    const getUiVirtual = () =>
-      getFallbackColor("ui.virtual", getUiVirtualInlayHint);
-
-    const getUiCursor = () =>
-      getFallbackColor("ui.cursor.primary", () => background.fg);
-
-    const getUiVirtualInlayHint = () =>
-      getColor("ui.virtual.inlay-hint", () => background.fg);
-    const getUiVirtualIndentGuide = () =>
-      getFallbackColor("ui.virtual.indent-guide", getUiVirtualInlayHint);
-    const getUiVirtualRuler = () =>
-      getFallbackColor("ui.virtual.ruler", getUiVirtualIndentGuide);
-
-    return [
-      [
-        themeName,
-        theme.palette,
-        {
-          "ui.background": getUiBackground(),
-          keyword: getKeyword(),
-          variable: getVariable(),
-          "variable.parameter": getVariableParameter(),
-          "ui.statusline": getUiStatusline(),
-          "ui.linenr": getUiLinenr(),
-          function: getFunction(),
-          "function.macro": getMacro(),
-          operator: getOperator(),
-          punctuation: getPunctuation(),
-          "punctuation.delimiter": getPunctuationDelimiter(),
-          constant: getConstant(),
-          "constant.numeric": getConstantNumeric(),
-          comment: getComment(),
-          "type.builtin": getTypeBuiltin(),
-          "ui.cursorline.primary": getUiCursorlinePrimary(),
-          "ui.linenr.selected": getUiLinenrSelected(),
-          "ui.virtual.ruler": getUiVirtualRuler(),
-          "ui.virtual": getUiVirtual(),
-          "ui.cursor": getUiCursor(),
-        },
-      ],
-    ];
-  })
-  .map(([name, palette, theme]) => {
-    return [
-      name,
-      Object.fromEntries(
-        Object.entries(theme).map(([scope, color]) => {
-          const isHex = (s) => s.startsWith("#");
-          if (typeof color.fg === "string") {
-            if (!isHex(color.fg)) {
-              color.fg = palette[color.fg];
-            }
-          }
-          if (typeof color.bg === "string") {
-            if (!isHex(color.bg)) {
-              color.bg = palette[color.bg];
-            }
-          }
-          return [scope, color];
-        }),
-      ),
-    ];
-  })
-  .map(themeToHtml)
-  .join("");
-
-console.log(parsedThemes);
+if (!fs.existsSync(writeDirectory)) {
+  fs.mkdirSync(writeDirectory, { recursive: true });
+}
 
 function factory(theme) {
   const backgroundWrapperCreator = (cursorline) => (children) =>
@@ -361,9 +235,198 @@ function themeToHtml([themeName, theme]) {
     [space(32)],
   ];
 
-  return `\
+  return [
+    themeName,
+    `\
 <h3>
   <code>${themeName}</code>
 </h3>
-${lines.map((line) => line.join("")).join("\n")}`;
+${lines.map((line) => line.join("")).join("\n")}`,
+  ];
 }
+
+const indexPath = path.join(writeDirectory, "..", "ListOfThemes.astro");
+
+if (fs.existsSync(indexPath)) {
+  fs.rmSync(indexPath);
+}
+
+const indexStream = fs.createWriteStream(indexPath, { flags: "a" });
+
+indexStream.write(`\
+---
+/*
+ * This file has been automatically generated
+ */
+
+`);
+
+const htmlThemes = themes
+  .flatMap(([themeName, theme]) => {
+    const background = {
+      bg: theme["ui.background"].bg,
+      fg: theme["ui.background"].fg || "#ffaaaa",
+    };
+
+    const getColor = (key, fallback) => {
+      const value = theme[key];
+      if (typeof value === "string") {
+        return { fg: value };
+      } else if (!value) {
+        return fallback();
+      }
+      return value;
+    };
+
+    const getFallbackColor = (primaryKey, fallbackFn) => {
+      const value = theme[primaryKey];
+      if (typeof value === "string") {
+        return { fg: value };
+      } else if (!value) {
+        return fallbackFn();
+      }
+      return value;
+    };
+
+    const getUiBackground = () => background;
+    const getVariable = () => getColor("variable", () => background.fg);
+    const getFunction = () => getFallbackColor("function", getVariable);
+    const getMacro = () => getFallbackColor("function.macro", getFunction);
+    const getKeyword = () => getFallbackColor("keyword", getVariable);
+    const getUiLinenr = () => getColor("ui.linenr", () => background.fg);
+    const getUiLinenrSelected = () =>
+      getFallbackColor("ui.linenr.selected", getHighlight);
+    const getOperator = () => getColor("operator", () => background.fg);
+    const getPunctuation = () => getColor("punctuation", () => background.fg);
+    const getConstant = () => getFallbackColor("constant", getVariable);
+    const getConstantNumeric = () =>
+      getFallbackColor("constant.numeric", getConstant);
+    const getPunctuationDelimiter = () =>
+      getFallbackColor("punctuation.delimiter", getPunctuation);
+    const getVariableParameter = () =>
+      getFallbackColor("variable.parameter", getVariable);
+    const getComment = () => getColor("comment", () => background.fg);
+    const getType = () => getFallbackColor("type", getVariable);
+    const getTypeBuiltin = () => getFallbackColor("type.builtin", getType);
+
+    const getUiStatusline = () => {
+      const uiStatusline = theme["ui.statusline"];
+      if (!uiStatusline) return {};
+      return {
+        fg: uiStatusline.fg || background.fg,
+        bg: uiStatusline.bg || background.bg,
+      };
+    };
+
+    const getHighlight = () => getColor("ui.highlight", getUiBackground);
+    const getUiCursorline = () =>
+      getFallbackColor("ui.cursorline", getHighlight);
+    const getUiCursorlinePrimary = () =>
+      getFallbackColor("ui.cursorline.primary", getUiCursorline);
+
+    const getUiVirtual = () =>
+      getFallbackColor("ui.virtual", getUiVirtualInlayHint);
+
+    const getUiCursor = () =>
+      getFallbackColor("ui.cursor.primary", () => background.fg);
+
+    const getUiVirtualInlayHint = () =>
+      getColor("ui.virtual.inlay-hint", () => background.fg);
+    const getUiVirtualIndentGuide = () =>
+      getFallbackColor("ui.virtual.indent-guide", getUiVirtualInlayHint);
+    const getUiVirtualRuler = () =>
+      getFallbackColor("ui.virtual.ruler", getUiVirtualIndentGuide);
+
+    return [
+      [
+        themeName,
+        theme.palette,
+        {
+          "ui.background": getUiBackground(),
+          keyword: getKeyword(),
+          variable: getVariable(),
+          "variable.parameter": getVariableParameter(),
+          "ui.statusline": getUiStatusline(),
+          "ui.linenr": getUiLinenr(),
+          function: getFunction(),
+          "function.macro": getMacro(),
+          operator: getOperator(),
+          punctuation: getPunctuation(),
+          "punctuation.delimiter": getPunctuationDelimiter(),
+          constant: getConstant(),
+          "constant.numeric": getConstantNumeric(),
+          comment: getComment(),
+          "type.builtin": getTypeBuiltin(),
+          "ui.cursorline.primary": getUiCursorlinePrimary(),
+          "ui.linenr.selected": getUiLinenrSelected(),
+          "ui.virtual.ruler": getUiVirtualRuler(),
+          "ui.virtual": getUiVirtual(),
+          "ui.cursor": getUiCursor(),
+        },
+      ],
+    ];
+  })
+  .map(([name, palette, theme]) => {
+    return [
+      name,
+      Object.fromEntries(
+        Object.entries(theme).map(([scope, color]) => {
+          const isHex = (s) => s.startsWith("#");
+          if (typeof color.fg === "string") {
+            if (!isHex(color.fg)) {
+              color.fg = palette[color.fg];
+            }
+          }
+          if (typeof color.bg === "string") {
+            if (!isHex(color.bg)) {
+              color.bg = palette[color.bg];
+            }
+          }
+          return [scope, color];
+        }),
+      ),
+    ];
+  })
+  .map(themeToHtml);
+
+const themePascalNames = htmlThemes.map(([themeName, theme]) => {
+  const themeNamePascalCase = themeName
+    // create dash intermediary separators for proper PascalCase conversion
+    .replaceAll("_", "-")
+    .replace(/\w+/g, (w) => {
+      return w[0].toUpperCase() + w.slice(1).toLowerCase();
+    })
+    // only keep alphanumeric chars
+    .replace(/\W/g, "");
+
+  const themeWritePath = path.join(
+    writeDirectory,
+    `${themeNamePascalCase}.astro`,
+  );
+
+  const comment = `\
+---
+/*
+ * This file has been automatically generated by the script \`pnpm termshot-themes\`
+ *
+ * See helix-editor.vercel.app/contributing/this-site#termshots for more information
+ */
+---
+
+`;
+  const htmlTheme = `${comment}${theme}`;
+
+  fs.writeFileSync(themeWritePath, htmlTheme);
+
+  indexStream.write(
+    `import ${themeNamePascalCase} from "./list-of-themes/${themeNamePascalCase}.astro"\n`,
+  );
+
+  return themeNamePascalCase;
+});
+
+indexStream.write("---\n\n");
+
+themePascalNames.forEach((themeFilepath) => {
+  indexStream.write(`<${themeFilepath} />\n\n`);
+});
