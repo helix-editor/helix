910d370fad33ba69c7b219b4d6ffde68da87c6ea
Rolo <roloedits@gmail.com>
2024-12-18 02:07:14 -0800

    feat: add `takes` field for Flag
    This indicates if the flag takes an argument

--- helix-term/src/commands/flag.rs
+++ helix-term/src/commands/flag.rs
@@ -2,29 +2,61 @@ pub struct Flag {
     pub long: &'static str,
     pub short: Option<&'static str>,
     pub desc: &'static str,
+    pub takes: bool,
 }
 
 #[macro_export]
 macro_rules! flag {
+    // Case: All fields provided
     (
         long: $long:expr,
         short: $short:expr,
-        desc: $desc:expr
+        desc: $desc:expr,
+        takes: $takes:expr $(,)?
     ) => {
         $crate::commands::flag::Flag {
             long: $long,
             short: Some($short),
             desc: $desc,
+            takes: $takes,
         }
     };
+    // Case: All fields except takes
     (
         long: $long:expr,
-        desc: $desc:expr
+        short: $short:expr,
+        desc: $desc:expr $(,)?
+    ) => {
+        $crate::commands::flag::Flag {
+            long: $long,
+            short: Some($short),
+            desc: $desc,
+            takes: false,
+        }
+    };
+    // Case: Only long, desc, and takes
+    (
+        long: $long:expr,
+        desc: $desc:expr,
+        takes: $takes:expr $(,)?
     ) => {
         $crate::commands::flag::Flag {
             long: $long,
             short: None,
             desc: $desc,
+            takes: $takes,
+        }
+    };
+    // Case: Only long and desc
+    (
+        long: $long:expr,
+        desc: $desc:expr $(,)?
+    ) => {
+        $crate::commands::flag::Flag {
+            long: $long,
+            short: None,
+            desc: $desc,
+            takes: false,
         }
     };
 }
@@ -37,12 +69,14 @@ mod tests {
         let full = flag! {
            long: "--all",
            short: "-a",
-           desc:  "clears all registers"
+           desc:  "clears all registers",
+           takes: true,
         };
 
         assert_eq!("--all", full.long);
         assert_eq!(Some("-a"), full.short);
         assert_eq!("clears all registers", full.desc);
+        assert!(full.takes);
 
         let partial = flag! {
            long: "--all",
@@ -52,5 +86,6 @@ mod tests {
         assert_eq!("--all", partial.long);
         assert_eq!(None, partial.short);
         assert_eq!("clears all registers", partial.desc);
+        assert!(!partial.takes);
     }
 }
