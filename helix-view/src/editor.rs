ba4c3783fec2155aa3004c4b114a44b941e73bb0
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2024-12-20 18:08:51 +0300

    map keyboard scancode to KeyCode

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -21,6 +21,10 @@ use helix_vcs::DiffProviderRegistry;
 use futures_util::stream::select_all::SelectAll;
 use futures_util::{future, StreamExt};
 use helix_lsp::{Call, LanguageServerId};
+
+#[cfg(feature = "scancode")]
+use crate::scancode::{deserialize_scancode, KeyboardState, ScanCodeMap};
+
 use tokio_stream::wrappers::UnboundedReceiverStream;
 
 use std::{
@@ -370,6 +374,9 @@ pub struct Config {
     /// Whether to read settings from [EditorConfig](https://editorconfig.org) files. Defaults to
     /// `true`.
     pub editor_config: bool,
+    #[cfg(feature = "scancode")]
+    #[serde(skip_serializing, deserialize_with = "deserialize_scancode")]
+    pub scancode: ScanCodeMap,
 }
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize, Eq, PartialOrd, Ord)]
@@ -940,6 +947,7 @@ impl From<LineEndingConfig> for LineEnding {
             LineEndingConfig::Crlf => LineEnding::Crlf,
             #[cfg(feature = "unicode-lines")]
             LineEndingConfig::FF => LineEnding::FF,
+
             #[cfg(feature = "unicode-lines")]
             LineEndingConfig::CR => LineEnding::CR,
             #[cfg(feature = "unicode-lines")]
@@ -1017,6 +1025,8 @@ impl Default for Config {
             end_of_line_diagnostics: DiagnosticFilter::Disable,
             clipboard_provider: ClipboardProvider::default(),
             editor_config: true,
+            #[cfg(feature = "scancode")]
+            scancode: ScanCodeMap::default(),
         }
     }
 }
@@ -1119,6 +1129,8 @@ pub struct Editor {
 
     pub mouse_down_range: Option<Range>,
     pub cursor_cache: CursorCache,
+    #[cfg(feature = "scancode")]
+    pub keyboard_state: KeyboardState,
 }
 
 pub type Motion = Box<dyn Fn(&mut Editor)>;
@@ -1241,6 +1253,8 @@ impl Editor {
             handlers,
             mouse_down_range: None,
             cursor_cache: CursorCache::default(),
+            #[cfg(feature = "scancode")]
+            keyboard_state: KeyboardState::new(),
         }
     }
 
@@ -2240,6 +2254,13 @@ impl Editor {
 
     pub fn get_last_cwd(&mut self) -> Option<&Path> {
         self.last_cwd.as_deref()
+    }
+
+    #[cfg(feature = "scancode")]
+    pub fn scancode_apply(&mut self, event: KeyEvent) -> KeyEvent {
+        self.config()
+            .scancode
+            .apply(event, &mut self.keyboard_state)
     }
 }
 
