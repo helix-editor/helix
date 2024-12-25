ea543094b478d1c4f1d6e9fa00c767e921841202
Rolo <roloedits@gmail.com>
2024-12-25 08:17:18 -0800

    feat: build out prompt structure

--- helix-view/src/commands/custom.rs
+++ helix-view/src/commands/custom.rs
@@ -4,9 +4,12 @@
 // TODO: Need to get access to a new table in the config: [commands].
 // TODO: Could add an `aliases` to `CustomTypableCommand` and then add those as well?
 
+use std::fmt::Write;
+
 use serde::{Deserialize, Serialize};
 
 // TODO: Might need to manually implement Serialize and Deserialize
+//     -Will need to do so if want to use `Arc` to make cloning cheaper.
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
 pub struct CustomTypeableCommands {
     pub commands: Vec<CustomTypableCommand>,
@@ -15,15 +18,32 @@ pub struct CustomTypeableCommands {
 impl Default for CustomTypeableCommands {
     fn default() -> Self {
         Self {
-            commands: vec![CustomTypableCommand {
-                name: String::from(":lg"),
-                desc: Some(String::from("runs lazygit in a floating pane")),
-                commands: vec![String::from(
-                    ":sh wezterm cli spawn --floating-pane lazygit",
-                )],
-                accepts: None,
-                completer: None,
-            }],
+            commands: vec![
+                CustomTypableCommand {
+                    name: String::from(":lg"),
+                    desc: Some(String::from("runs lazygit in a floating pane")),
+                    commands: vec![String::from(
+                        ":sh wezterm cli spawn --floating-pane lazygit",
+                    )],
+                    accepts: None,
+                    completer: None,
+                },
+                CustomTypableCommand {
+                    name: String::from(":w"),
+                    desc: Some(String::from(
+                        "writes buffer forcefully and changes directory",
+                    )),
+                    commands: vec![
+                        String::from(":write --force %{arg}"),
+                        String::from(":cd %sh{ %{arg} | path dirname }"),
+                        String::from(":cd %sh{ %{arg} | path dirname }"),
+                        String::from(":cd %sh{ %{arg} | path dirname }"),
+                        String::from(":cd %sh{ %{arg} | path dirname }"),
+                    ],
+                    accepts: Some(String::from("<path>")),
+                    completer: Some(String::from(":write")),
+                },
+            ],
         }
     }
 }
@@ -46,6 +66,7 @@ impl CustomTypeableCommands {
     }
 }
 
+// TODO: Arc<str> ?
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
 pub struct CustomTypableCommand {
     pub name: String,
@@ -61,7 +82,53 @@ impl CustomTypableCommand {
         //
         // maps:
         //     :write --force %{arg} -> :cd %sh{ %{arg} | path dirname }
-        todo!()
+        let mut prompt = String::new();
+
+        prompt.push_str(self.name.trim_start_matches(':'));
+
+        if let Some(accepts) = &self.accepts {
+            write!(prompt, " {accepts}").unwrap();
+        }
+
+        prompt.push(':');
+
+        // TODO: Might need to port the spacing algo from argument flags branch.
+        if let Some(desc) = &self.desc {
+            write!(prompt, " {desc}").unwrap();
+        }
+
+        prompt.push('\n');
+        prompt.push('\n');
+
+        writeln!(prompt, "maps:").unwrap();
+        prompt.push_str("   ");
+
+        for (idx, command) in self.commands.iter().enumerate() {
+            write!(prompt, ":{}", command.trim_start_matches(':')).unwrap();
+
+            if idx + 1 == self.commands.len() {
+                break;
+            }
+
+            // There are two columns of commands, and after that they will overflow
+            // downward:
+            //
+            // maps:
+            //     :write --force %{arg} -> :cd %sh{ %{arg} | path dirname }
+            //     -> :write --force %{arg} -> :cd %sh{ %{arg} | path dirname }
+            //     -> :write --force %{arg} -> :cd %sh{ %{arg} | path dirname }
+            //
+            // Its starts with `->` to indicate that its not a new `:command`
+            // but still one sequence.
+            if idx % 2 == 0 {
+                prompt.push('\n');
+                prompt.push_str("    -> ");
+            } else {
+                prompt.push_str(" -> ");
+            }
+        }
+
+        prompt
     }
 
     pub fn iter(&self) -> impl Iterator<Item = &str> {
