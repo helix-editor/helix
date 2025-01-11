8a67c6d7117ca7ac7e92ed3faf9444b8a2f1fdf0
j03-dev <24nomeniavo@gmail.com>
2025-01-11 20:39:30 +0300

    feat: highlight html,css on angular inline component

---
+++ runtime/queries/angular/injections.scm
@@ -0,0 +1,7 @@
+((property_identifier) @prop (#eq? @prop "template")
+ (template_string) @injection.content
+ (#set! injection.language "html"))
+
+((property_identifier) @prop (#eq? @prop "styles")
+ (template_string) @injection.content
+ (#set! injection.language "css"))
