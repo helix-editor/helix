655453821bf3e2a821eda699acbc1def7ba2cab1
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-01-28 20:56:32 +0000

    feat: `.multicursor` file type name

---
+++ runtime/queries/multicursor/highlights.scm
@@ -0,0 +1,20 @@
+[
+  (start_left_primary)
+  (end_left_primary)
+  (start_right_primary)
+  (end_right_primary)
+  (start_left)
+  (end_left)
+  (start_right)
+  (end_right)
+] @punctuation.special
+
+(cursor_primary) @ui.cursor.primary
+(cursor) @ui.cursor
+
+(left_primary (char) @ui.selection.primary)
+(right_primary (char) @ui.selection.primary)
+(left (char) @ui.selection)
+(right (char) @ui.selection)
+
+(ERROR) @error
