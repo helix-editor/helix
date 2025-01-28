03d627a20d91b410fc9d5e1b3e092db57effae32
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-01-28 14:11:48 +0000

    chore: remove example

--- helix-term/src/ui/markdown.rs
+++ helix-term/src/ui/markdown.rs
@@ -28,30 +28,7 @@ fn styled_multiline_text<'a>(text: &str, style: Style) -> Text<'a> {
         .collect();
     Text::from(spans)
 }
-/// `copy_selection_on_prev_line`
-///
-/// Copies the current primary selection to the first previous line long enough to accomodate it.
-///
-/// # Examples
-///
-/// The selection is copied from line 2 to line 1.
-///
-/// Before:
-///
-/// ```helix
-/// This is text #[|on line 1]#.
-/// This is text #(|on line 1)#.
-/// This is text on line 2.
-/// ```
-///
-/// Command: `C`
-///
-/// After:
-///
-/// ```helix
-/// This is text #(|on line 1)#.
-/// This is text #[|on line 2]#.
-/// ```
+
 pub fn highlighted_code_block<'a>(
     text: &str,
     language: &str,
