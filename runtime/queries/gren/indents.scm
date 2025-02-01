046684a4222bc7cf910436f3ec2de7ab92bafb3a
Robin Heggelund Hansen <git@heghan.org>
2025-02-01 08:28:06 +0100

    Simplify indents queries, and add indent after module declaration.

--- runtime/queries/gren/indents.scm
+++ runtime/queries/gren/indents.scm
@@ -1,10 +1,14 @@
-((value_declaration body: (_)) @indent)
-((type_alias_declaration typeExpression: (_)) @indent)
-((type_declaration unionVariant: (_)) @indent @extend)
+((module_declaration) @indent)
+((value_declaration) @indent)
+((type_alias_declaration) @indent)
+((type_declaration) @indent @extend)
+
 ((when_is_expr) @indent)
 ((when_is_branch) @indent)
 ((when_is_branch expr: (_)) @indent)
+
 ((let_in_expr "let") @indent @extend)
+
 ((if_else_expr "if") @indent)
 ((if_else_expr "if" (_) "then") @indent)
 ((if_else_expr "if" (_) "then" (_) "else") @indent)
