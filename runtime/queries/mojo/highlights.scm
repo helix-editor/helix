24e32b39d00af5a9d28dae0c6fddf10a8035b838
Michael Davis <mcarsondavis@gmail.com>
2025-02-02 17:01:11 -0500

    mojo

--- runtime/queries/mojo/highlights.scm
+++ runtime/queries/mojo/highlights.scm
@@ -1,3 +1,28 @@
+; Variables
+
+(identifier) @variable
+
+(attribute attribute: (identifier) @variable.other.member)
+
+((identifier) @constant
+  (#match? @constant "^_*[A-Z][A-Z\\d_]*$"))
+
+((identifier) @type
+  (#match? @type "^[A-Z]"))
+
+; Literals
+(none) @constant.builtin
+[
+  (true)
+  (false)
+] @constant.builtin.boolean
+
+(integer) @constant.numeric.integer
+(float) @constant.numeric.float
+(comment) @comment
+(string) @string
+(escape_sequence) @constant.character.escape
+
 ; Docstrings
 
 (expression_statement (string) @comment.block.documentation)
@@ -97,30 +122,6 @@
 
 (class_definition name: (identifier) @type)
 (class_definition superclasses: (argument_list (identifier) @type))
-
-; Variables
-
-((identifier) @constant
-  (#match? @constant "^_*[A-Z][A-Z\\d_]*$"))
-
-((identifier) @type
-  (#match? @type "^[A-Z]"))
-
-(attribute attribute: (identifier) @variable.other.member)
-(identifier) @variable
-
-; Literals
-(none) @constant.builtin
-[
-  (true)
-  (false)
-] @constant.builtin.boolean
-
-(integer) @constant.numeric.integer
-(float) @constant.numeric.float
-(comment) @comment
-(string) @string
-(escape_sequence) @constant.character.escape
 
 ["," "." ":" ";" (ellipsis)] @punctuation.delimiter
 (interpolation
