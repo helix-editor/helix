376d50046edccfc8f057dbea7c4e1317c7808c31
Michael Davis <mcarsondavis@gmail.com>
2025-02-02 15:58:41 -0500

    comment

--- runtime/queries/comment/highlights.scm
+++ runtime/queries/comment/highlights.scm
@@ -1,34 +1,34 @@
-; Hint level tags
-((tag (name) @hint)
- (#match? @hint "^(HINT|MARK|PASSED|STUB|MOCK)$"))
-
-("text" @hint
- (#match? @hint "^(HINT|MARK|PASSED|STUB|MOCK)$"))
-
-; Info level tags
-((tag (name) @info)
- (#match? @info "^(INFO|NOTE|TODO|PERF|OPTIMIZE|PERFORMANCE|QUESTION|ASK)$"))
-
-("text" @info
- (#match? @info "^(INFO|NOTE|TODO|PERF|OPTIMIZE|PERFORMANCE|QUESTION|ASK)$"))
-
-; Warning level tags
-((tag (name) @warning)
- (#match? @warning "^(HACK|WARN|WARNING|TEST|TEMP)$"))
-
-("text" @warning
- (#match? @warning "^(HACK|WARN|WARNING|TEST|TEMP)$"))
-
-; Error level tags
-((tag (name) @error)
- (#match? @error "^(BUG|FIXME|ISSUE|XXX|FIX|SAFETY|FIXIT|FAILED|DEBUG|INVARIANT|COMPLIANCE)$"))
-
-("text" @error
- (#match? @error "^(BUG|FIXME|ISSUE|XXX|FIX|SAFETY|FIXIT|FAILED|DEBUG|INVARIANT|COMPLIANCE)$"))
-
 (tag
  (name) @ui.text
  (user)? @constant)
+
+; Hint level tags
+((tag (name) @hint)
+ (#any-of? @hint "HINT" "MARK" "PASSED" "STUB" "MOCK"))
+
+("text" @hint
+ (#any-of? @hint "HINT" "MARK" "PASSED" "STUB" "MOCK"))
+
+; Info level tags
+((tag (name) @info)
+ (#any-of? @info "INFO" "NOTE" "TODO" "PERF" "OPTIMIZE" "PERFORMANCE" "QUESTION" "ASK"))
+
+("text" @info
+ (#any-of? @info "INFO" "NOTE" "TODO" "PERF" "OPTIMIZE" "PERFORMANCE" "QUESTION" "ASK"))
+
+; Warning level tags
+((tag (name) @warning)
+ (#any-of? @warning "HACK" "WARN" "WARNING" "TEST" "TEMP"))
+
+("text" @warning
+ (#any-of? @warning "HACK" "WARN" "WARNING" "TEST" "TEMP"))
+
+; Error level tags
+((tag (name) @error)
+ (#any-of? @error "BUG" "FIXME" "ISSUE" "XXX" "FIX" "SAFETY" "FIXIT" "FAILED" "DEBUG" "INVARIANT" "COMPLIANCE"))
+
+("text" @error
+ (#any-of? @error "BUG" "FIXME" "ISSUE" "XXX" "FIX" "SAFETY" "FIXIT" "FAILED" "DEBUG" "INVARIANT" "COMPLIANCE"))
 
 ; Issue number (#123)
 ("text" @constant.numeric
