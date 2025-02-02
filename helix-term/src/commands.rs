22c59dc4b1fda8cb37885dc88e80a837fb24ecc6
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-02 18:39:56 +0000

    refactor: rename variables ,use iterator methods, separate vars for added and removed chars

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -5209,6 +5209,7 @@ fn toggle_line_comments(cx: &mut Context) {
                 // also include the added tokens.
                 let mut selections = SmallVec::new();
                 let mut added_chars = 0;
+                let mut removed_chars = 0;
                 let transaction = Transaction::change(
                     rope,
                     selection.iter().flat_map(|range| {
@@ -5233,6 +5234,7 @@ fn toggle_line_comments(cx: &mut Context) {
                                 block_comment_tokens,
                                 &mut selections,
                                 &mut added_chars,
+                                &mut removed_chars,
                             )
                         } else {
                             comment::toggle_line_comments(rope, range, line_token)
@@ -5253,6 +5255,7 @@ fn toggle_block_comments(cx: &mut Context) {
             |doc_line_token, doc_block_tokens, rope, selection, mut get_injected_tokens| {
                 let mut selections = SmallVec::new();
                 let mut added_chars = 0;
+                let mut removed_chars = 0;
                 let transaction = Transaction::change(
                     rope,
                     selection.iter().flat_map(|range| {
@@ -5279,6 +5282,7 @@ fn toggle_block_comments(cx: &mut Context) {
                                 block_comment_tokens,
                                 &mut selections,
                                 &mut added_chars,
+                                &mut removed_chars,
                             )
                         }
                     }),
