a2fdc172b1d56a8a5eefa67c1b3e69e697f163cf
Michael Davis <mcarsondavis@gmail.com>
2025-02-02 16:07:53 -0500

    yuck

--- runtime/queries/yuck/highlights.scm
+++ runtime/queries/yuck/highlights.scm
@@ -1,3 +1,6 @@
+(ident) @variable
+(index) @variable
+
 ; Errors
 
 (ERROR) @error
@@ -64,6 +67,12 @@
 (function_call
   name: (ident) @function)
 
+; Tags
+
+; TODO apply to every symbol in list? I think it should probably only be applied to the first child of the list
+(list
+  (symbol) @tag)
+
 ; Variables
 
 (ident) @variable
@@ -94,14 +103,3 @@
 (loop_widget . "for" @keyword.control.repeat . (symbol) @variable . "in" @keyword.operator . (symbol) @variable)
 
 (loop_widget . "for" @keyword.control.repeat . (symbol) @variable . "in" @keyword.operator)
-
-; Tags
-
-; TODO apply to every symbol in list? I think it should probably only be applied to the first child of the list
-(list
-  (symbol) @tag)
-
-; Other stuff that has not been caught by the previous queries yet
-
-(ident) @variable
-(index) @variable
