7e4e37deafc92f8b97b35b5c7a154ecb5b57ab1d
uncenter <47499684+uncenter@users.noreply.github.com>
2025-02-03 09:21:42 -0500

    Update highlights

--- runtime/queries/tera/highlights.scm
+++ runtime/queries/tera/highlights.scm
@@ -1,9 +1,169 @@
-; Namespaces
+; Variables
+;----------
 
-(import_statement
-  scope: (identifier) @namespace)
+(identifier) @variable
+
+((identifier) @variable.builtin
+  (#any-of? @variable.builtin
+    "loop"
+    "__tera_context"))
+
+; Properties
+;-----------
+
+(member_expression
+  property: (identifier)? @variable.other.member)
+
+; Literals
+;-----------
+
+(string) @string
+
+(bool) @constant.builtin
+
+(number) @constant.numeric
+
+; Tokens
+;-----------
+
+[
+  "."
+  ","
+] @punctuation.delimiter
+
+[
+  "*"
+  "/"
+  "%"
+  "|"
+  "+"
+  "-"
+  "~"
+  "="
+  "=="
+  "!="
+  "<"
+  ">"
+  "<="
+  ">="
+] @operator
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{%"
+  "%}"
+  "-%}"
+  "{%-"
+  "}}"
+  "{{"
+  "-}}"
+  "{{-"
+  "::"
+] @punctuation.bracket
+
+; Tags
+;-----------
+
+(comment_tag) @comment
+
+; Keywords
+;-----------
+
+[
+  "if"
+  "elif"
+  "else"
+  "endif"
+] @keyword.control.conditional
+
+[
+  "for"
+  "endfor"
+] @keyword.control.repeat
+
+[
+  "include"
+  "import"
+  "extends"
+] @keyword.control.import
+
+[
+  "in"
+  "and"
+  "or"
+  "not"
+  "is"
+] @keyword.operator
+
+[
+  "break"
+  "continue"
+] @keyword.control.return
+
+[
+  "set"
+  "set_global"
+  "filter"
+  "endfilter"
+  "block"
+  "endblock"
+  "macro"
+  "endmacro"
+  "raw"
+  "endraw"
+  "as"
+] @keyword
 
 ; Functions
+;-----------
+
+(macro_statement
+  name: (identifier) @function
+  (parameter_list
+    parameter: (identifier) @variable.parameter
+    (optional_parameter
+      name: (identifier) @variable.parameter)))
+
+(call_expression
+  scope: (identifier)? @namespace
+  name: (identifier) @function)
+
+(call_expression
+  name: (identifier) @function.builtin
+  (#any-of? @function.builtin
+    ; Functions - https://keats.github.io/tera/docs/#built-in-functions
+    "range"
+    "now"
+    "throw"
+    "get_random"
+    "get_env"))
+
+(test_expression
+  test: (identifier) @function)
+
+(test_expression
+  test: (identifier) @function.builtin
+  (#any-of? @function.builtin
+    ; Tests - https://keats.github.io/tera/docs/#built-in-tests
+    "defined"
+    "undefined"
+    "odd"
+    "even"
+    "string"
+    "number"
+    "divisibleby"
+    "iterable"
+    "object"
+    "starting_with"
+    "ending_with"
+    "containing"
+    "matching"))
+
+(filter_expression
+  filter: (identifier) @function.method)
 
 (filter_expression
   filter: (identifier) @function.builtin
@@ -58,161 +218,8 @@
     "as_str"
     "default"))
 
-(filter_expression
-  filter: (identifier) @function.method)
+; Namespaces
+;-----------
 
-(test_expression
-  test: (identifier) @function.builtin
-  (#any-of? @function.builtin
-    ; Tests - https://keats.github.io/tera/docs/#built-in-tests
-    "defined"
-    "undefined"
-    "odd"
-    "even"
-    "string"
-    "number"
-    "divisibleby"
-    "iterable"
-    "object"
-    "starting_with"
-    "ending_with"
-    "containing"
-    "matching"))
-
-(test_expression
-  test: (identifier) @function)
-
-(call_expression
-  name: (identifier) @function.builtin
-  (#any-of? @function.builtin
-    ; Functions - https://keats.github.io/tera/docs/#built-in-functions
-    "range"
-    "now"
-    "throw"
-    "get_random"
-    "get_env"))
-
-(call_expression
-  scope: (identifier)? @namespace
-  name: (identifier) @function)
-
-(macro_statement
-  name: (identifier) @function
-  (parameter_list
-    parameter: (identifier) @variable.parameter
-    (optional_parameter
-      name: (identifier) @variable.parameter)))
-
-; Keywords
-
-[
-  "set"
-  "set_global"
-  "filter"
-  "endfilter"
-  "block"
-  "endblock"
-  "macro"
-  "endmacro"
-  "raw"
-  "endraw"
-  "as"
-] @keyword
-
-[
-  "break"
-  "continue"
-] @keyword.control.return
-
-[
-  "in"
-  "and"
-  "or"
-  "not"
-  "is"
-] @keyword.operator
-
-[
-  "include"
-  "import"
-  "extends"
-] @keyword.control.import
-
-[
-  "for"
-  "endfor"
-] @keyword.control.repeat
-
-[
-  "if"
-  "elif"
-  "else"
-  "endif"
-] @keyword.control.conditional
-
-; Tags
-
-(comment_tag) @comment
-
-; Tokens
-
-[
-  "("
-  ")"
-  "["
-  "]"
-  "{%"
-  "%}"
-  "-%}"
-  "{%-"
-  "}}"
-  "{{"
-  "-}}"
-  "{{-"
-  "::"
-] @punctuation.bracket
-
-[
-  "*"
-  "/"
-  "%"
-  "|"
-  "+"
-  "-"
-  "~"
-  "="
-  "=="
-  "!="
-  "<"
-  ">"
-  "<="
-  ">="
-] @operator
-
-[
-  "."
-  ","
-] @punctuation.delimiter
-
-
-; Literals
-
-(number) @constant.numeric
-
-(bool) @constant.builtin
-
-(string) @string
-
-; Properties
-
-(member_expression
-  property: (identifier)? @variable.other.member)
-
-; Variables
-
-((identifier) @variable.builtin
-  (#any-of? @variable.builtin
-    "loop"
-    "__tera_context"))
-
-(identifier) @variable
+(import_statement
+  scope: (identifier) @namespace)
