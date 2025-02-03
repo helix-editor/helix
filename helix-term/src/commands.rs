7d53290dd23d4f7c0323bcb38b16a43370bc73dc
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-03 12:46:12 +0000

    fix: panic

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -5244,7 +5244,11 @@ fn toggle_line_comments(cx: &mut Context) {
                 }),
             );
 
-            transaction.with_selection(Selection::new(selections, selection.primary_index()))
+            if selections.is_empty() {
+                transaction
+            } else {
+                transaction.with_selection(Selection::new(selections, selection.primary_index()))
+            }
         },
     );
 }
