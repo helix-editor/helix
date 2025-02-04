d37c979ff2460ae3566b1795aaa7b2fda49975c0
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-04 18:15:21 +0000

    feat: use a general (attribute)

---
+++ runtime/queries/test/highlights.scm
@@ -0,0 +1,15 @@
+(name) @markup.heading.1
+
+(attribute) @attribute
+
+(attribute
+  language: (parameter) @string)
+
+(attribute
+  platform: (parameter) @constant.builtin)
+
+[
+  (separator)
+  ")"
+  "("
+] @punctuation
