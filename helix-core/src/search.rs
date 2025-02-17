f8561763e3b5739881325baba14ef602a9509ba7
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-17 10:50:53 +0000

    docs: add info for {extend,find}_{next,prev}_pair

--- helix-core/src/search.rs
+++ helix-core/src/search.rs
@@ -44,35 +44,6 @@ pub fn find_nth_next<M: CharMatcher>(
     Some(pos - 1)
 }
 
-pub fn find_nth_next_pair<M: CharMatcher>(
-    text: RopeSlice,
-    char_matcher: M,
-    char_matcher_2: M,
-    mut pos: usize,
-    n: usize,
-) -> Option<usize> {
-    if pos >= text.len_chars() || n == 0 {
-        return None;
-    }
-
-    let mut chars = text.chars_at(pos).peekable();
-
-    for _ in 0..n {
-        loop {
-            let c = chars.next()?;
-            let c2 = chars.peek()?;
-
-            pos += 1;
-
-            if char_matcher.char_match(c) && char_matcher_2.char_match(*c2) {
-                break;
-            }
-        }
-    }
-
-    Some(pos - 1)
-}
-
 pub fn find_nth_prev(text: RopeSlice, ch: char, mut pos: usize, n: usize) -> Option<usize> {
     if pos == 0 || n == 0 {
         return None;
@@ -95,6 +66,35 @@ pub fn find_nth_prev(text: RopeSlice, ch: char, mut pos: usize, n: usize) -> Opt
     Some(pos)
 }
 
+pub fn find_nth_next_pair<M: CharMatcher>(
+    text: RopeSlice,
+    char_matcher_left: M,
+    char_matcher_right: M,
+    mut pos: usize,
+    n: usize,
+) -> Option<usize> {
+    if pos >= text.len_chars() || n == 0 {
+        return None;
+    }
+
+    let mut chars = text.chars_at(pos).peekable();
+
+    for _ in 0..n {
+        loop {
+            let ch_left = chars.next()?;
+            let ch_right = chars.peek()?;
+
+            pos += 1;
+
+            if char_matcher_left.char_match(ch_left) && char_matcher_right.char_match(*ch_right) {
+                break;
+            }
+        }
+    }
+
+    Some(pos - 1)
+}
+
 pub fn find_nth_prev_pair<M: CharMatcher>(
     text: RopeSlice,
     char_matcher_left: M,
@@ -110,17 +110,16 @@ pub fn find_nth_prev_pair<M: CharMatcher>(
 
     for _ in 0..n {
         loop {
-            let c_right = chars.next()?;
-            let c_left = chars.peek()?;
+            let ch_right = chars.next()?;
+            let ch_left = chars.peek()?;
 
             pos -= 1;
 
-            if char_matcher_left.char_match(*c_left) && char_matcher_right.char_match(c_right) {
+            if char_matcher_left.char_match(*ch_left) && char_matcher_right.char_match(ch_right) {
                 break;
             }
         }
     }
-    log::error!("{pos}");
 
     Some(pos - 1)
 }
