fac6c7c1a6ddccd2b70c8de61769f3be9e413c00
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-18 12:38:27 +0000

    chore: appease clippy

--- helix-term/src/ui/mod.rs
+++ helix-term/src/ui/mod.rs
@@ -515,7 +515,7 @@ pub fn file_explorer(root: PathBuf, editor: &Editor) -> Result<FileExplorer, std
                     };
 
                     if confirmation.ends_with(std::path::MAIN_SEPARATOR) {
-                        if let Err(err) = fs::remove_dir_all(&to_delete).map_err(
+                        if let Err(err) = fs::remove_dir_all(to_delete).map_err(
                             |err| format!(
                                 "Unable to delete directory {}: {err}", to_delete.display()
                             )
@@ -525,7 +525,7 @@ pub fn file_explorer(root: PathBuf, editor: &Editor) -> Result<FileExplorer, std
 
                         Some(Ok(format!("Deleted directory: {}", to_delete.display())))
                     } else {
-                        if let Err(err) = fs::remove_file(&to_delete).map_err(
+                        if let Err(err) = fs::remove_file(to_delete).map_err(
                             |err| format!(
                                 "Unable to delete file {}: {err}", to_delete.display()
                             )
