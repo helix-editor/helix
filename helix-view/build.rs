a440a27daa8e4895eadb835f5c3052c4a6627e95
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2025-02-19 23:18:09 +0300

    style: format

--- helix-view/build.rs
+++ helix-view/build.rs
@@ -1,5 +1,9 @@
 fn main() {
     // alias scancode feature flag
-    #[cfg(any(feature = "scancode-query", feature = "scancode-evdev", feature = "scancode-hidapi"))]
+    #[cfg(any(
+        feature = "scancode-query",
+        feature = "scancode-evdev",
+        feature = "scancode-hidapi"
+    ))]
     println!("cargo:rustc-cfg=scancode")
 }
