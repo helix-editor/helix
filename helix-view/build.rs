cbfd1cb2622732de478491f4be92c8b282fcf3b8
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2025-02-19 23:09:17 +0300

    feat: hidapi to listen keyboard events

--- helix-view/build.rs
+++ helix-view/build.rs
@@ -1,5 +1,5 @@
 fn main() {
     // alias scancode feature flag
-    #[cfg(any(feature = "scancode-query", feature = "scancode-evdev"))]
+    #[cfg(any(feature = "scancode-query", feature = "scancode-evdev", feature = "scancode-hidapi"))]
     println!("cargo:rustc-cfg=scancode")
 }
