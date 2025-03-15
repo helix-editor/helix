a63955709367eb222c291ac5f5e2bbf8e57976ca
Nik Revenco <154856872+nik-rev@users.noreply.github.com>
2025-03-15 17:15:39 +0000

    revert: remove cargo virtual manifest

--- runtime/queries/markdown-rustdoc/injections.scm
+++ runtime/queries/markdown-rustdoc/injections.scm
@@ -8,13 +8,6 @@
   (#set! injection.language "rust")
   (#set! injection.include-unnamed-children))
 
-; cargo-script uses an embedded virtual Cargo manifest
-(fenced_code_block
-  (info_string
-    (language) @_language)
-  (code_fence_content) @injection.content
-(#eq? @_language "cargo") (#set! injection.language "toml") (#set! injection.include-unnamed-children))
-
 (fenced_code_block
   (info_string
     (language) @injection.language)
