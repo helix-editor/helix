3345874a2e2a1307d3d2faea90e2f1d34c82e91a
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-03-20 03:00:00 +0000

    fix: remove clashing imports

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -8,13 +8,7 @@ use std::{env, error::Error};
 type DynError = Box<dyn Error>;
 
 pub mod tasks {
-    use crate::docgen::{lang_features, static_commands, typable_commands, write};
-    use crate::docgen::{
-        LANG_SUPPORT_MD_OUTPUT, STATIC_COMMANDS_MD_OUTPUT, TYPABLE_COMMANDS_MD_OUTPUT,
-    };
     use crate::grammarcheck::grammar_check;
-    use crate::querycheck::query_check;
-    use crate::theme_check::theme_check;
     use crate::DynError;
 
     pub fn grammarcheck() -> Result<(), DynError> {
