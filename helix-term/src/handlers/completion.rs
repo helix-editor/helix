a1939f4b6806ae3285c86e948174740696d9b019
Michael Davis <mcarsondavis@gmail.com>
2025-03-23 16:50:24 -0400

    minor: spurious whitespace change

--- helix-term/src/handlers/completion.rs
+++ helix-term/src/handlers/completion.rs
@@ -119,7 +119,6 @@ pub fn trigger_auto_completion(editor: &Editor, trigger_char_only: bool) {
         return;
     }
     let (view, doc): (&helix_view::View, &helix_view::Document) = current_ref!(editor);
-
     let mut text = doc.text().slice(..);
     let cursor = doc.selection(view.id).primary().cursor(text);
     text = doc.text().slice(..cursor);
