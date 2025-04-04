616758ee81565d16fdc69a061c20ef1774a2a62d
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-04-04 08:19:40 +0100

    refactor: rename macro
    _

--- helix-stdx/src/str.rs
+++ helix-stdx/src/str.rs
@@ -1,10 +1,10 @@
 /// Concatenates strings together.
 ///
-/// `concat!(a, " ", b, " ", c)` is:
+/// `str_concat!(a, " ", b, " ", c)` is:
 /// - more performant than `format!("{a} {b} {c}")`
 /// - more ergonomic than using `String::with_capacity` followed by a series of `String::push_str`
 #[macro_export]
-macro_rules! concat {
+macro_rules! str_concat {
     ($($value:expr),*) => {{
         // Rust does not allow using `+` as separator between value
         // so we must add that at the end of everything. The `0` is necessary
