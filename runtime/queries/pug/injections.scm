dd23db5211155ec4636b51c347cdc7c1f2fbd71c
Spenser Black <spenserblack01@gmail.com>
2025-04-29 13:18:28 -0400

    Inject JS and CSS in Pug
    Injects JavaScript into `script` tags and CSS into `style` tags.

--- runtime/queries/pug/injections.scm
+++ runtime/queries/pug/injections.scm
@@ -1,3 +1,15 @@
 ((javascript) @injection.content
   (#set! injection.language "javascript")
 )
+
+(
+  script_tag
+  ((content_code) @injection.content
+    (#set! injection.language "javascript"))
+)
+
+(
+  style_tag
+  ((content_code) @injection.content
+    (#set! injection.language "css"))
+)
