1c353f616de926d462a1c39c8a80d5c6ae07e55c
Michael Davis <mcarsondavis@gmail.com>
2025-05-02 09:44:50 -0400

    Avoid OOB index in tree-sitter query parser/builder
    This works around a bug in tree-sitter where the query parser/builder
    accesses an array out-of-bounds causing undefined behavior. This fixes
    a non-deterministic bug that would affect the behavior of the comment
    grammar in Rust injections - sometimes comment tokens like WARN would
    not be highlighted. Note that this change to the query is cosmetic: the
    position of predicates in a pattern is not consequential.
    
    The bug upstream is tracked in
    <https://redirect.github.com/tree-sitter/tree-sitter/issues/4421>.
    
    Co-authored-by: Pascal Kuthe <pascalkuthe@pm.me>

--- runtime/queries/rust/injections.scm
+++ runtime/queries/rust/injections.scm
@@ -60,7 +60,7 @@
 
 (call_expression
   function: (scoped_identifier
-    path: (scoped_identifier (identifier) @_regex (#any-of? @_regex "Regex" "RegexBuilder") .)
+    path: (scoped_identifier (identifier) @_regex . (#any-of? @_regex "Regex" "RegexBuilder"))
     name: (identifier) @_new (#eq? @_new "new"))
   arguments: (arguments (raw_string_literal (string_content) @injection.content))
   (#set! injection.language "regex"))
