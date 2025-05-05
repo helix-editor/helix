16e324824d224b1b1b1afb7918a0e9ff54ea0cfa
Caleb Larsen <caleb.john.larsen@gmail.com>
2025-05-05 18:19:54 -0500

    Updated tree-sitter-haskell (fixes #13428)

--- runtime/queries/haskell/locals.scm
+++ runtime/queries/haskell/locals.scm
@@ -1,4 +1,4 @@
 (signature name: (variable)) @local.definition
 (function name: (variable)) @local.definition
-(pat_name (variable)) @local.definition
-(exp_name (variable)) @local.reference
+(pattern/variable) @local.definition
+(expression/variable) @local.reference
