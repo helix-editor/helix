9e72279b099942510d25e1a70f8684cb44e4faa4
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-05-27 16:05:01 +0100

    test: remove tests for removed default keybindings

--- helix-term/tests/test/commands/paste_join.rs
+++ helix-term/tests/test/commands/paste_join.rs
@@ -8,48 +8,54 @@ const IN: &str = indoc! {"\
 
 #[tokio::test(flavor = "multi_thread")]
 async fn after() -> anyhow::Result<()> {
-    const OUT: &str = indoc! {"\
-        o#[o
-        t|]#ne
-        t#(o
-        t|)#wo
-        three
-    "};
-
-    test((IN, "y:paste-join<ret>", OUT)).await?;
-    test((IN, "y<C-p>", OUT)).await?;
+    test((
+        IN,
+        "y:paste-join<ret>",
+        indoc! {"\
+            o#[o
+            t|]#ne
+            t#(o
+            t|)#wo
+            three
+        "},
+    ))
+    .await?;
 
     Ok(())
 }
 
 #[tokio::test(flavor = "multi_thread")]
 async fn before() -> anyhow::Result<()> {
-    const OUT: &str = indoc! {"\
-        #[o
-        t|]#one
-        #(o
-        t|)#two
-        three
-    "};
-
-    test((IN, "y:paste-join --position before<ret>", OUT)).await?;
-    test((IN, "y<C-P>", OUT)).await?;
+    test((
+        IN,
+        "y:paste-join --position before<ret>",
+        indoc! {"\
+            #[o
+            t|]#one
+            #(o
+            t|)#two
+            three
+        "},
+    ))
+    .await?;
 
     Ok(())
 }
 
 #[tokio::test(flavor = "multi_thread")]
 async fn replace() -> anyhow::Result<()> {
-    const OUT: &str = indoc! {"\
+    test((
+        IN,
+        "y:paste-join --position replace<ret>",
+        indoc! {"\
             #[o
             t|]#ne
             #(o
             t|)#wo
             three
-        "};
-
-    test((IN, "y:paste-join --position replace<ret>", OUT)).await?;
-    test((IN, "y<C-R>", OUT)).await?;
+        "},
+    ))
+    .await?;
 
     Ok(())
 }
