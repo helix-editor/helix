787e2db32ba685c7d135edbc9c860100c515497e
Jeff Bencin <jeff.bencin@gmail.com>
2025-05-29 17:20:50 -0400

    Improve syntax highlighting for Clarity language

--- runtime/queries/clarity/highlights.scm
+++ runtime/queries/clarity/highlights.scm
@@ -43,27 +43,13 @@
   ","
 ] @punctuation.delimiter
 
-[
-  "block-height"
-  "burn-block-height"
-  "chain-id"
-  "contract-caller"
-  "is-in_mainnet" ; FIXME
-  "is-in-regtest"
-  "stacks-block-height"
-  "stx-liquid-supply"
-  "tenure-height"
-  "tx-sender"
-  "tx-sponsor?"
-] @keyword
-
 ; Keywords
-[
-  "some"
+(list_lit_token) @keyword
+(some_lit ("some") @keyword)
+(response_lit [
   "ok"
   "err"
-  (list_lit_token)
-] @keyword
+] @keyword)
 
 [
   "+"
@@ -84,11 +70,14 @@
 ; Functions
 (function_signature (identifier) @function)
 (function_signature_for_trait (identifier) @function)
-(basic_native_form operator: (native_identifier) @function.builtin)
-(contract_function_call operator: (identifier) @function.method)
+(contract_function_call operator: (identifier) @function)
 
+(basic_native_form operator: (native_identifier) @function.builtin)
 [
   "let"
+] @function.builtin
+
+[
   "impl-trait"
   "use-trait"
   "define-trait"
@@ -100,7 +89,7 @@
   "define-non-fungible-token"
   "define-constant"
   "define-map"
-] @function.builtin
+] @function.special
 
 ; Variables and parameters
 (function_parameter) @variable.parameter
@@ -109,4 +98,6 @@
 (tuple_lit key: (identifier) @variable)
 (tuple_type key: (identifier) @variable)
 (tuple_type_for_trait key: (identifier) @variable)
+
+(global) @variable.builtin
 
