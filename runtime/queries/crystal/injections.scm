4cd5e7b5776b3b13d00a84dcac78ffe033ab0bac
Margret Riegert <margret@eowyn.net>
2025-06-20 15:22:07 -0400

    Update Crystal tree sitter and add support for ameba-ls

--- runtime/queries/crystal/injections.scm
+++ runtime/queries/crystal/injections.scm
@@ -1,1 +1,11 @@
-; inherits: ruby
+((heredoc_body
+  (heredoc_content) @injection.content
+  (heredoc_end) @name
+  (#set! injection.language "sql"))
+  (#eq? @name "SQL"))
+
+((heredoc_body
+  (heredoc_content) @injection.content
+  (heredoc_end) @name
+  (#set! injection.language "html"))
+  (#eq? @name "HTML"))
--- runtime/queries/crystal/locals.scm
+++ runtime/queries/crystal/locals.scm
@@ -1,1 +1,14 @@
-; inherits: ruby
+((method_def) @local.scope
+ (#set! local.scope-inherits false))
+((fun_def) @local.scope
+ (#set! local.scope-inherits false))
+
+(block) @local.scope
+
+(param
+  name: (identifier) @local.definition)
+
+(assign
+  lhs: (identifier) @local.definition)
+
+(identifier) @local.reference
