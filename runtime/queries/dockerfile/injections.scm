a9d51ef2582a226758773dc3e45565c7d6f2d05b
Kristoffer Plagborg Bak Sørensen <57013304+kpbaks@users.noreply.github.com>
2025-06-26 19:51:10 +0200

    queries: inject all lines in a newline escaped RUN Dockerfile instruction as bash (#13845)

--- runtime/queries/dockerfile/injections.scm
+++ runtime/queries/dockerfile/injections.scm
@@ -1,6 +1,7 @@
 ((comment) @injection.content
  (#set! injection.language "comment"))
 
-([(shell_command) (shell_fragment)] @injection.content
- (#set! injection.language "bash"))
+((shell_command (shell_fragment) @injection.content)
+ (#set! injection.language "bash")
+ (#set! injection.combined))
 
