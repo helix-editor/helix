a080e6b735e4c4a91d9f668a2aec1f7a956a7bb4
Jonas Köhnen <mail@satoqz.net>
2025-06-28 15:00:47 +0200

    queries/dockerfile: injections for heredocs

--- runtime/queries/dockerfile/injections.scm
+++ runtime/queries/dockerfile/injections.scm
@@ -5,3 +5,12 @@
  (#set! injection.language "bash")
  (#set! injection.combined))
 
+((run_instruction
+ (heredoc_block (heredoc_line) @injection.content . "\n" @injection.content))
+ (#set! injection.language "bash")
+ (#set! injection.combined))
+
+((copy_instruction
+ (path (heredoc_marker)) . (path) @injection.filename
+ (heredoc_block (heredoc_line) @injection.content . "\n" @injection.content))
+ (#set! injection.combined))
