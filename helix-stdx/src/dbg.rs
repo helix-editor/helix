cc2529c4ac69d1f4b5445c5fd4d06787037810e7
nik rev <pm@nikrev.com>
2025-07-17 16:13:20 +0100

    chore: Add `helix_stdx::dbg!` for easier print debugging
    I often reach for the `dbg!` macro before realizing it does not work,
    and there is no good alternative. The best that currently exists is
    `log::error!` which always prints, but it lacks many features of `dbg!`
    
    This `dbg!` macro is the same as `std`'s `dbg!`, the only difference
    being the fact that it uses `log::error!` instead of `eprintln!` under
    the hood
    
    I've also added instructions to `CONTRIBUTING.md` to use it - as I
    believe it is really useful

---
+++ helix-stdx/src/dbg.rs
@@ -0,0 +1,32 @@
+/// A macro for debugging purposes. The implementation  is taken directly from `std::dbg`,
+/// but instead of using `eprintln!` it uses `log::error!`.
+///
+/// The regular `dbg!` macro does not work because it print to stderr, but that's where
+/// Helix renders.
+///
+/// To see output, check `:log-open`
+// implementation copied from the standard library's `dbg!` implementation
+#[macro_export]
+macro_rules! dbg {
+    // NOTE: We cannot use `concat!` to make a static string as a format argument
+    // of `eprintln!` because `file!` could contain a `{` or
+    // `$val` expression could be a block (`{ .. }`), in which case the `eprintln!`
+    // will be malformed.
+    () => {
+        log::error!("[{}:{}:{}]", file!(), line!(), column!())
+    };
+    ($val:expr $(,)?) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                log::error!("[{}:{}:{}] {} = {:#?}",
+                    file!(), line!(), column!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($(dbg!($val)),+,)
+    };
+}
--- helix-stdx/src/lib.rs
+++ helix-stdx/src/lib.rs
@@ -1,6 +1,7 @@
 //! Extensions to the standard library. A collection of helper functions
 //! used throughout helix.
 
+pub mod dbg;
 pub mod env;
 pub mod faccess;
 pub mod path;
