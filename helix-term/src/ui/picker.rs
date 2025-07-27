0d715049e2866bbd952b9a79842545d9dd5dbd83
rotmh <horesh.rotem@gmail.com>
2025-07-27 15:28:41 +0300

    feat: support scrolling in pickers

--- helix-term/src/ui/picker.rs
+++ helix-term/src/ui/picker.rs
@@ -249,6 +249,8 @@ pub struct Picker<T: 'static + Send + Sync, D: 'static> {
     completion_height: u16,
 
     cursor: u32,
+    scroll: u32,
+
     prompt: Prompt,
     query: PickerQuery,
 
@@ -377,6 +379,7 @@ impl<T: 'static + Send + Sync, D: 'static + Send + Sync> Picker<T, D> {
             editor_data,
             version,
             cursor: 0,
+            scroll: 0,
             prompt,
             query,
             truncate_start: true,
@@ -727,11 +730,36 @@ impl<T: 'static + Send + Sync, D: 'static + Send + Sync> Picker<T, D> {
         // subtract area of prompt from top
         let inner = inner.clip_top(2);
         let rows = inner.height.saturating_sub(self.header_height()) as u32;
-        let offset = self.cursor - (self.cursor % std::cmp::max(1, rows));
-        let cursor = self.cursor.saturating_sub(offset);
-        let end = offset
-            .saturating_add(rows)
-            .min(snapshot.matched_item_count());
+
+        let items_count = snapshot.matched_item_count();
+
+        let max_scroll = items_count.saturating_sub(rows);
+        self.scroll = self.scroll.clamp(0, max_scroll);
+
+        let view_len = std::cmp::min(items_count, rows);
+        let view_end = self.scroll + view_len;
+
+        let scrolloff = cx.editor.config().picker.scrolloff as u32;
+
+        // Ensure the scrolloff is low enough so we won't scroll on every
+        // `move_by(1)`.
+        let max_scrolloff = (view_len / 2).saturating_sub(1);
+        let scrolloff = std::cmp::min(scrolloff, max_scrolloff);
+
+        if self.cursor < self.scroll + scrolloff {
+            self.scroll = self.cursor.saturating_sub(scrolloff);
+        } else if view_end.saturating_sub(scrolloff + 1) < self.cursor {
+            let new_scroll = (self.cursor + scrolloff + 1).saturating_sub(view_len);
+            self.scroll = std::cmp::min(new_scroll, max_scroll);
+        }
+
+        debug_assert!((0..=max_scroll).contains(&self.scroll));
+        debug_assert!((self.scroll..=self.scroll + view_len).contains(&self.cursor));
+
+        let offset = self.scroll;
+        let cursor = self.cursor - self.scroll;
+        let end = offset + view_len;
+
         let mut indices = Vec::new();
         let mut matcher = MATCHER.lock();
         matcher.config = Config::DEFAULT;
