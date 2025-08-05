c8d88e0456e28afd812271b318fe8f9f23e0e9ea
Arthur Deierlein <info@c0rydoras.dev>
2025-08-05 10:49:03 +0200

    fix indents for cython

--- runtime/queries/cython/indents.scm
+++ runtime/queries/cython/indents.scm
@@ -1,49 +1,47 @@
 [
+  (list)
+  (tuple)
+  (dictionary)
+  (set)
+  (if_statement)
+  (for_statement)
+  (while_statement)
+  (with_statement)
+  (try_statement)
+  (match_statement)
+  (case_clause)
   (import_from_statement)
+
+  (parenthesized_expression)
   (generator_expression)
   (list_comprehension)
   (set_comprehension)
   (dictionary_comprehension)
+
   (tuple_pattern)
   (list_pattern)
+  (argument_list)
+  (parameters)
   (binary_operator)
-  (lambda)
-  (concatenated_string)
-] @indent.begin
 
-((list) @indent.align
-  (#set! indent.open_delimiter "[")
-  (#set! indent.close_delimiter "]"))
+  (function_definition)
+  (cdef_statement)
+  (class_definition)
+] @indent
 
-((dictionary) @indent.align
-  (#set! indent.open_delimiter "{")
-  (#set! indent.close_delimiter "}"))
-
-((set) @indent.align
-  (#set! indent.open_delimiter "{")
-  (#set! indent.close_delimiter "}"))
-
-((parenthesized_expression) @indent.align
-  (#set! indent.open_delimiter "(")
-  (#set! indent.close_delimiter ")"))
-
-((for_statement) @indent.begin
-  (#set! indent.immediate 1))
-
-((if_statement) @indent.begin
-  (#set! indent.immediate 1))
-
-((while_statement) @indent.begin
-  (#set! indent.immediate 1))
-
-((try_statement) @indent.begin
-  (#set! indent.immediate 1))
+; Workaround for the tree-sitter grammar creating large errors when a
+; try_statement is missing the except/finally clause
 
 (ERROR
   "try"
   .
-  ":"
-  (#set! indent.immediate 1)) @indent.begin
+  ":" @indent @extend)
+(ERROR
+  .
+  "def") @indent @extend
+(ERROR
+  (block) @indent @extend
+  (#set! "scope" "all"))
 
 (ERROR
   "try"
@@ -55,159 +53,44 @@
         (identifier) @_except) @indent.branch))
   (#eq? @_except "except"))
 
-((function_definition) @indent.begin
-  (#set! indent.immediate 1))
+[
+  (if_statement)
+  (for_statement)
+  (while_statement)
+  (with_statement)
+  (try_statement)
+  (match_statement)
+  (case_clause)
 
-((class_definition) @indent.begin
-  (#set! indent.immediate 1))
-
-((with_statement) @indent.begin
-  (#set! indent.immediate 1))
-
-((match_statement) @indent.begin
-  (#set! indent.immediate 1))
-
-((case_clause) @indent.begin
-  (#set! indent.immediate 1))
-
-; if (cond1
-;     or cond2
-;         or cond3):
-;     pass
-;
-(if_statement
-  condition: (parenthesized_expression) @indent.align
-  (#lua-match? @indent.align "^%([^\n]")
-  (#set! indent.open_delimiter "(")
-  (#set! indent.close_delimiter ")")
-  (#set! indent.avoid_last_matching_next 1))
-
-; while (
-;     cond1
-;     or cond2
-;         or cond3):
-;     pass
-;
-(while_statement
-  condition: (parenthesized_expression) @indent.align
-  (#lua-match? @indent.align "[^\n ]%)$")
-  (#set! indent.open_delimiter "(")
-  (#set! indent.close_delimiter ")")
-  (#set! indent.avoid_last_matching_next 1))
-
-; if (
-;     cond1
-;     or cond2
-;         or cond3):
-;     pass
-;
-(if_statement
-  condition: (parenthesized_expression) @indent.align
-  (#lua-match? @indent.align "[^\n ]%)$")
-  (#set! indent.open_delimiter "(")
-  (#set! indent.close_delimiter ")")
-  (#set! indent.avoid_last_matching_next 1))
-
-(ERROR
-  "(" @indent.align
-  (#set! indent.open_delimiter "(")
-  (#set! indent.close_delimiter ")")
-  .
-  (_))
-
-((argument_list) @indent.align
-  (#set! indent.open_delimiter "(")
-  (#set! indent.close_delimiter ")"))
-
-((parameters) @indent.align
-  (#set! indent.open_delimiter "(")
-  (#set! indent.close_delimiter ")"))
-
-((parameters) @indent.align
-  (#lua-match? @indent.align "[^\n ]%)$")
-  (#set! indent.open_delimiter "(")
-  (#set! indent.close_delimiter ")")
-  (#set! indent.avoid_last_matching_next 1))
-
-((tuple) @indent.align
-  (#set! indent.open_delimiter "(")
-  (#set! indent.close_delimiter ")"))
-
-(ERROR
-  "[" @indent.align
-  (#set! indent.open_delimiter "[")
-  (#set! indent.close_delimiter "]")
-  .
-  (_))
-
-(ERROR
-  "{" @indent.align
-  (#set! indent.open_delimiter "{")
-  (#set! indent.close_delimiter "}")
-  .
-  (_))
+  (cdef_statement)
+  (function_definition)
+  (class_definition)
+] @extend
 
 [
+  (return_statement)
   (break_statement)
   (continue_statement)
-] @indent.dedent
-
-(ERROR
-  (_) @indent.branch
-  ":"
-  .
-  (#lua-match? @indent.branch "^else"))
-
-(ERROR
-  (_) @indent.branch @indent.dedent
-  ":"
-  .
-  (#lua-match? @indent.branch "^elif"))
-
-(generator_expression
-  ")" @indent.end)
-
-(list_comprehension
-  "]" @indent.end)
-
-(set_comprehension
-  "}" @indent.end)
-
-(dictionary_comprehension
-  "}" @indent.end)
-
-(tuple_pattern
-  ")" @indent.end)
-
-(list_pattern
-  "]" @indent.end)
-
-(return_statement
-  [
-    (_) @indent.end
-    (_
-      [
-        (_)
-        ")"
-        "}"
-        "]"
-      ] @indent.end .)
-    (attribute
-      attribute: (_) @indent.end)
-    (call
-      arguments: (_
-        ")" @indent.end))
-    "return" @indent.end
-  ] .)
+  (raise_statement)
+  (pass_statement)
+] @extend.prevent-once
 
 [
   ")"
   "]"
   "}"
-  (elif_clause)
-  (else_clause)
-  (except_clause)
-  (finally_clause)
-] @indent.branch
+] @outdent
+(elif_clause
+  "elif" @outdent)
+(else_clause
+  "else" @outdent)
 
-(string) @indent.auto
+(parameters
+  .
+  (identifier) @anchor
+  (#set! "scope" "tail")) @align
+
+(argument_list
+  .
+  (_) @anchor
+  (#set! "scope" "tail")) @align
