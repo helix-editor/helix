207c0e389968888ed1a1fa064e9d2a100b60cf20
Kristoffer Plagborg Bak Sørensen <57013304+kpbaks@users.noreply.github.com>
2025-08-18 15:23:53 +0200

    feat: inject `jq` into `jq "..."` string literals in common shells (#14253)

--- runtime/queries/fish/injections.scm
+++ runtime/queries/fish/injections.scm
@@ -1,2 +1,7 @@
 ((comment) @injection.content
  (#set! injection.language "comment"))
+
+(command
+  name: (word) @_command (#any-of? @_command "jq" "jaq")
+  argument: [(double_quote_string) (single_quote_string)] @injection.content
+  (#set! injection.language "jq"))
