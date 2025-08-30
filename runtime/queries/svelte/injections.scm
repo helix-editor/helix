253acf232f2635d9a5298977c5f56d49fe975278
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-08-30 15:51:37 +0530

    feat: update svelte runtime queries
    Source: https://github.com/nvim-treesitter/nvim-treesitter/tree/master/queries/svelte

--- runtime/queries/svelte/injections.scm
+++ runtime/queries/svelte/injections.scm
@@ -1,40 +1,47 @@
-; injections.scm
-; --------------
-((style_element
-  (raw_text) @injection.content)
-  (#set! injection.language "css"))
+; inherits: html_tags
 
 ((style_element
-    (start_tag
-      (attribute
-        (attribute_name) @_attr
-        (quoted_attribute_value (attribute_value) @_lang)))
-    (raw_text) @injection.content)
+  (start_tag
+    (attribute
+      (attribute_name) @_attr
+      (quoted_attribute_value
+        (attribute_value) @_lang)))
+  (raw_text) @injection.content)
   (#eq? @_attr "lang")
-  (#match? @_lang "scss")
+  (#any-of? @_lang "scss" "postcss" "less")
   (#set! injection.language "scss"))
 
-((attribute
-   (attribute_name) @_attr
-   (quoted_attribute_value (attribute_value) @css))
- (#eq? @_attr "style"))
-
-(
-  (script_element
-    (start_tag
-      (attribute
-        (quoted_attribute_value (attribute_value) @_lang)))
-    (raw_text) @injection.content)
-  (#match? @_lang "(ts|typescript)")
-  (#set! injection.language "typescript")
-)
-
-((script_element
-  (raw_text) @injection.content)
+((svelte_raw_text) @injection.content
   (#set! injection.language "javascript"))
 
-((raw_text_expr) @injection.content
- (#set! injection.language "javascript"))
+((script_element
+  (start_tag
+    (attribute
+      (attribute_name) @_attr
+      (quoted_attribute_value
+        (attribute_value) @_lang)))
+  (raw_text) @injection.content)
+  (#eq? @_attr "lang")
+  (#any-of? @_lang "ts" "typescript")
+  (#set! injection.language "typescript"))
 
-((comment) @injection.content
- (#set! injection.language "comment"))
+((script_element
+  (start_tag
+    (attribute
+      (attribute_name) @_attr
+      (quoted_attribute_value
+        (attribute_value) @_lang)))
+  (raw_text) @injection.content)
+  (#eq? @_attr "lang")
+  (#any-of? @_lang "js" "javascript")
+  (#set! injection.language "javascript"))
+
+((element
+  (start_tag
+    (attribute
+      (attribute_name) @_attr
+      (quoted_attribute_value
+        (attribute_value) @injection.language)))
+  (text) @injection.content)
+  (#eq? @_attr "lang")
+  (#eq? @injection.language "pug"))
