25f925923e75f242d2435c65597815a3bb4b55ef
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-08-30 20:47:18 +0530

    fix(queries/svelte): use helix compatible scopes

--- runtime/queries/svelte/indents.scm
+++ runtime/queries/svelte/indents.scm
@@ -1,34 +1,24 @@
-; inherits: html
-
 [
+  (element)
+  (start_tag)
   (if_statement)
-  (each_statement)
-  (await_statement)
-  (key_statement)
-  (snippet_statement)
-] @indent.begin
-
-(if_end
-  "}" @indent.end)
-
-(each_end
-  "}" @indent.end)
-
-(await_end
-  "}" @indent.end)
-
-(key_end
-  "}" @indent.end)
-
-(snippet_end
-  "}" @indent.end)
-
-[
-  (if_end)
   (else_if_block)
   (else_block)
+  (then_block)
+  (each_statement)
+  (key_statement)
+  (snippet_statement)
+  (await_statement)
+  (style_element)
+  (script_element)
+  (expression)
+] @indent
+
+[
+  (end_tag)
+  (if_end)
   (each_end)
   (await_end)
   (key_end)
   (snippet_end)
-] @indent.branch
+] @outdent
