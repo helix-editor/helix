f59dc9e48f3b372f5577b6846a73edd94a602493
may <63159454+m4rch3n1ng@users.noreply.github.com>
2025-09-04 16:08:20 +0200

    mark arguments of case-lambda as variable, mark case-lambda as keyword (#14386)

--- runtime/queries/scheme/highlights.scm
+++ runtime/queries/scheme/highlights.scm
@@ -51,6 +51,15 @@
 (list
  .
  (symbol) @_f
+ (list
+  .
+  (list
+   (symbol) @variable))
+ (#eq? @_f "case-lambda"))
+
+(list
+ .
+ (symbol) @_f
  .
  (list
    (list
@@ -92,7 +101,7 @@
   .
   (symbol) @keyword
   (#match? @keyword
-   "^(define-syntax|let\\*|lambda|λ|case|=>|quote-splicing|unquote-splicing|set!|let|letrec|letrec-syntax|let-values|let\\*-values|do|else|define|cond|syntax-rules|unquote|begin|quote|let-syntax|and|if|quasiquote|letrec|delay|or|when|unless|identifier-syntax|assert|library|export|import|rename|only|except|prefix)$"
+   "^(define-syntax|let\\*|lambda|λ|case-lambda|case|=>|quote-splicing|unquote-splicing|set!|let|letrec|letrec-syntax|let-values|let\\*-values|do|else|define|cond|syntax-rules|unquote|begin|quote|let-syntax|and|if|quasiquote|letrec|delay|or|when|unless|identifier-syntax|assert|library|export|import|rename|only|except|prefix)$"
    ))
 
 (list
