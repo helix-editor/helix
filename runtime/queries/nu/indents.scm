769b177d98d5bd72ba9d7c99992f7eaff8f55535
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-09-07 19:19:53 +0800

    bring back indents

---
+++ runtime/queries/nu/indents.scm
@@ -0,0 +1,26 @@
+[
+  (expr_parenthesized)
+  (parameter_bracks)
+  (ctrl_match)
+
+  (val_record)
+  (val_list)
+  (val_closure)
+  (val_table)
+
+  (block)
+] @indent.begin
+
+[
+  "}"
+  "]"
+  ")"
+] @indent.end
+
+[
+  "}"
+  "]"
+  ")"
+] @indent.branch
+
+(comment) @indent.auto
