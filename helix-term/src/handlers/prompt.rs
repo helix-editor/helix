5ffd3d6810bc80394de5eade2ef3d6bd2ae5c58c
Michael Davis <mcarsondavis@gmail.com>
2025-09-15 09:30:39 -0400

    Resolve unused_variables lint

--- helix-term/src/handlers/prompt.rs
+++ helix-term/src/handlers/prompt.rs
@@ -5,9 +5,8 @@ use helix_view::handlers::Handlers;
 use crate::job::{self};
 use crate::ui;
 
-#[allow(unused_variables)]
 pub(super) fn register_hooks(_handlers: &Handlers) {
-    register_hook!(move |event: &mut DocumentFocusLost<'_>| {
+    register_hook!(move |_event: &mut DocumentFocusLost<'_>| {
         job::dispatch_blocking(move |_, compositor| {
             if compositor.find::<ui::Prompt>().is_some() {
                 compositor.remove_type::<ui::Prompt>();
