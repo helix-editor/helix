52455aef918a523ac8c7a09e8ef79783e7d673a6
David <12832280+David-Else@users.noreply.github.com>
2025-11-11 18:28:37 +0000

    Add locals.scm unmodified

---
+++ runtime/queries/gotmpl/locals.scm
@@ -0,0 +1,12 @@
+[
+  (if_action)
+  (range_action)
+  (block_action)
+  (with_action)
+  (define_action)
+] @local.scope
+
+(variable_definition
+  variable: (variable) @local.definition.var)
+
+(variable) @local.reference
