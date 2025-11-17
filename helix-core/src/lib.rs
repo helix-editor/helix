ece11d77bc32cb9c3d46ae699a412b8b675e2077
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-11-17 22:59:20 +0100

    feat(view): render lenses in gutter

--- helix-core/src/lib.rs
+++ helix-core/src/lib.rs
@@ -16,6 +16,7 @@ pub mod graphemes;
 pub mod history;
 pub mod increment;
 pub mod indent;
+pub mod lens;
 pub mod line_ending;
 pub mod macros;
 pub mod match_brackets;
@@ -68,6 +69,7 @@ pub use syntax::Syntax;
 
 pub use completion::CompletionItem;
 pub use diagnostic::Diagnostic;
+pub use lens::Lens;
 
 pub use line_ending::{LineEnding, NATIVE_LINE_ENDING};
 pub use transaction::{Assoc, Change, ChangeSet, Deletion, Operation, Transaction};
