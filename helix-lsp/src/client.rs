aa3f015551e05ac2f29c70e2e1bfc4dcf97ba422
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-11-17 21:36:31 +0100

    feat(lsp): code lens support

--- helix-lsp/src/client.rs
+++ helix-lsp/src/client.rs
@@ -403,6 +403,7 @@ impl Client {
                         | CallHierarchyServerCapability::Options(_)
                 )
             ),
+            LanguageServerFeature::CodeLens => capabilities.code_lens_provider.is_some(),
         }
     }
 
@@ -722,6 +723,9 @@ impl Client {
                     }),
                     call_hierarchy: Some(lsp::DynamicRegistrationClientCapabilities {
                         dynamic_registration: Some(false),
+                    }),
+                    code_lens: Some(lsp::CodeLensClientCapabilities {
+                        ..Default::default()
                     }),
                     ..Default::default()
                 }),
@@ -1712,5 +1716,23 @@ impl Client {
         self.notify::<lsp::notification::DidChangeWatchedFiles>(lsp::DidChangeWatchedFilesParams {
             changes,
         })
+    }
+
+    pub fn code_lens(
+        &self,
+        text_document: lsp::TextDocumentIdentifier,
+    ) -> Option<impl Future<Output = Result<Option<Vec<lsp::CodeLens>>>>> {
+        let capabilities = self.capabilities.get().unwrap();
+
+        // Return early if the server does not support code lens.
+        capabilities.code_lens_provider.as_ref()?;
+
+        let params = lsp::CodeLensParams {
+            text_document,
+            work_done_progress_params: lsp::WorkDoneProgressParams::default(),
+            partial_result_params: lsp::PartialResultParams::default(),
+        };
+
+        Some(self.call::<lsp::request::CodeLensRequest>(params))
     }
 }
