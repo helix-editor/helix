05fbafbc089c1dccaed8cd8d3defe8d867136d4b
Yerlan Sergaziyev <yerlaser@gmail.com>
2025-12-09 02:02:28 +0100

    Fixed jump labels not clearing on Enter

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -7209,7 +7209,6 @@ struct FlashMatch {
     label: char,
 }
 
-// fn find_char(cx: &mut Context, direction: Direction, inclusive: bool, extend: bool) {
 #[allow(clippy::too_many_arguments)]
 fn flash_chunks(editor: &mut Editor, input: &str, forward: bool) -> Option<Vec<FlashMatch>> {
     // if no search string provided - return early
@@ -7285,6 +7284,7 @@ fn flash_chunks(editor: &mut Editor, input: &str, forward: bool) -> Option<Vec<F
     Some(results).filter(|ws| !ws.is_empty())
 }
 
+// fn find_char(cx: &mut Context, direction: Direction, inclusive: bool, extend: bool) {
 fn flash_impl_rec(
     cx: &mut Context,
     movement: Movement,
@@ -7363,6 +7363,7 @@ fn flash_impl_rec(
                 goto_first_nonwhitespace(cx)
             }
 
+            doc_mut!(cx.editor, &doc_id).remove_jump_labels(view_id);
             return;
         }
 
