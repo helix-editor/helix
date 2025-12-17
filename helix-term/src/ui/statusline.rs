8bd3de1c34b31b236a7e757a352c86b1331c1f1e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-12-17 17:27:38 +0800

    do the thing

--- helix-term/src/ui/statusline.rs
+++ helix-term/src/ui/statusline.rs
@@ -338,7 +338,14 @@ where
         if count == 1 {
             " 1 sel ".into()
         } else {
-            format!(" {}/{count} sels ", selection.primary_index() + 1).into()
+            // Fill the current selection index in such a way, that `rotate_selections_…` actions will never make the widget
+            // decrease or increase in length.
+            format!(
+                " {:>count_len$}/{count} sel ",
+                selection.primary_index() + 1,
+                count_len = count.to_string().len()
+            )
+            .into()
         },
     );
 }
@@ -348,10 +355,7 @@ where
     F: Fn(&mut RenderContext<'a>, Span<'a>) + Copy,
 {
     let tot_sel = context.doc.selection(context.view.id).primary().len();
-    write(
-        context,
-        format!(" {} char{} ", tot_sel, if tot_sel == 1 { "" } else { "s" }).into(),
-    );
+    write(context, format!(" {:>3} char ", tot_sel).into());
 }
 
 fn get_position(context: &RenderContext) -> Position {
@@ -372,7 +376,7 @@ where
     let position = get_position(context);
     write(
         context,
-        format!(" {}:{} ", position.row + 1, position.col + 1).into(),
+        format!(" {:>4}:{:<3} ", position.row + 1, position.col + 1).into(),
     );
 }
 
@@ -380,7 +384,7 @@ fn render_total_line_numbers<'a, F>(context: &mut RenderContext<'a>, write: F)
 where
     F: Fn(&mut RenderContext<'a>, Span<'a>) + Copy,
 {
-    let total_line_numbers = context.doc.text().len_lines();
+    let total_line_numbers = context.doc.text().len_lines().saturating_sub(1);
 
     write(context, format!(" {} ", total_line_numbers).into());
 }
@@ -391,9 +395,16 @@ where
 {
     let position = get_position(context);
     let maxrows = context.doc.text().len_lines();
+    let percentage = ((position.row + 1) as f64 * 100. / maxrows as f64).round() as usize;
     write(
         context,
-        format!("{}%", (position.row + 1) * 100 / maxrows).into(),
+        if percentage == 0 || position.row == 0 {
+            "top".into()
+        } else if percentage == 100 || position.row + 2 == maxrows {
+            "bot".into()
+        } else {
+            format!("{:>2}%", percentage).into()
+        },
     );
 }
 
