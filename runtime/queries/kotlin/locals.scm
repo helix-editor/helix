0e49c8ff76ffeeeca9ad8bb1d4d67143a9f5edd1
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-12-23 20:24:47 +0200

    fix locals for functions

--- runtime/queries/kotlin/locals.scm
+++ runtime/queries/kotlin/locals.scm
@@ -1,15 +1,16 @@
 ; Scopes
-
 [
   (class_declaration)
   (function_declaration)
 ] @local.scope
 
 ; Definitions
-
 (type_parameter
   (type_identifier) @local.definition.type.parameter)
 
-; References
+(parameter
+  (simple_identifier) @local.definition.variable.parameter)
 
+; References
+(simple_identifier) @local.reference
 (type_identifier) @local.reference
