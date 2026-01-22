57c684778ced16cee2fbb09284b94c865745f58b
xe_nul <iamxenul@gmail.com>
2026-01-22 23:00:35 +0300

    Add rudimentary workspace trust support

--- helix-term/src/handlers.rs
+++ helix-term/src/handlers.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use arc_swap::ArcSwap;
 use diagnostics::PullAllDocumentsDiagnosticHandler;
 use helix_event::AsyncHook;
+use helix_view::editor::TrustSelector;
 
 use crate::config::Config;
 use crate::events;
@@ -21,11 +22,12 @@ mod document_colors;
 mod prompt;
 mod signature_help;
 mod snippet;
+mod workspace_trust;
 
 pub fn setup(config: Arc<ArcSwap<Config>>) -> Handlers {
     events::register();
 
-    let event_tx = completion::CompletionHandler::new(config).spawn();
+    let event_tx = completion::CompletionHandler::new(config.clone()).spawn();
     let signature_hints = SignatureHelpHandler::new().spawn();
     let auto_save = AutoSaveHandler::new().spawn();
     let document_colors = DocumentColorsHandler::default().spawn();
@@ -51,5 +53,8 @@ pub fn setup(config: Arc<ArcSwap<Config>>) -> Handlers {
     snippet::register_hooks(&handlers);
     document_colors::register_hooks(&handlers);
     prompt::register_hooks(&handlers);
+    if config.load().editor.trust_selector == TrustSelector::SelectPopUp {
+        workspace_trust::register_hooks(&handlers);
+    }
     handlers
 }
