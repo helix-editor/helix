aeb9b6372fadfc45cf91912b5b52b562285e7775
Kyle Lukaszek <kylelukaszek@gmail.com>
2026-01-23 20:43:07 -0500

    Penrose grammar and syntax highlighting.

---
+++ runtime/queries/penrose/textobjects.scm
@@ -0,0 +1,16 @@
+; Functions/constructors/predicates in Domain
+(constructor_decl) @function.outer
+(function_decl) @function.outer
+(predicate_decl) @function.outer
+
+; Blocks
+(block) @block.outer
+(block) @block.inner
+
+; Parameters
+(parameter_list) @parameter.outer
+(parameter) @parameter.inner
+
+; Arguments
+(argument_list) @call.outer
+(argument_list) @call.inner
