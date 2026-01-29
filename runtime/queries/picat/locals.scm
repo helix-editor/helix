efe72462ea1e26f0a3570ab8d93f2511cd7c7a5f
Michael Davis <mcarsondavis@gmail.com>
2026-01-29 10:29:41 -0500

    Add highlights to locals.scm definition captures
    Helix adds the highlight name to the end of the local.definition capture.
    (Unfortunately this is not really documented anywhere yet :/)

--- runtime/queries/picat/locals.scm
+++ runtime/queries/picat/locals.scm
@@ -7,18 +7,18 @@
   (actor_definition)
 ] @local.scope
 
-(import_declaration (_) @local.definition)
+(import_declaration (_) @local.definition.namespace)
 
-(module_declaration (_) @local.definition)
+(module_declaration (_) @local.definition.namespace)
 
-(binary_relational_expression left: (atom) @local.definition)
+(binary_relational_expression left: (atom) @local.definition.variable)
 
 (parameters
-  [(variable) @local.definition
-   (atom) @local.definition
-   (array_expression [(variable) @local.definition (atom) @local.definition])
-   (list_expression [(variable) @local.definition (atom) @local.definition])
-   (as_pattern_expression left: [(variable) @local.definition (atom) @local.definition])])
+  [(variable) @local.definition.variable.parameter
+   (atom) @local.definition.variable.parameter
+   (array_expression [(variable) (atom)] @local.definition.variable.parameter)
+   (list_expression [(variable) (atom)] @local.definition.variable.parameter)
+   (as_pattern_expression left: [(variable) (atom)] @local.definition.variable.parameter)])
 
 (arguments (argument [(variable) @local.reference (atom) @local.reference]))
 
