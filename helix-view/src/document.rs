helix-editor/helix

commit b029e70942f614fc8470ebd92b95f62a3073f16e

copy if file is a symlink (#15233)

helix-view/src/document.rs
@@ -1035,6 +1035,8 @@ impl Document {
 
             // Assume it is a hardlink to prevent data loss if the metadata cant be read (e.g. on certain Windows configurations)
             let is_hardlink = helix_stdx::faccess::hardlink_count(&write_path).unwrap_or(2) > 1;
+            let is_symlink = tokio::fs::symlink_metadata(&write_path).await?.is_symlink();
+            let must_copy = is_hardlink || is_symlink;
             let backup = if path.exists() && atomic_save {
                 let path_ = write_path.clone();
                 // hacks: we use tempfile to handle the complex task of creating
@@ -1046,7 +1048,7 @@ impl Document {
                     let mut builder = tempfile::Builder::new();
                     builder.prefix(path_.file_name()?).suffix(".bck");
 
-                    let backup_path = if is_hardlink {
+                    let backup_path = if must_copy {
                         builder
                             .make_in(path_.parent()?, |backup| std::fs::copy(&path_, backup))
                             .ok()?
@@ -1081,7 +1083,7 @@ impl Document {
             };
 
             if let Some(backup) = backup {
-                if is_hardlink {
+                if must_copy {
                     let mut delete = true;
                     if write_result.is_err() {
                         // Restore backup

commit 5875e70d7a7d8f0afe64997d2c9ed7b649330785

fix(helix-view): regression in force write
Fixes regression introduced by: https://github.com/helix-editor/helix/pull/15233
When saving a new file (using `write!`) we are now checking for symlink
metadata. But if the file doesn't exist, the symlink check fails
and bubbles up, resulting in os error `No such file or directory`.
Fix this by ignoring not found error when checking for symlink metadata.

helix-view/src/document.rs
@@ -1035,7 +1035,11 @@ impl Document {
 
             // Assume it is a hardlink to prevent data loss if the metadata cant be read (e.g. on certain Windows configurations)
             let is_hardlink = helix_stdx::faccess::hardlink_count(&write_path).unwrap_or(2) > 1;
-            let is_symlink = tokio::fs::symlink_metadata(&write_path).await?.is_symlink();
+            let is_symlink = match tokio::fs::symlink_metadata(&write_path).await {
+                Ok(meta) => meta.is_symlink(),
+                Err(err) if err.kind() == std::io::ErrorKind::NotFound => false,
+                Err(err) => return Err(err.into()),
+            };
             let must_copy = is_hardlink || is_symlink;
             let backup = if path.exists() && atomic_save {
                 let path_ = write_path.clone();