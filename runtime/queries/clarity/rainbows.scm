c32f78e37cf425efc5988383683631d36253d43d
Jeff Bencin <jeff.bencin@gmail.com>
2026-03-13 14:32:16 -0400

    Add `rainbows.scm`

---
+++ runtime/queries/clarity/rainbows.scm
@@ -0,0 +1,52 @@
+; Definitions
+[
+  (trait_definition)
+  (trait_implementation)
+  (trait_usage)
+  (fungible_token_definition)
+  (non_fungible_token_definition)
+  (constant_definition)
+  (variable_definition)
+  (mapping_definition)
+  (private_function)
+  (read_only_function)
+  (public_function)
+] @rainbow.scope
+
+; Function calls
+[
+  (basic_native_form)
+  (contract_function_call)
+  (let_expression)
+] @rainbow.scope
+
+; Bindings and signatures
+[
+  (local_binding)
+  (function_signature)
+  (function_parameter)
+  (function_signature_for_trait)
+] @rainbow.scope
+
+; Literals
+[
+  (list_lit)
+  (some_lit)
+  (response_lit)
+  (tuple_lit)
+] @rainbow.scope
+
+; Types
+[
+  (buffer_type)
+  (ascii_string_type)
+  (utf8_string_type)
+  (list_type)
+  (optional_type)
+  (tuple_type)
+  (tuple_type_for_trait)
+  (response_type)
+] @rainbow.scope
+
+; Bracket characters
+["(" ")" "{" "}"] @rainbow.bracket
