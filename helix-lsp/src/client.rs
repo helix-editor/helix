62689c81971d2a012512f3dbe448711bc37c77c6
inianv <inian.vasanth@gmail.com>
2026-03-31 08:59:58 +0530

    fix linter issues

--- helix-lsp/src/client.rs
+++ helix-lsp/src/client.rs
@@ -786,7 +786,10 @@ impl Client {
         };
         // The response receiver is dropped immediately; we do not wait for a reply.
         let (chan, _) = tokio::sync::mpsc::channel(1);
-        let _ = self.server_tx.send(Payload::Request { chan, value: request });
+        let _ = self.server_tx.send(Payload::Request {
+            chan,
+            value: request,
+        });
         self.exit();
     }
 
