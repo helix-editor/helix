helix-editor/helix

commit 6c504a829288d75f5f070182aad5e0df1008bc9e

ecma/injections: support more graphql injection modes (#14727)

runtime/queries/ecma/injections.scm
@@ -22,13 +22,49 @@
  (#eq? @_template_function_name "$")
  (#set! injection.language "bash"))
 
-; Parse the contents of gql template literals
+; GraphQL detection generally matches the rules provided by the 'GraphQL: Syntax Highlighting'
+; VSCode extension: https://github.com/graphql/graphiql/blob/8f25b38f4ab14dc99c046109f255fb283bccde52/packages/vscode-graphql-syntax/grammars/graphql.js.json
 
-((call_expression
-   function: (identifier) @_template_function_name
-   arguments: (template_string (string_fragment) @injection.content))
- (#eq? @_template_function_name "gql")
- (#set! injection.language "graphql"))
+; Parse the contents of 'gql' and 'graphql' template literals and function calls
+(
+  (call_expression
+    function: (identifier) @_template_function_name
+    arguments: [
+      ; Tagged template literal: NAME``
+      (template_string (string_fragment) @injection.content)
+      (
+        arguments . [
+          ; Function call containing a string literal: NAME('')
+          (string (string_fragment) @injection.content)
+          ; Function call containing a template literal: NAME(``)
+          (template_string (string_fragment) @injection.content)
+        ]
+      )
+    ]
+  )
+  (#any-of? @_template_function_name "gql" "graphql")
+  (#set! injection.language "graphql")
+)
+
+; Parse the contents of strings and tagged template literals that begin with a GraphQL comment '#graphql'
+(
+  [
+    (string (string_fragment) @injection.content)
+    (template_string (string_fragment) @injection.content)
+  ]
+  (#match? @injection.content "^\\s*#graphql")
+  (#set! injection.language "graphql")
+)
+
+; Parse the contents of strings and tagged template literals with leading ECMAScript comments '/* GraphQL */'
+(
+  ((comment) @_ecma_comment [
+    (string (string_fragment) @injection.content)
+    (template_string (string_fragment) @injection.content)
+  ])
+  (#eq? @_ecma_comment "/* GraphQL */")
+  (#set! injection.language "graphql")
+)
 
 ; Parse regex syntax within regex literals
 

commit b47d322e53fe2385262fbb94e0c53a7a49ee300a

ecma/injections: fix regressions
1. Fixes algorithmic complexity regression because of GraphQL injection based on comments introduced by https://github.com/helix-editor/helix/pull/14727
2. Fixes broken nested language highlighting introduced by https://github.com/helix-editor/helix/pull/14568/files

runtime/queries/ecma/injections.scm
@@ -7,7 +7,7 @@
     (member_expression
       property: (property_identifier) @injection.language)
   ]
-  arguments: (template_string) @injection.content
+  arguments: (template_string (string_fragment) @injection.content)
   (#any-of? @injection.language "html" "css" "json" "sql" "js" "ts" "bash"))
 
 ; Parse the contents of $ template literals as shell commands
@@ -18,7 +18,7 @@
     (member_expression
       property: (property_identifier) @_template_function_name)
   ]
-  arguments: (template_string) @injection.content
+  arguments: (template_string (string_fragment) @injection.content)
  (#eq? @_template_function_name "$")
  (#set! injection.language "bash"))
 
@@ -58,10 +58,13 @@
 
 ; Parse the contents of strings and tagged template literals with leading ECMAScript comments '/* GraphQL */'
 (
-  ((comment) @_ecma_comment [
+  ((comment) @_ecma_comment) . [
     (string (string_fragment) @injection.content)
     (template_string (string_fragment) @injection.content)
-  ])
+    (call_expression
+      arguments: (template_string (string_fragment) @injection.content)
+    )
+  ]
   (#eq? @_ecma_comment "/* GraphQL */")
   (#set! injection.language "graphql")
 )