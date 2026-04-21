144f4205b4f348701413b1766687766cde24b206
may <m4rch3n1ng@gmail.com>
2026-04-21 22:52:12 +0200

    feat(rainbows/r): add rainbows queries for r

---
+++ runtime/queries/r/rainbows.scm
@@ -0,0 +1,16 @@
+[
+  (parameters)
+  (if_statement)
+  (for_statement)
+  (while_statement)
+  (braced_expression)
+  (parenthesized_expression)
+  (arguments)
+] @rainbow.scope
+
+[
+  "(" ")"
+  "[" "]"
+  "{" "}"
+  "[[" "]]"
+] @rainbow.bracket
