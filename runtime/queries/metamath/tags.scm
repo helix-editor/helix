8186656f9c6fb042bf469989426bd2c6367c31c9
m4dh0rs3 <m4dh0rs3@posteo.de>
2026-04-26 12:49:16 +0200

    feat(languages): syntax highlighting for Metamath

---
+++ runtime/queries/metamath/tags.scm
@@ -0,0 +1,19 @@
+; Axioms
+(axiomstmt
+  (label) @name) @definition.function
+
+; Provable theorems
+(provablestmt
+  (label) @name) @definition.function
+
+; Floating hypotheses
+(floatingstmt
+  (label) @name) @definition.variable
+
+; Essential hypotheses
+(essentialstmt
+  (label) @name) @definition.variable
+
+; References in proofs
+(uncompressedproof
+  (label) @name) @reference.call
