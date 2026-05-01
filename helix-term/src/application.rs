378fbfc4c3438b75f3bef2a9938bc7b18b80fd9c
lightningkappa <lightning_kappa@protonmail.com>
2026-05-01 07:41:33 +0100

    get rid of unreachable!, only changing focus if there is a view

--- helix-term/src/application.rs
+++ helix-term/src/application.rs
@@ -213,11 +213,10 @@ impl Application {
                         doc.set_selection(view_id, selection);
                     }
                 }
-                // im not sure if this may fail under some conditions 
-                let Some(first_view) = editor.tree.views().map(|(view, _)| view.id).next() else {
-                    unreachable!()
-                };
-                editor.focus(first_view);
+                let first_view = editor.tree.views().map(|(view, _)| view.id).next();
+                if let Some(first_view) = first_view {
+                    editor.focus(first_view);
+                }
 
                 // if all files were invalid, replace with empty buffer
                 if nr_of_files == 0 {
