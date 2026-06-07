1dc8599722ba30c1746c7fdd22249883d2ae3d35
Blaž Hrastnik <blaz@mxxn.io>
2026-06-07 20:48:50 +0900

    runtime: Add rainbows.scm for more languages

---
+++ runtime/queries/elm/rainbows.scm
@@ -0,0 +1,14 @@
+[
+  (exposing_list)
+  (list_expr)
+  (parenthesized_expr)
+  (record_expr)
+  (record_type)
+  (tuple_expr)
+] @rainbow.scope
+
+[
+  "(" ")"
+  "{" "}"
+  "[" "]"
+] @rainbow.bracket
---
+++ runtime/queries/solidity/rainbows.scm
@@ -0,0 +1,35 @@
+[
+  (array_access)
+  (assembly_flags)
+  (assembly_statement)
+  (block_statement)
+  (call_argument)
+  (contract_body)
+  (do_while_statement)
+  (enum_body)
+  (error_declaration)
+  (for_statement)
+  (function_body)
+  (if_statement)
+  (inline_array_expression)
+  (meta_type_expression)
+  (override_specifier)
+  (parenthesized_expression)
+  (slice_access)
+  (struct_body)
+  (struct_expression)
+  (tuple_expression)
+  (using_directive)
+  (variable_declaration_tuple)
+  (while_statement)
+  (yul_block)
+  (yul_function_call)
+  (yul_function_definition)
+  (yul_variable_declaration)
+] @rainbow.scope
+
+[
+  "(" ")"
+  "{" "}"
+  "[" "]"
+] @rainbow.bracket
