9bc423cbd6bfab4827ba2459c4369541dd7f78c1
Tyler Laprade, CFA <pinkmonkeyblue@gmail.com>
2026-06-20 14:10:29 -0400

    Fix file explorer path test on Windows

--- helix-term/src/ui/file_explorer.rs
+++ helix-term/src/ui/file_explorer.rs
@@ -461,7 +461,7 @@ mod tests {
 
     #[test]
     fn test_pathbuf_is_absolute() {
-        let abs_path = PathBuf::from("/home/user/file.txt");
+        let abs_path = std::env::current_dir().unwrap().join("file.txt");
         let rel_path = PathBuf::from("./file.txt");
 
         assert!(abs_path.is_absolute());
