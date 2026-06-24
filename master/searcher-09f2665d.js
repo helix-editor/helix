c887930bac7375e2b9c785e68f3af6f690e6dff6
archseer <archseer@users.noreply.github.com>
2026-06-24 06:02:55 +0000

    deploy: 4a8a56ec2c3f1ad2e39fcabfd44793cf9402a135

--- master/searcher-09f2665d.js
+++ master/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5398f83b.js',
+                path_to_root + 'searchindex-b55beadb.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
