7f21f974826c8e7a189055d8706b7b0ccd848947
Vinicius Deolindo <git@vini.cat>
2026-06-29 16:24:21 -0300

    tests: fix nix highlight test assertions

--- tests/query/highlights/nix/calls.nix
+++ tests/query/highlights/nix/calls.nix
@@ -1,7 +1,7 @@
 {
   x = map double items;
-//    ^ @function.builtin
+#     ^ @function.builtin
   y = builtins.length items;
-//    ^ @constant.builtin
-//             ^ @function.builtin
+#     ^ @constant.builtin
+#              ^ @function.builtin
 }
