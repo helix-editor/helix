069c6f1b8d9420c727ef677e71c855a05a111827
Vinicius Deolindo <git@vini.cat>
2026-06-29 16:40:33 -0300

    queries: highlight calling-side of pipe operators as functions

--- tests/query/highlights/nix/calls.nix
+++ tests/query/highlights/nix/calls.nix
@@ -4,4 +4,8 @@
   y = builtins.length items;
 #     ^ @constant.builtin
 #              ^ @function.builtin
+  z = value |> lib.foo;
+#                  ^ @function
+  w = lib.foo <| value;
+#         ^ @function
 }
