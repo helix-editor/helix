9ff353c201fe5f10d66a1782117b563389cb9333
the-mikedavis <the-mikedavis@users.noreply.github.com>
2026-07-04 20:27:00 +0000

    deploy: a262aab7be5e78f059185c0fdb43ebf1e8bcde60

--- master/searcher-09f2665d.js
+++ master/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c40829c8.js',
+                path_to_root + 'searchindex-d0f695ab.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
